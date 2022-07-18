b838012b433dc7722eddcdda8a26d7e1609393d6
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 17:26:35 +0300

    Added Solution 09

---
+++ Fundamentals C#/DataTypes and Variables/T09. Chars to String/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace T09._Chars_to_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char third = char.Parse(Console.ReadLine());
+            Console.WriteLine(first.ToString() + second.ToString() + third.ToString());
+        }
+    }
+}
