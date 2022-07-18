6bcff588bb9f1b576db74cf938d8a5116bf3d2ec
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 12:03:40 +0300

    Added Solution 03

---
+++ Fundamentals C#/DataTypes and Variables/T03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T03._Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //Number of digits
+            decimal exactSum = 0; //decimal -> NOT lose precision
+            for (int i = 1; i <= n; i++)
+            {
+                decimal num = decimal.Parse(Console.ReadLine()); //decimal -> Not lose precision
+                exactSum += num;
+            }
+            Console.WriteLine(exactSum);
+        }
+    }
+}
