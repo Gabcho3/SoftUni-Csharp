fc7e937129ed7f281612acf3b6e63975bfab2ed7
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 23:08:51 +0300

    Added Solution 02

---
+++ Fundamentals C#/DataTypes and Variables Ex/T02. Sum Digits/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sumOfDigits = 0;
+            for(int i = 0; i < num.ToString().Length; i++) {
+                sumOfDigits += int.Parse(num.ToString()[i].ToString());
+            }
+            Console.WriteLine(sumOfDigits);
+        }
+    }
+}
