e6af9582caa94ac15bbba621890e69591859bf38
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 11:55:17 +0300

    Added Solution 02

---
+++ Fundamentals C#/DataTypes and Variables/T02. Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace T02._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pounds = double.Parse(Console.ReadLine()); //DOUBLE because "dollars" is DOUBLE
+            double dollars = pounds * 1.31;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
