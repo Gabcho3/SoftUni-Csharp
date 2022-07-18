f0e2dd60fe1b7fb988064ff2fffa40420ce2e768
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 17:23:23 +0300

    Added Solution 08

---
+++ Fundamentals C#/DataTypes and Variables/T08. Town Info/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace T08._Town_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string townName = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            ushort area = ushort.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
+        }
+    }
+}
