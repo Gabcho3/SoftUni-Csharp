9e0110f7af2ae5a0a11376a51efe5b9ac4800bed
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 16:16:03 +0300

    Added Solution 04

---
+++ Fundamentals C#/DataTypes and Variables/T04. Centuries to Minutes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T04._Centuries_to_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = byte.Parse(Console.ReadLine());
+            ushort years = (ushort)(centuries * 100);
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            ulong minutes = (ulong)hours * 60;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
