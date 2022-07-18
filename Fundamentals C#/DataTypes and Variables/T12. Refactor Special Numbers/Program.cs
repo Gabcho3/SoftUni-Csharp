2bcae0a815f9e3e7e45abe620d9be80aea51570f
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 17:51:54 +0300

    Added Solution 12

---
+++ Fundamentals C#/DataTypes and Variables/T12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T12._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine()); //changed name
+            bool special = false; //changed name
+            for (int num = 1; num <= count; num++) //change name 
+            {
+                int sum = 0; //smaller span
+                int digits = num; //smaler span
+                while (digits > 0) //num -> digits
+                {
+                    sum += digits % 10; //num -> digits
+                    digits /= 10; //num -> digits
+                }
+                if (sum == 5 || sum == 7 || sum == 11) special = true; //made if-else construction
+                else special = false;
+                Console.WriteLine("{0} -> {1}", num, special); //digits -> num
+            }
+        }
+    }
+}
