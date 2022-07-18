e2651fdb4b26470877ac9b47799530e3214c28d2
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 17:19:32 +0300

    Added Solution 07

---
+++ Fundamentals C#/DataTypes and Variables/T07. Concat Names/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace T07._Concat_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            string delimeter = Console.ReadLine();
+            Console.WriteLine($"{firstname}{delimeter}{secondName}");
+        }
+    }
+}
