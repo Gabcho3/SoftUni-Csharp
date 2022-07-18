80f4d5019c94a40f694369fa50171192279fb847
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 17:51:32 +0300

    Added Solution 11

---
+++ Fundamentals C#/DataTypes and Variables/T11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T11._Refactor_Volume_of_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine()); //0 -> double.Parse(Console.ReadLine())
+
+            Console.Write("Width: ");
+            double hight = double.Parse(Console.ReadLine()); //0 -> double.Parse(Console.ReadLine())
+
+            Console.Write("Height: "); //heigth -> height
+
+            double volume = double.Parse(Console.ReadLine()); //0 -> double.Parse(Console.ReadLine())
+
+            volume = (length * hight * volume) / 3; //+ -> *
+
+            Console.WriteLine($"Pyramid Volume: {volume:f2}");
+
+        }
+    }
+}
