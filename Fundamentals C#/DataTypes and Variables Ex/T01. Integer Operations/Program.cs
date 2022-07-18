55becd4b8e0070f0b34ea9d5765a5fa4e7269383
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 23:05:02 +0300

    Added Solution 01

---
+++ Fundamentals C#/DataTypes and Variables Ex/T01. Integer Operations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T01._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+
+            //Operations
+            int operationAdd = firstNum + secondNum;
+            int divide = operationAdd / thirdNum;
+            int multyply = divide * fourthNum;
+
+            Console.WriteLine(multyply);
+        }
+    }
+}
