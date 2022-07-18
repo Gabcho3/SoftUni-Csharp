8cfadc5a1fd0264cd5012a113e21bf7bc453107c
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 17:05:49 +0300

    Added Solution 05

---
+++ Fundamentals C#/DataTypes and Variables/T05. Special Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace T05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //number of digits
+
+            for (int num = 1; num <= n; num++)
+            {
+                int sumOfDigits = 0;
+                int digits = num;
+                while (digits > 0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits /= 10;
+                }
+                Console.WriteLine(sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11 ? $"{num} -> True" : $"{num} -> False");
+            }
+
+            //OTHER SOLUTION
+            //for (int num = 1; num <= n; num++)
+            //{
+            //    int sum = 0;
+            //    int numLength = num.ToString().Length;
+            //    for (int i = 0; i < numLength; i++)
+            //    {
+            //        sum += int.Parse(num.ToString()[i].ToString());
+            //    }
+            //    Console.WriteLine(sum == 5 || sum == 7 || sum == 11 ? $"{num} -> True" : $"{num} -> False");
+            //}
+        }
+    }
+}
