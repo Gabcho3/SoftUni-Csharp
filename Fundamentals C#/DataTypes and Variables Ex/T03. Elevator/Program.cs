2fc8adaffe6ceabf1a1dbbe7a640b6e91092cda8
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 23:15:31 +0300

    Added Solution 03

---
+++ Fundamentals C#/DataTypes and Variables Ex/T03. Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace T03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double people = ushort.Parse(Console.ReadLine()); //double because of Math.Ceiling()
+            double capacity = ushort.Parse(Console.ReadLine()); //double beacuse of Math.Ceiling()
+            double courses = Math.Ceiling(people / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
