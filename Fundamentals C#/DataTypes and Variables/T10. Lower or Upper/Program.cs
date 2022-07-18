c8d7c7254f2ca4b29b44036bf1313308900851f7
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 17:51:21 +0300

    Added Solution 10

---
+++ Fundamentals C#/DataTypes and Variables/T10. Lower or Upper/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace T10._Lower_or_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+            if (char.IsUpper(input)) Console.WriteLine("upper-case");
+            else Console.WriteLine("lower-case");
+        }
+    }
+}
