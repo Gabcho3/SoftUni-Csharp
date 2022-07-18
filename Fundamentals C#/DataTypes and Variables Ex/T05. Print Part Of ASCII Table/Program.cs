229bc25997b21b7f3279f339a268c2e670f259ec
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 23:31:56 +0300

    Added Solution 05

---
+++ Fundamentals C#/DataTypes and Variables Ex/T05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T05._Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startCharIndex = int.Parse(Console.ReadLine());
+            int lastCharIndex = int.Parse(Console.ReadLine());
+            for (int charIndex = startCharIndex; charIndex <= lastCharIndex; charIndex++) {
+                Console.Write((char)charIndex + " "); //(char) -> to print char of charIndex, NOT int
+            }
+        }
+    }
+}
