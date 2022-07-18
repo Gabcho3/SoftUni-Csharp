0515e290d307ffb811740e13e57066c77b89b9c6
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 23:30:41 +0300

    Added Solution 04

---
+++ Fundamentals C#/DataTypes and Variables Ex/T04. Sum of Chars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T04._Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte newLines = sbyte.Parse(Console.ReadLine()); //[1…20]
+            int sumOfChars = 0;
+            for (int i = 1; i <= newLines; i++) {
+                char newChar = char.Parse(Console.ReadLine());
+                sumOfChars += newChar; //char is ASCII code
+            }
+            Console.WriteLine($"The sum equals: {sumOfChars}");
+        }
+    }
+}
