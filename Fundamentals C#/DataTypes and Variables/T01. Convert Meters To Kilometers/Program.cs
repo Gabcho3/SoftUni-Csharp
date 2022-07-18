fd621da20c351a30b463fda8db9211f6f32ea211
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-18 11:52:23 +0300

    Added Solution 01

---
+++ Fundamentals C#/DataTypes and Variables/T01. Convert Meters To Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace T01._Convert_Meters_To_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine()); //DOUBLE because kilometers are DOUBLE
+            double kilometers = meters / 1000;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
