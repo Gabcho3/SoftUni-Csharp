fcd265a6f5c9f3a632ef8d7c2533408bf9527560
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-20 00:27:49 +0300

    Added Solution 01

---
+++ Fundamentals C#/DataTypes and Variables MoreEx/T01. Data Type Finder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T01._Data_Type_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string finalType = string.Empty;
+            while (input != "END")
+                if (!string.IsNullOrEmpty(input))
+                {
+                    int tryInt;
+                    double floatingPoint;
+                    char character;
+                    bool boolean;
+                    string String;
+                    if (Int32.TryParse(input, out tryInt))
+                        finalType = "integer";
+                    else if (Double.TryParse(input, out floatingPoint))
+                        finalType = "floating point";
+                    else if (Char.TryParse(input, out character))
+                        finalType = "character";
+                    else if (Boolean.TryParse(input, out boolean))
+                        finalType = "boolean";
+                    else
+                        finalType = "string";
+                    Console.WriteLine($"{input} is {finalType} type");
+                    input = Console.ReadLine();
+                }
+        }
+    }
+}
