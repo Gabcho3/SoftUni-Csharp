676d7e3d1b78c872723ac03a64fad8ed4f398c60
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-19 23:21:17 +0300

    Improved code of Solution 05

--- Basics C#/FirstStepsMoreEx/T05.TrainingLab/Program.cs
+++ Basics C#/FirstStepsMoreEx/T05.TrainingLab/Program.cs
@@ -9,15 +9,15 @@ namespace T05.TrainingLab
             double w = double.Parse(Console.ReadLine());
             double h = double.Parse(Console.ReadLine());
 
-            double wcm = w * 100;
-            double hcm = h * 100;
+            double wCm = w * 100;
+            double hCm = h * 100;
 
-            double withoutcoridor = hcm - 100;
+            double withoutcoridor = hCm - 100;
             double deskonrow = withoutcoridor % 70;
             double deskonrow1 = withoutcoridor - deskonrow;
             double deskonrow2 = deskonrow1 / 70;
-            double row = wcm % 120 ;
-            double row1 = wcm - row;
+            double row = wCm % 120 ;
+            double row1 = wCm - row;
             double row2 = row1 / 120;
 
             double places = (deskonrow2 * row2) - 3;
