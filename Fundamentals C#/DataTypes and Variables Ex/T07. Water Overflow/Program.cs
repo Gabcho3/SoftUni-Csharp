3cef2680491d1a9c0a8b49ef1312685de20eec01
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-19 13:34:34 +0300

    Added Solution 07

---
+++ Fundamentals C#/DataTypes and Variables Ex/T07. Water Overflow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //capacity of tank is 255 liters
+            //no more than 255 liters
+            int lines = int.Parse(Console.ReadLine());
+            int totalLiters = 0;
+            for(int i = 0; i < lines; i++) { 
+                int liters = int.Parse(Console.ReadLine());
+                totalLiters += liters;
+                if(totalLiters > 255) {
+                    Console.WriteLine("Insufficient capacity!");
+                    totalLiters -= liters; //liters are more and don't count
+                }
+            }
+            Console.WriteLine(totalLiters);
+        }
+    }
+}
