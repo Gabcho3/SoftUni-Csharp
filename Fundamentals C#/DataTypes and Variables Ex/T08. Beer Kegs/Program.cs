ac6107d413a25b0ac8ef12489300223a1e9200bd
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-19 13:50:12 +0300

    Added Solution 08

---
+++ Fundamentals C#/DataTypes and Variables Ex/T08. Beer Kegs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kegs = int.Parse(Console.ReadLine());
+            double bigestKegVolume = int.MinValue;
+            string biggestKeg = string.Empty; //Must print the model of the BIGGEST keg
+
+            for(int keg = 1; keg <= kegs ; keg++) { //we have 3 INPUTS for every keg
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * radius * radius * height; //formula of VOLUME
+                if (volume > bigestKegVolume) {
+                    bigestKegVolume = volume;
+                    biggestKeg = model;
+                }
+            }
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
