d3172aa4814f9ac2cd42a7c4b305118ea31cf1e1
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-19 14:56:05 +0300

    Added Solution 10

---
+++ Fundamentals C#/DataTypes and Variables Ex/T10. Pokemon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T10._Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int powerоfPokemon = int.Parse(Console.ReadLine());
+            int originalValue = powerоfPokemon;
+
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int reachedTargets = 0;
+
+            while(powerоfPokemon >= distance) {
+                powerоfPokemon -= distance;
+                if (originalValue * 0.5 == powerоfPokemon) {
+                    powerоfPokemon /= exhaustionFactor;
+                }
+                
+                reachedTargets++; 
+            }
+            Console.WriteLine(powerоfPokemon);
+            Console.WriteLine(reachedTargets);
+        }
+    }
+}
