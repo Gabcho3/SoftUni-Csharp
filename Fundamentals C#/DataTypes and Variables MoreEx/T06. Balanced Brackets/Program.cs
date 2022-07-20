611812f15e1c13aefcc1109c185cf0b1fe91d07e
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-20 19:34:47 +0300

    Added Solution 06

---
+++ Fundamentals C#/DataTypes and Variables MoreEx/T06. Balanced Brackets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace T06._Balanced_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int openedBrackets = 0;
+            int closedBrackets = 0;
+            for(int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+                if(input == "(")
+                {
+                    openedBrackets++;
+                    if(openedBrackets - closedBrackets == 2)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+                if(input == ")")
+                {
+                    closedBrackets++;
+                    if (openedBrackets - closedBrackets != 0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+            if (openedBrackets != closedBrackets)
+            {
+                Console.WriteLine("UNBALANCED");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("BALANCED");
+                return;
+            }
+        }
+    }
+}
