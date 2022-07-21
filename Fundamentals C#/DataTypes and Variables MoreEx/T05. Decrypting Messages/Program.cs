0c3049055ede2866b6df29f50cb43df67cd7dea5
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-21 12:37:23 +0300

    Added Solution 05

---
+++ Fundamentals C#/DataTypes and Variables MoreEx/T05. Decrypting Messages/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T05._Decrypting_Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte key = sbyte.Parse(Console.ReadLine());
+            sbyte lines = sbyte.Parse(Console.ReadLine());
+            
+            for(int i = 0; i < lines; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                int output = (int)letter + key;
+                char outputLetter = (char)output;
+                Console.Write(outputLetter);
+            }
+        }
+    }
+}
