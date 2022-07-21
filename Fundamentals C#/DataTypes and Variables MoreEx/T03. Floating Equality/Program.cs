bff07431f5de985aaa418bb80711798d43c7b8e3
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-21 12:37:51 +0300

    Added Solution 03

---
+++ Fundamentals C#/DataTypes and Variables MoreEx/T03. Floating Equality/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T03._Floating_Equality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = Math.Abs(double.Parse(Console.ReadLine()));
+            double num2 = Math.Abs(double.Parse(Console.ReadLine()));
+
+            double eps = 0.000001;
+
+            double diff = Math.Abs(num1 - num2); //because of NEGATIVE number
+
+            if(diff < eps)
+                Console.WriteLine("True");
+            else
+                Console.WriteLine("False");
+        }
+    }
+}
