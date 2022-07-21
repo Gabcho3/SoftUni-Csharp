e2740834c8cebf43487e7dbc586c997b06a5cca7
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-21 12:16:34 +0300

    Added Solution 04

---
+++ Fundamentals C#/DataTypes and Variables MoreEx/T04. Refactoring Prime Checker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T04._Refactoring_Prime_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine()); //changed name 
+            for (int num = 2; num <= range; num++) //changed name
+            {
+                bool isPrime = true; //changed name
+                for (int divider = 2; divider < num; divider++) //changed name
+                {
+                    if (num % divider == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine(isPrime ? $"{num} -> true": $"{num} -> false"); //chanhged structure
+            }
+
+        }
+    }
+}
