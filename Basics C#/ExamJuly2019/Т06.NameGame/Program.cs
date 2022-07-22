c0e401cd9bd8ccc8505995820da209fc6fd4067d
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-22 23:18:33 +0300

    Added Solution  06.NameGame

--- Basics C#/ExamJuly2019/Т06.NameGame/Program.cs
+++ Basics C#/ExamJuly2019/Т06.NameGame/Program.cs
@@ -6,7 +6,33 @@ namespace Т06.NameGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nameOfPlayer = Console.ReadLine();
+
+            string winner = null;
+            int points = 0;
+            int maxPoints = int.MinValue;
+
+            while(nameOfPlayer != "Stop")
+            {
+                int length = nameOfPlayer.Length;
+                for(int i = 0; i < length; i++)
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    if (number == (char)nameOfPlayer[i])
+                        points += 10;
+                    else
+                        points += 2;
+                }
+                if (points >= maxPoints)
+                {
+                    winner = nameOfPlayer;
+                    maxPoints = points;
+                }
+
+                points = 0;
+                nameOfPlayer = Console.ReadLine();
+            }
+            Console.WriteLine($"The winner is {winner} with {maxPoints} points!");
         }
     }
 }
