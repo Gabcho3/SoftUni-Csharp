754f8dec18046278c8bd1d8acbbc3427808576dd
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-22 21:39:34 +0300

    Added Solution 13

--- Basics C#/NestedLoopsMoreEx/T13. Prime Pairs/Program.cs
+++ Basics C#/NestedLoopsMoreEx/T13. Prime Pairs/Program.cs
@@ -13,6 +13,41 @@ namespace T13._Prime_Pairs
 
             int max1 = start1 + diff1;
             int max2 = start2 + diff2;
+
+
+            for (int first = start1; first <= max1; first++)
+            {
+                bool isPrime1 = true;
+                bool isPrime2 = true;
+                for(int divider1 = 2; divider1 < first; divider1++) //check if first pair is PRIME
+                {
+                    if (first % divider1 == 0)
+                    {
+                        isPrime1 = false;
+                        break;
+                    }
+                    else
+                        isPrime1 = true; 
+                }
+                if (!isPrime1)
+                    continue;
+                for (int second = start2; second <= max2; second++)
+                {
+                    for (int divider2 = 2; divider2 < second; divider2++) //check if second pair is PRIME
+                    {
+                        if (second % divider2 == 0)
+                        {
+                            isPrime2 = false;
+                            break;
+                        }
+                        else
+                            isPrime2 = true;
+                    }
+                    if (isPrime1 && isPrime2)
+                        Console.WriteLine("{0}{1}", first, second);
+                }
+            }
+
         }
     }
 }
