f90d69ecbd60f9606d81a62f3fd71afb6e72e039
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-22 17:50:58 +0300

    Added Solution 02

--- Basics C#/NestedLoopsMoreEx/T02.LettersCombinations/Program.cs
+++ Basics C#/NestedLoopsMoreEx/T02.LettersCombinations/Program.cs
@@ -6,7 +6,30 @@ namespace T02.LettersCombinations
     {
         static void Main(string[] args)
         {
-            
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            char skip = char.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (char first = start; first <= end; first++)
+            {
+                if (first == skip)
+                    continue;
+                for (char second = start; second <= end; second++)
+                {
+                    if (second == skip)
+                        continue;
+                    for (char third = start; third <= end; third++)
+                    {
+                        if (third == skip)
+                            continue;
+                        Console.Write($"{first}{second}{third} ");
+                        count++;
+                    }
+                }
+            }
+            Console.Write(count);
         }
     }
 }
