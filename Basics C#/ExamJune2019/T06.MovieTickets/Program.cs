4d2a5b640992b2ddae2e4a6585660d51c5e186b3
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-22 22:37:58 +0300

    Added Solution 06. Movie Tickets

--- Basics C#/ExamJune2019/T06.MovieTickets/Program.cs
+++ Basics C#/ExamJune2019/T06.MovieTickets/Program.cs
@@ -6,7 +6,27 @@ namespace T06.MovieTickets
     {
         static void Main(string[] args)
         {
-            
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            for (char symbol1 = (char)a1; symbol1 <= a2 - 1; symbol1++)
+            {
+                if(symbol1 % 2 == 0)
+                {
+                    continue;
+                }
+                for (int symbol2 = 1; symbol2 <= n - 1; symbol2++)
+                {
+                    for (int symbol3 = 1; symbol3 <= n / 2 - 1; symbol3++)
+                    {
+                        int symbol4 = symbol1;
+                        int sumOfSymbols = symbol2 + symbol3 + symbol4;
+                        if(sumOfSymbols % 2 != 0)
+                            Console.WriteLine("{0}-{1}{2}{3}", symbol1, symbol2, symbol3, symbol4);
+                    }
+                }
+            }
         }
     }
 }
