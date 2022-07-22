36c524e0382518e2e1e1e07a8c5832b62169f6b1
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-22 22:21:50 +0300

    Added Solution 06. Favorite Movie

--- Basics C#/ExamJune2019/T06.FavoriteMovie/Program.cs
+++ Basics C#/ExamJune2019/T06.FavoriteMovie/Program.cs
@@ -6,7 +6,45 @@ namespace T06.FavoriteMovie
     {
         static void Main(string[] args)
         {
-            
+            //ASCII: A - Z -> 65 - 90
+            //ASCII: a - z -> 97 - 122
+            string film = Console.ReadLine();
+
+            int filmCount = 0;
+            int sum = 0;
+            int maxSum = int.MinValue;
+            string bestFilm = null;
+
+            while (film != "STOP")
+            {
+                int length = film.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    char letter = film[i];
+                    sum += letter;
+                    if (letter >= 65 && letter <= 90)
+                        sum -= length;
+                    if (letter >= 97 && letter <= 122)
+                        sum -= length * 2;
+                }
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    bestFilm = film;
+                }
+
+                filmCount++;
+                if (filmCount == 7)
+                {
+                    Console.WriteLine("The limit is reached.");
+                    break;
+                }
+
+                sum = 0;
+                film = Console.ReadLine();
+            }
+            Console.WriteLine("The best movie for you is {0} with {1} ASCII sum.", bestFilm, maxSum);
         }
     }
 }
