19dd9be198a5ad9a0e72ee30b7132807f2c282c9
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-22 17:29:40 +0300

    Fixed code

--- Basics C#/WhileLoopMoreEx/T03. Stream Of Letters/Program.cs
+++ Basics C#/WhileLoopMoreEx/T03. Stream Of Letters/Program.cs
@@ -14,32 +14,50 @@ namespace T03._Stream_Of_Letters
             int countO = 0;
             int countN = 0;
 
+            bool c = false;
+            bool o = false;
+            bool n = false;
+
             while (input != "End")
             {
                 char letter = char.Parse(input);
+                string word = null;
+
                 if (letter == 'c')
                 {
                     countC++;
                     if (countC > 1)
-                        Console.Write(input);
+                        word += input;
                 }
-                if (letter == 'o')
+                else if (letter == 'o')
                 {
                     countO++;
                     if (countO > 1)
-                        Console.Write(input);
+                        word += input;
                 }
-                if (letter == 'n')
+                else if (letter == 'n')
                 {
                     countN++;
                     if (countN > 1)
-                        Console.Write(input);
+                        word += input;
                 }
+                else if (letter >= 65 && letter <= 90 || letter >= 97 && letter <= 122)
+                    word += input;
 
                 if (countC == 1 && countO == 1 && countN == 1)
+                {
+                    Console.Write(word);
+                    word = null;
                     Console.Write(" ");
-                if (letter >= 65 && letter <= 90 || letter >= 97 && letter <= 122)
-                    Console.Write(input);
+
+                    countC = 0;
+                    countO = 0;
+                    countN = 0;
+
+                    c = true;
+                    o = true;
+                    n = true;
+                }
                 input = Console.ReadLine();
             }
         }
