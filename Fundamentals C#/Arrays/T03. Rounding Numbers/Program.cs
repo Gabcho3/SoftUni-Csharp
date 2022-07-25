8b4cdd3364c29bd479a583897d532978986deb91
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-25 17:28:19 +0300

    Added Solution 03

---
+++ Fundamentals C#/Arrays/T03. Rounding Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace T03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+            int[] roundedNums = new int[numbers.Length];
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                roundedNums[i] = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+            }
+            for(int j = 0; j < roundedNums.Length; j++)
+            {
+                Console.WriteLine($"{numbers[j]} => {roundedNums[j]}");
+            }
+        }
+    }
+}
