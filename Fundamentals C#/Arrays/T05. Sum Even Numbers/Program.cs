78cf4268720dd1a2c8669e1180ddfedc1c866ea6
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-25 18:02:24 +0300

    Added Solution 05

---
+++ Fundamentals C#/Arrays/T05. Sum Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace T05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = 0;
+
+            foreach(int num in nums)
+            {
+                if (num % 2 == 0)
+                    sum += num;
+            }
+            Console.Write(sum);
+        }
+    }
+}
