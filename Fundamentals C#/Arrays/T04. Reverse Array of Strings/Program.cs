74281d6b5100b3c4dd6fb6eaa2399726b16572da
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-25 17:53:40 +0300

    Added Solution 04

---
+++ Fundamentals C#/Arrays/T04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace T04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                string oldElement = input[i];
+                input[i] = input[input.Length - 1 - i]; //to reverse input
+                input[input.Length - 1 - i] = oldElement;
+            }
+            Console.WriteLine(string.Join(" ", input));
+
+            //OTHER SOLUTION:
+            //Array.Reverse(input);
+            //Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
