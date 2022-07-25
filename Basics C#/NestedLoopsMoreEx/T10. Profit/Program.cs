b83e2bf27c441fac40f6925a5157b4e7b75ebb92
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-25 10:40:17 +0300

    Added Solution 10

--- Basics C#/NestedLoopsMoreEx/T10. Profit/Program.cs
+++ Basics C#/NestedLoopsMoreEx/T10. Profit/Program.cs
@@ -6,7 +6,21 @@ namespace T10._Profit
     {
         static void Main(string[] args)
         {
-            
+            int coin1Lev = int.Parse(Console.ReadLine());
+            int coin2Lev = int.Parse(Console.ReadLine());
+            int cash5Lev = int.Parse(Console.ReadLine());
+            int profit = int.Parse(Console.ReadLine());
+
+            for (int oneLevs = 0; oneLevs <= coin1Lev; oneLevs++)
+                for(int twoLevs = 0; twoLevs <= coin2Lev; twoLevs++) 
+                    for(int fiveLevs = 0; fiveLevs <= cash5Lev; fiveLevs++)
+                    {
+                        int sum = 5 * fiveLevs + 2 * twoLevs + 1 * oneLevs;
+                        if(sum == profit)
+                        {
+                            Console.WriteLine("{0} * 1 lv. + {1} * 2 lv. + {2} * 5 lv. = {3} lv.", oneLevs, twoLevs, fiveLevs, profit);
+                        }
+                    }
         }
     }
 }
