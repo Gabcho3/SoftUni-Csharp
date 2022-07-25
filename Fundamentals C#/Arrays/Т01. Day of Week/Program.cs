58211e551d8299b765914030cfb3f65f05c36492
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-25 16:58:23 +0300

    Added Solution 01

---
+++ Fundamentals C#/Arrays/Т01. Day of Week/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Т01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dayOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            int day = int.Parse(Console.ReadLine()) - 1; //day - 1 -> ZERO is first index 
+
+            if (day >= 0 && day < dayOfWeek.Length) //length = 7, MaxIndex = 6
+                Console.WriteLine(dayOfWeek[day]);
+            else
+                Console.WriteLine("Invalid day!");
+        }
+    }
+}
