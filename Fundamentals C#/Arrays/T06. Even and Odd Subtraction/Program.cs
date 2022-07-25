b4efc4e6da43998f3995288b54d48728b2226e70
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-25 18:02:59 +0300

    Added Solution 06

---
+++ Fundamentals C#/Arrays/T06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace T06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            foreach (int num in nums)
+            {
+                if (num % 2 == 0)
+                    evenSum += num;
+                else
+                    oddSum += num;
+            }
+            int diff = evenSum - oddSum;
+            Console.WriteLine(diff);
+        }
+    }
+}
