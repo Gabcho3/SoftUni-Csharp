7f5ac78287ee53e2ae717ec4f793a8fdd60e0159
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-26 19:45:14 +0300

    Added Solution 02

---
+++ Fundamentals C#/ArraysEx/T02. Common Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace T02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input1 = Console.ReadLine().Split(" ").ToArray();
+            string[] input2 = Console.ReadLine().Split(" ").ToArray();
+
+            for(int i = 0; i < input2.Length; i++)
+            {
+                for (int j = 0; j < input1.Length; j++)
+                {
+                    if (input2[i] == input1[j])
+                    {
+                        Console.Write(input1[j] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
