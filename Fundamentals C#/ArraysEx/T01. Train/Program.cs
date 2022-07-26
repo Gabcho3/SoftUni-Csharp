0a51361eb4f020efaec331b731fcbed7a8818f58
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-26 19:37:15 +0300

    Added Solution 01

---
+++ Fundamentals C#/ArraysEx/T01. Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace T01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[int.Parse(Console.ReadLine())];
+
+            int sum = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+                sum += nums[i];
+            }
+            Console.WriteLine(string.Join(" ", nums));
+            Console.WriteLine(sum);
+        }
+    }
+}
