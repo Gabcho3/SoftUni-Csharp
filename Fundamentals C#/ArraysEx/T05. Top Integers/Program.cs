eee9d3725609962541d16ce617b040454a1ec1f6
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-27 14:31:47 +0300

    Added Solution 05

---
+++ Fundamentals C#/ArraysEx/T05. Top Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace T05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //top integer is an integer that is greater than all the elements to its right
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
+            for(int i = 0; i < nums.Length; i++)
+            {
+                bool isGreater = true;  
+
+                for(int j = i + 1; j < nums.Length; j++)//checking right side elements
+                {
+                    if (nums[i] <= nums[j])
+                    {
+                        isGreater = false;
+                        break;
+                    }
+                }
+
+                if(isGreater)
+                {
+                    Console.Write(nums[i] + " ");
+                }
+            }
+        }
+    }
+}
