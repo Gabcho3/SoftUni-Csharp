c0dcdb209fe5ecf35b8d6881d20ce04a09c78edd
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-27 14:28:56 +0300

    Added Solution 04

---
+++ Fundamentals C#/ArraysEx/T04. Array Rotation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace T04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+
+            for(int rotation = 1; rotation <= rotations; rotation++)
+            {
+                int firstNum = nums[0];
+                for(int i = 0; i < nums.Length - 1; i++) //rotation of numbers
+                {
+                    nums[i] = nums[i + 1];
+                }
+                nums[nums.Length - 1] = firstNum;
+            }
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
