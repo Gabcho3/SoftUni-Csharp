4da39bd63ed6ae85f0058104ef4b2cec231022d6
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-27 14:48:14 +0300

    Added Solution 06

---
+++ Fundamentals C#/ArraysEx/T06. Equal Sums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace T06._Equal_Sums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                    rightSum += nums[j];
+
+                for (int k = i - 1; k >= 0; k--)
+                    leftSum += nums[k];
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+                rightSum = 0;
+                leftSum = 0;
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
