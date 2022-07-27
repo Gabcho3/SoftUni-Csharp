b9634e5693d9771774c922f839c7fd24672eecba
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-07-28 00:26:21 +0300

    Added Solution 08

---
+++ Fundamentals C#/ArraysEx/T08. Magic Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace T08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int needSum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for(int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == needSum)
+                    {
+                        Console.Write(nums[i] + " " + nums[j]);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
