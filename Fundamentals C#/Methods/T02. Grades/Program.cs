af7faeb843eae6fdbf1cb856d5ee6b07ed95ff59
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-01 18:09:45 +0300

    Added Solution 02

---
+++ Fundamentals C#/Methods/T02. Grades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double garde = double.Parse(Console.ReadLine());
+            CheckGrades(garde);
+        }
+
+        static void CheckGrades(double grade)
+        {
+            if (grade < 3.00)
+                Console.WriteLine("Fail");
+
+            else if (grade < 3.50)
+                Console.WriteLine("Poor");
+
+            else if (grade < 4.50)
+                Console.WriteLine("Good");
+
+            else if (grade < 5.50)
+                Console.WriteLine("Very good");
+
+            else
+                Console.WriteLine("Excellent");
+
+        }
+    }
+}
