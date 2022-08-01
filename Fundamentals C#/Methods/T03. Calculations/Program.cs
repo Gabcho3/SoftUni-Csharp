e520607e2ed50a08d212b2e256724d9e755665db
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-01 18:18:53 +0300

    Added Solution 03

---
+++ Fundamentals C#/Methods/T03. Calculations/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace T03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string action = Console.ReadLine();
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            switch (action)
+            {
+                case "add":
+                    Add(firstNum, secondNum);
+                    break;
+                case "multiply":
+                    Multiply(firstNum, secondNum);
+                    break;
+                case "subtract":
+                    Substract(firstNum, secondNum);
+                    break;
+                case "divide":
+                    Divide(firstNum, secondNum);
+                    break;
+            }
+        }
+
+        static void Add(int firstNum, int secondNum)
+        {
+            int result = firstNum + secondNum;
+            Console.WriteLine(result);
+        }
+
+        static void Multiply(int firstNum, int secondNum)
+        {
+            int result = firstNum * secondNum;
+            Console.WriteLine(result);
+        }
+
+        static void Substract(int firstNum, int secondNum)
+        {
+            int result = firstNum - secondNum;
+            Console.WriteLine(result);
+        }
+
+        static void Divide(int firstNum, int secondNum)
+        {
+            int result = firstNum / secondNum;
+            Console.WriteLine(result);
+        }
+    }
+}
