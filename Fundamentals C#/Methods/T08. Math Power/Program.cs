6ba829110d251f71642aca013241d93b3511bdb8
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-02 00:23:36 +0300

    Added Solution 08

---
+++ Fundamentals C#/Methods/T08. Math Power/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine()); //the base
+            int power = int.Parse(Console.ReadLine());  
+
+            double result = MathPower(num, power);
+            Console.WriteLine(result);
+        }
+
+        static double MathPower(double num, int power)
+        {
+            double result = 1;
+
+            for(int i = 0; i < power; i++)
+            {
+                result *= num;
+            }
+
+            //OTHER SOLUTION
+            //result = Math.Pow(num, power);
+            
+            return result;
+        }
+    }
+}
