ed6633b564417433537cc3371388d46d3c324d04
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-01 18:03:37 +0300

    Added Solution 01

---
+++ Fundamentals C#/Methods/T01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace T01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            CheckNumber(num);
+        }
+
+        static void CheckNumber(int num)
+        {
+            if (num > 0)
+                Console.WriteLine($"The number {num} is positive.");
+
+            else if(num < 0)
+                Console.WriteLine($"The number {num} is negative.");
+
+            else
+                Console.WriteLine($"The number {num} is zero.");
+        }
+    }
+}
