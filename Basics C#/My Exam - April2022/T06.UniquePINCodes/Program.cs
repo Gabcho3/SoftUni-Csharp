7e005658ec9ec9467af366f6b374a26d074e9d8f
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-01 14:40:51 +0300

    Improved code (Solution 06)

--- Basics C#/My Exam - April2022/T06.UniquePINCodes/Program.cs
+++ Basics C#/My Exam - April2022/T06.UniquePINCodes/Program.cs
@@ -10,24 +10,15 @@ namespace T06.UniquePINCodes
             int interval2 = int.Parse(Console.ReadLine());
             int interval3 = int.Parse(Console.ReadLine());
 
-            for (int num1 = 1; num1 <= interval1; num1++)
+            for (int num1 = 2; num1 <= interval1; num1 += 2)
             {
-                if (num1 % 2 != 0)
-                {
-                    continue;
-                }
                 for (int num2 = 2; num2 <= interval2; num2++)
                 {
                     if (num2 == 4 || num2 == 6 || num2 > 7)
-                    {
                         continue;
-                    }
-                    for (int num3 = 1; num3 <= interval3; num3++)
+
+                    for (int num3 = 2; num3 <= interval3; num3 += 2)
                     {
-                        if (num3 % 2 != 0)
-                        {
-                            continue;
-                        }
                         Console.WriteLine($"{num1} {num2} {num3}");
                     }
                 }
