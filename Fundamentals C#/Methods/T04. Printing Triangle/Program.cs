a5b9f5875ac79691b9956cb3bbe5720b8e97a6c4
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-01 18:32:03 +0300

    Added Solution 04

---
+++ Fundamentals C#/Methods/T04. Printing Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            PrintWholeTrngle(input);
+        }
+
+        static void PrinLine(int start, int end)
+        {
+            for(int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintWholeTrngle(int input)
+        {
+            for(int i = 1; i <= input; i++)
+            {
+                PrinLine(1, i);
+            }
+            for (int i = input - 1; i >= 1; i--)
+            {
+                PrinLine(1, i);
+            }
+        }
+    }
+}
