af963ae4f2399c530dc9a777dc1fa0846262c8fa
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-01 14:21:46 +0300

    Changed variables' names

--- Basics C#/My Exam - April2022/T01. Cat Diet/Program.cs
+++ Basics C#/My Exam - April2022/T01. Cat Diet/Program.cs
@@ -6,18 +6,18 @@ namespace T01._Cat_Diet
     {
         static void Main(string[] args)
         {
-            double p1 = double.Parse(Console.ReadLine()) / 100;
-            double p2 = double.Parse(Console.ReadLine()) / 100;
-            double p3 = double.Parse(Console.ReadLine()) / 100;
+            double percentFats = double.Parse(Console.ReadLine()) / 100;
+            double percentProteins = double.Parse(Console.ReadLine()) / 100;
+            double percentCarbohydrates = double.Parse(Console.ReadLine()) / 100;
             double calories = double.Parse(Console.ReadLine());
             double water = double.Parse(Console.ReadLine()) / 100;
 
-            double grams1 = calories * p1 / 9;
-            double grams2 = calories * p2 / 4;
-            double grams3 = calories * p3 / 4;
-            double total1 = grams1 + grams2 + grams3;
-            double for1Meal = calories / total1;
-            double totalCalories = (1 - water) * for1Meal;
+            double gramsFats = calories * percentFats / 9;
+            double gramsProteins = calories * percentProteins / 4;
+            double gramsCarbohydrates = calories * percentCarbohydrates / 4;
+            double totalGrams = gramsFats + gramsProteins + gramsCarbohydrates;
+            double caloriesForOneGram = calories / totalGrams;
+            double totalCalories = (1 - water) * caloriesForOneGram;
             Console.WriteLine($"{totalCalories:f4}");
         }
     }
