c79287a9ad9a9b2d39daa4d610e14a907dea338d
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-01 19:03:24 +0300

    Added Solution 06

---
+++ Fundamentals C#/Methods/T06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double length = double.Parse(Console.ReadLine());
+            double area = SumRectangelArea(width, length);
+            Console.WriteLine(area);
+        }
+
+        static double SumRectangelArea(double width, double length)
+        {
+            return width * length;
+        }
+    }
+}
