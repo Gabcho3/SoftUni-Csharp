ab5472cbdabd912fe38a8da5d247c343cd166020
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-01 18:56:28 +0300

    Added Solution 05

---
+++ Fundamentals C#/Methods/Т05. Orders/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Т05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //coffeePrice = 1.50;
+            //waterPrice = 1.00;
+            //cokePrice = 1.40;
+            //snacksPrice = 2.00;
+
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            PrintPrice(product, quantity);
+        }
+
+        static void PrintPrice(string product, int quantity)
+        {
+            double totalPrice = 0;
+
+            switch(product)
+            {   
+                case "coffee":
+                    totalPrice = quantity * 1.50;
+                    break;
+                case "water":
+                    totalPrice = quantity * 1.00;
+                    break;
+                case "coke":
+                    totalPrice = quantity * 1.40;
+                    break;
+                case "snacks":
+                    totalPrice = quantity * 2.00;
+                    break;
+            }
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
