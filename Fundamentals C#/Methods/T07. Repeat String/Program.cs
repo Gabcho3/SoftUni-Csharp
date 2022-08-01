50fa60d497c72b054473b5bd59fc88bf4679ace2
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-02 00:04:57 +0300

    Added Solution 05

---
+++ Fundamentals C#/Methods/T07. Repeat String/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace T07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int repeats = int.Parse(Console.ReadLine());
+
+            string result = RepeatString(input, repeats);
+            Console.WriteLine(result);
+        }
+
+        //static string RepeatString(string input, int repeats)
+        //{
+        //    string result = String.Empty;
+
+        //    for(int i = 0; i < repeats; i++)
+        //    {
+        //        result += input;
+        //    }
+        //    return result;
+        //}
+
+        static string RepeatString(string input, int repeats)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < repeats; i++)
+            {
+                result.Append(input);
+            }
+            return result.ToString();
+        }
+    }
+}
