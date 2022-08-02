0e6cfac5d676899345b795ab78aff24b27cff167
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-02 16:03:08 +0300

    Added Solution 02

---
+++ Fundamentals C#/MethodsEx/T02. Vowels Count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            GetVowelsCount(input);
+        }
+
+        static void GetVowelsCount(string input)
+        {
+            int vowelsCount = 0;
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' ||
+                    input[i] == 'A' || input[i] == 'E' || input[i] == 'I' || input[i] == 'O' || input[i] == 'U')
+                    vowelsCount++;
+            }
+
+            Console.WriteLine(vowelsCount);
+        }
+    }
+}
