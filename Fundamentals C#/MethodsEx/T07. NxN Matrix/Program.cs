72d88de9061d5255456ae512a2d13978baa86fa4
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-03 00:29:00 +0300

    Added Solution 07

---
+++ Fundamentals C#/MethodsEx/T07. NxN Matrix/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            PrintMatrix(num);
+        }
+
+        static void PrintMatrix(int num)
+        {
+            int[] output = new int[num];
+
+            for(int i = 0; i < output.Length; i++)
+            {
+                output[i] = num;
+            }
+
+            for(int j = 0; j < num; j++)
+            {
+                Console.WriteLine(string.Join(" ", output));
+            }
+        }
+    }
+}
