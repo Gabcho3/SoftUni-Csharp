d292761c6f24d759829d673f5e9fb648247842d1
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-02 15:06:46 +0300

    Added Solution 10

---
+++ Fundamentals C#/Methods/T10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace T10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Math.Abs(int.Parse(Console.ReadLine())); //Math.Abs() --> for negative nums
+
+            int evenSum = GetSumOfEvenDigits(num);
+            int oddSum = GetSumOfOddDigits(num);
+            int result = GetMultipleOfEvenAndOdds(evenSum, oddSum);
+
+            Console.WriteLine(result);
+        }
+
+        static int GetMultipleOfEvenAndOdds(int evenSum, int oddSum)
+        {
+            return evenSum * oddSum;
+        }
+
+        static int GetSumOfEvenDigits(int num)
+        {
+            int sum = 0;
+            int digits = num;
+
+            while(digits > 0)
+            {
+                int currDigit = digits % 10;
+
+                if (currDigit % 2 == 0)
+                    sum += currDigit;
+
+                digits = digits / 10;
+            }
+
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int num)
+        {
+            int sum = 0;
+            int digits = num;
+
+            while (digits > 0)
+            {
+                int currDigit = digits % 10;
+
+                if (currDigit % 2 != 0)
+                    sum += currDigit;
+
+                digits = digits / 10;
+            }
+
+            return sum;
+        }
+    }
+}
