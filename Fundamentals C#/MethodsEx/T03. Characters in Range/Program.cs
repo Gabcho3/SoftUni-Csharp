3b505ac515b62875732bb03f86ce5d57812413cc
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-02 16:03:20 +0300

    Added Solution 03

---
+++ Fundamentals C#/MethodsEx/T03. Characters in Range/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch1 = char.Parse(Console.ReadLine());
+            char ch2 = char.Parse(Console.ReadLine());
+
+            PrintChars(ch1, ch2);
+        }
+
+        static void PrintChars(char ch1, char ch2)
+        {
+            for(int i = ch1 + 1; i < ch2; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+
+            //Swapping chars if the second letter's ASCII value is LESS than that of the first one 
+            for (int j = ch2 + 1; j < ch1; j++) 
+            {
+                Console.Write((char)j + " ");
+            }
+        }
+    }
+}
