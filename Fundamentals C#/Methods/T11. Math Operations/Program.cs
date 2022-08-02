e90e983092b14606ec994d7b2ead5d35fcad54b1
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-02 15:35:48 +0300

    Added Solution 11

---
+++ Fundamentals C#/Methods/T11. Math Operations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace T11._Math_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            switch(@operator)
+            {
+                case '/':
+                   result = Divide(num1, num2);
+                    break;
+                case '*':
+                    result = Multiply(num1, num2);
+                    break;
+                case '+':
+                    result = (Add(num1, num2));
+                    break;
+                case '-':
+                    result = Subtract(num1, num2);
+                    break;
+            }
+            Console.WriteLine(result);
+        }
+
+        static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        static double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
