2d18dbe72750eef890cd1915dd042f2b650b61a1
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-02 15:43:36 +0300

    Added Solution 01

---
+++ Fundamentals C#/MethodsEx/T01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            GetMinNum(num1, num2, num3);
+        }
+
+        static void GetMinNum(int num1, int num2, int num3)
+        {
+            int min = int.MaxValue;
+
+            if (num1 < min)
+                min = num1;
+
+            if (num2 < min)
+                min = num2;
+
+            if (num3 < min)
+                min = num3;
+
+            Console.WriteLine(min);
+        }
+    }
+}
