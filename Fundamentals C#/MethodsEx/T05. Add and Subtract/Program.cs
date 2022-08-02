3611d073851b27dc038f57ce4c53b580b1f0bbe1
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-02 23:47:43 +0300

    Added Solution 05

---
+++ Fundamentals C#/MethodsEx/T05. Add and Subtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int sum = AddTwoNums(num1, num2);
+            int result = Subtract(num3, sum);
+
+            Console.WriteLine(result);
+        }
+        static int AddTwoNums(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static int Subtract(int num3, int sum)
+        {
+            return sum - num3;
+        }
+
+    }
+}
