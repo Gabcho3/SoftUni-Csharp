4b08837d7311932633985ed8fea22255d60e73eb
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-04 14:09:15 +0300

    Added Solution 05

---
+++ Fundamentals C#/MethodsMoreEx/T05. Multiplication Sign/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace T05._Multiplication_Sign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WITHOUT multiplying the 3 numbers.
+
+            //ALL COMBINATIONS:
+            //+ + +; + - -; - - +; - + - >>> sign = positive
+            //- - -; + + -; + - +; - + + >>> sign = negative
+            //at least one 0 >>> sign = zero
+
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+
+            CheckMultiplicationSign(num1, num2, num3);
+            Console.WriteLine(CheckMultiplicationSign(num1, num2, num3));
+        }
+        static string CheckMultiplicationSign(double num1, double num2, double num3)
+        {
+            string sign = "";
+            if (num1 < 0 && num2 < 0 && num3 < 0)
+                sign = "negative";
+
+            else if (num1 > 0 && num2 > 0 && num3 > 0)
+                sign = "positive";
+
+            else if (num1 == 0 || num2 == 0 || num3 == 0)
+                sign = "zero";
+
+            else if(num1 > 0)
+            {
+                if (num2 < 0 && num3 < 0)
+                    sign = "positive";
+
+                else
+                    sign = "negative";
+            }
+
+            else if(num1 < 0)
+            {
+                if (num2 > 0 && num3 > 0)
+                    sign = "negative";
+
+                else
+                    sign = "positive";
+            }
+
+            return sign;
+        }
+    }
+}
