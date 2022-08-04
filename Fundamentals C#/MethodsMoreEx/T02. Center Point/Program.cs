92cc036e65bbb32c5374175388f6c2e33029318d
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-04 12:41:29 +0300

    Improved code (Solution 02)

--- Fundamentals C#/MethodsMoreEx/T02. Center Point/Program.cs
+++ Fundamentals C#/MethodsMoreEx/T02. Center Point/Program.cs
@@ -16,38 +16,9 @@ namespace T02._Center_Point
 
         static void PrintClosestPoint(double x1, double y1, double x2, double y2)
         {
-            int distance1 = 0;
-            int distance2 = 0;
+            double distance1 = Math.Abs(x1) + Math.Abs(y1);
+            double distance2 = Math.Abs(x2) + Math.Abs(y2);
 
-            double currX1 = Math.Abs(x1);
-            double currY1 = Math.Abs(y1);
-            double currX2 = Math.Abs(x2);
-            double currY2 = Math.Abs(y2);
-
-            while(currX1 >= 0)
-            {
-                distance1++;
-                currX1--;
-            }
-
-            while (currY1 >= 0)
-            {
-                distance1++;
-                currY1--;
-            }
-
-            while (currX2 >= 0)
-            {
-                distance2++;
-                currX2--;
-            }
-
-            while (currY2 >= 0)
-            {
-                distance2++;
-                currY2--;
-            }
-            
             Console.WriteLine(distance1 <= distance2 ? $"({x1}, {y1})" : $"({x2}, {y2})");
         }
     }
