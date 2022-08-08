cd628e2c64dd307e17e168ba9ae1ba24fe07ec19
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-08 17:00:36 +0300

    Added Solution 02

---
+++ Fundamentals C#/Lists/T02. Gauss' Trick/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            for(int i = 0; i < nums.Count / 2; i++) //avoiding center num of List if nums.Count is odd
+            {
+                nums[i] += nums[nums.Count - 1]; //adding
+
+                nums.RemoveAt(nums.Count - 1); //we added it to nums[i]
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
