376a9880744431b173d190e015936c16cee87290
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-09 22:51:36 +0300

    Added Solution 05
    Not fully working

---
+++ Fundamentals C#/ListsEx/T05. Bomb Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequense = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var bombAndPower = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for(int i = 0; i < sequense.Count; i++)
+            {
+                if(sequense[i] == bombAndPower[0])
+                {
+                    int indexOfBomb = i; 
+                    for(int j = 1; j <= bombAndPower[1]; j++)
+                    {
+                        if (indexOfBomb - j >= 0)
+                            sequense.RemoveAt(i - j);
+
+                        indexOfBomb--; //sequence.Count decrease with 1
+
+                        if (indexOfBomb + j <= sequense.Count - 1)
+                            sequense.RemoveAt(indexOfBomb + j);
+
+                    }
+                }
+            }
+            sequense.RemoveAll(num => num == bombAndPower[0]);
+
+            Console.WriteLine(sequense.Sum());
+        }
+    }
+}
