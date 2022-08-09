3184f24fcc760c39b3011cfd9552497f890bf6c3
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-09 16:42:37 +0300

    Added Solution 03

---
+++ Fundamentals C#/ListsEx/T03. House Party/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03._House_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int commands = int.Parse(Console.ReadLine());
+
+            List<string> guests = new List<string>();
+            bool isInList = false;
+
+            for(int i = 0; i < commands; i++)
+            {
+                string[] guestInfo = Console.ReadLine().Split().ToArray();
+                isInList = guests.Contains(guestInfo[0]);
+
+                if(guestInfo[2] == "going!")
+                {
+                    if (!isInList)
+                        guests.Add(guestInfo[0]);
+
+                    else
+                        Console.WriteLine(guestInfo[0] + " is already in the list!");
+                }
+                else
+                {
+                    if (isInList)
+                        guests.Remove(guestInfo[0]);
+
+                    else
+                        Console.WriteLine(guestInfo[0] + " is not in the list!");
+                }
+            }
+
+            for (int i = 0; i < guests.Count; i++)
+            {
+                Console.WriteLine(guests[i]);
+            }
+        }
+    }
+}
