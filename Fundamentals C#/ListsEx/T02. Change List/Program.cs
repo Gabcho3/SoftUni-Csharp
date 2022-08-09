1bb10e50c40f671126c6f777d5bd806ab4e8dcb8
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-09 16:25:56 +0300

    Added Solution 02

---
+++ Fundamentals C#/ListsEx/T02. Change List/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] command = Console.ReadLine().Split().ToArray();
+            string action = command[0];
+
+
+            while(action != "end")
+            {
+                switch(action)
+                {
+                    case "Delete":
+                        list.RemoveAll(n => n == int.Parse(command[1]));
+                        break;
+
+                    case "Insert":
+                        list.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                        break;
+                }
+
+                command = Console.ReadLine().Split().ToArray();
+                action = command[0];
+            }
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
