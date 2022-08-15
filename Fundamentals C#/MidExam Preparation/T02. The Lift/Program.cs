e38cba252c69b876777fbe3b1e7e9e13f03f0335
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-15 18:33:22 +0300

    Added Solution 02

---
+++ Fundamentals C#/MidExam Preparation/T02. The Lift/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace T02._The_Lift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int[] currState = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < currState.Length; i++)
+            {
+
+                while (currState[i] != 4 && people > 0)
+                {
+                    currState[i]++;
+                    people--;
+                }
+            }
+
+            if (currState[currState.Length - 1] == 4 && people == 0)
+                Console.WriteLine(string.Join(" ", currState));
+
+            else if (currState[currState.Length - 1] == 4 && people > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {people} people in a queue!");
+                Console.WriteLine(string.Join(" ", currState));
+            }
+
+            else
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", currState));
+            }
+
+        }
+    }
+}
