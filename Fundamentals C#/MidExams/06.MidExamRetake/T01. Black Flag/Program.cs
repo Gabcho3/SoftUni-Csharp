29a6f10aa68f7a0b47272c0b3144abb7528fc297
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-16 17:23:34 +0300

    Added Solution 01

---
+++ Fundamentals C#/MidExams/06.MidExamRetake/T01. Black Flag/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T01._Black_Flag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int daylyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double plunderGained = 0;
+
+            for(int day = 1; day <= days; day++)
+            {
+                plunderGained += daylyPlunder;
+
+                if (day % 3 == 0)
+                    plunderGained += daylyPlunder * 0.5;
+
+                if (day % 5 == 0)
+                    plunderGained *= 0.7;
+            }
+
+            if (plunderGained >= expectedPlunder)
+                Console.WriteLine($"Ahoy! {plunderGained:f2} plunder gained.");
+
+            else
+                Console.WriteLine($"Collected only {plunderGained / expectedPlunder * 100:f2}% of the plunder.");
+        }
+    }
+}
