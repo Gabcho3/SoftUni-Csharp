6b492df0e8531e3a9adeb0e8532907b3e94faee8
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-19 17:12:09 +0300

    Added Solution 01

---
+++ Fundamentals C#/MidExams/03. Mid Exam Retake/T01. Counter-Strike/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace T01._Counter_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            int wins = 0;
+
+            while(input != "End of battle")
+            {
+                int distance = int.Parse(input);
+
+                if (energy - distance >= 0)
+                {
+                    energy -= distance;
+                    wins++;
+
+                    if (wins % 3 == 0)
+                        energy += wins;
+                }
+
+                else
+                {
+                    Console.WriteLine($"Not enough energy! " +
+                         $"Game ends with {wins} won battles and {energy} energy");
+
+                    return;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+        }
+    }
+}
