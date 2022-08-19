fdbf71bf5c9b92d4294281eecff711caabb49493
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-19 17:25:17 +0300

    Added Solution 02

---
+++ Fundamentals C#/MidExams/03. Mid Exam Retake/T02. Shoot for the Win/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace T02._Shoot_for_the_Win
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string input = Console.ReadLine();
+
+            int shots = 0;
+
+            while(input != "End")
+            {
+                int index = int.Parse(input);
+
+                if(index < 0 || index >= targets.Length)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                for(int i = 0; i < targets.Length; i++)
+                {
+                    if (targets[i] == -1 || i == index)
+                        continue;
+
+                    if (targets[i] > targets[index])
+                        targets[i] -= targets[index];
+
+                    else
+                        targets[i] += targets[index];
+                }
+
+                targets[index] = -1;
+                shots++;
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Shot targets: {shots} -> {string.Join(" ", targets)}");
+        }
+    }
+}
