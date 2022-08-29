559bf2f9b655be67735e1722c00f3ed241c1bc9e
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-29 16:11:04 +0300

    Added Solution 01

---
+++ Fundamentals C#/MidExams/02. Mid Exam/T01. SoftUni Reception/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T01._SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int efficiency1 = int.Parse(Console.ReadLine());
+            int efficiency2 = int.Parse(Console.ReadLine());
+            int efficiency3 = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+
+            int hours = 0;
+
+            while (students > 0)
+            {
+                hours++;
+
+                if (hours % 4 == 0)
+                    hours++;
+
+                students -= efficiency1 + efficiency2 + efficiency3;
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
