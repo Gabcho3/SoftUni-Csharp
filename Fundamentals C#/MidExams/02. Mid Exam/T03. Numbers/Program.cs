14eadae4745de1515eaded4d10781818702666c8
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-08-29 16:47:39 +0300

    Added Solution 03

---
+++ Fundamentals C#/MidExams/02. Mid Exam/T03. Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03._Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            double avarage = nums.Sum() / (double)nums.Count; //(double) --> we need 1 double min int calculations
+
+            nums.RemoveAll(num => num <= avarage);
+
+            if (nums.Count == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            nums.Sort();
+            nums.Reverse();
+
+            if(nums.Count > 5)
+            {
+                int lastNum = nums[4];
+                nums.RemoveAll(num => num < lastNum);
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
