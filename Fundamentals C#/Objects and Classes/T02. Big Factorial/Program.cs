4fc55d80e42a5939165e82562d9c98e3217b38d4
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-02 19:14:33 +0300

    Added Solution 02

---
+++ Fundamentals C#/Objects and Classes/T02. Big Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace T02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+
+            for(int i = 1; i <= num; i++)
+                factorial *= i;
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
