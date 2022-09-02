7df849e62f3138f4ec69003e10702eb131ab6b20
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-02 19:06:03 +0300

    Added Solution 01

---
+++ Fundamentals C#/Objects and Classes/T01. Randomize Words/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace T01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ');
+
+            Random randomIndex = new Random();
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                int newIndex = randomIndex.Next(0, words.Length);
+
+                string currWord = words[i];
+
+                words[i] = words[newIndex];
+                words[newIndex] = currWord;
+            }
+
+            foreach(string word in words)
+                Console.WriteLine(word);
+        }
+    }
+}
