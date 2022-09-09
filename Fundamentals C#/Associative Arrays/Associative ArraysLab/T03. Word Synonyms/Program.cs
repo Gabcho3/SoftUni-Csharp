21033905b2190788fffe5fded9b8043371a86fe7
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-09 16:31:16 +0300

    Added Solution 03

---
+++ Fundamentals C#/Associative Arrays/Associative ArraysLab/T03. Word Synonyms/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace T03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var synonyms = new Dictionary<string, List<string>>();
+
+            List<string> words = new List<string>(); //to check if we get same word twice
+
+            for(int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+
+                if (words.Contains(word))
+                    synonyms[word].Add(synonym);
+
+                else
+                {
+                    synonyms[word] = new List<string>();
+                    synonyms[word].Add(synonym);
+                }
+
+                words.Add(word);
+            }
+
+            foreach(var synonym in synonyms)
+                Console.WriteLine($"{synonym.Key} - {string.Join(", ", synonym.Value)}");
+        }
+    }
+}
