31485a5d76b4dea5a7d3c1a5343657c6807c02c1
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-09 16:13:26 +0300

    Added Solution 02

---
+++ Fundamentals C#/Associative Arrays/Associative ArraysLab/T02. Odd Occurrences/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace T02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Select(w => w.ToLower()).ToArray();
+
+            var counts = new Dictionary<string, int>();
+
+
+            //CASE-INSENSITIVE!
+
+            foreach (var word in words)
+            {
+                if (counts.ContainsKey(word))
+                    counts[word]++;
+
+                else
+                    counts.Add(word, 1); 
+            }
+
+            var output = counts.Where(v => v.Value % 2 == 1);
+
+            foreach (var word in output)
+                Console.Write(word.Key + " ");
+        }
+    }
+}
