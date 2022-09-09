57f3894780d465b87b10c119ec4c96fae3d7d929
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-09 17:02:19 +0300

    Added Solution 02

---
+++ Fundamentals C#/Associative Arrays/Associative ArraysEx/T02. A Miner Task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var resourcesQuantity = new Dictionary<string, int>();
+
+            while(input != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resourcesQuantity.ContainsKey(input))
+                    resourcesQuantity[input] += quantity;
+
+                else
+                    resourcesQuantity.Add(input, quantity);
+
+                input = Console.ReadLine();
+            }
+
+            foreach(var resource in resourcesQuantity)
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+        }
+    }
+}
