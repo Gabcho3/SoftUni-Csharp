3b156fb164dd7b5c1422fdaab98d9077a66a2c55
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-09 15:51:27 +0300

    Added Solution 01

---
+++ Fundamentals C#/Associative Arrays/Associative ArraysLab/T01. Count Real Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            var counts = new SortedDictionary<double, int>();
+
+            foreach(var num in nums)
+            {
+                if (counts.ContainsKey(num))
+                    counts[num]++;
+
+                else
+                    counts[num] = 1;
+            }
+
+            foreach(var number in counts)
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+        }
+    }
+}
