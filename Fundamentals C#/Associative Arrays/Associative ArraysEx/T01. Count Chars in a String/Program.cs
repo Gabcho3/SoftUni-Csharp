aa19018d3496121dd13eb8ecf80e1b970729d986
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-09 16:50:57 +0300

    Added Solution 01

---
+++ Fundamentals C#/Associative Arrays/Associative ArraysEx/T01. Count Chars in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            var charCount = new Dictionary<char, int>();
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+
+                for(int j = 0; j < word.Length; j++)
+                {
+                    if (charCount.ContainsKey(word[j]))
+                        charCount[word[j]]++;
+
+                    else
+                        charCount[word[j]] = 1;
+                }
+            }
+
+            foreach(var ch in charCount)
+                Console.WriteLine($"{ch.Key} -> {ch.Value}");
+        }
+    }
+}
