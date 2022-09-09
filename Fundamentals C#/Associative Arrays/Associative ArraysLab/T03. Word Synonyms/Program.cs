ac7a6aed8159a4a6d88a4c15ea1124038db4b086
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-09 16:32:51 +0300

    Improved code (Solution 03)

--- Fundamentals C#/Associative Arrays/Associative ArraysLab/T03. Word Synonyms/Program.cs
+++ Fundamentals C#/Associative Arrays/Associative ArraysLab/T03. Word Synonyms/Program.cs
@@ -13,15 +13,13 @@ namespace T03._Word_Synonyms
 
             var synonyms = new Dictionary<string, List<string>>();
 
-            List<string> words = new List<string>(); //to check if we get same word twice
-
             for(int i = 0; i < n; i++)
             {
                 string word = Console.ReadLine();
                 string synonym = Console.ReadLine();
 
 
-                if (words.Contains(word))
+                if (synonyms.ContainsKey(word))
                     synonyms[word].Add(synonym);
 
                 else
@@ -29,8 +27,6 @@ namespace T03._Word_Synonyms
                     synonyms[word] = new List<string>();
                     synonyms[word].Add(synonym);
                 }
-
-                words.Add(word);
             }
 
             foreach(var synonym in synonyms)
---
+++ Fundamentals C#/Associative Arrays/Associative ArraysLab/T04. Word Filter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
