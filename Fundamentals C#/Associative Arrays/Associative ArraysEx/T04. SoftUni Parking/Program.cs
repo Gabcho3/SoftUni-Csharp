9114dc85d1134e1a4ec7d7f9a8ae4aabef06ea97
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-10 15:15:58 +0300

    Added Solution 04

---
+++ Fundamentals C#/Associative Arrays/Associative ArraysEx/T04. SoftUni Parking/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T04._SoftUni_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var userNameAndPlate = new Dictionary<string, string>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                string userName = command[1];
+
+                switch(command[0])
+                {
+                    case "register":
+                        string plateNumber = command[2];
+                        Register(userNameAndPlate, userName, plateNumber);
+                        break;
+
+                    case "unregister":
+                        Unregister(userNameAndPlate, userName);
+                        break;
+                }
+            }
+
+            foreach(var user in userNameAndPlate)
+                Console.WriteLine($"{user.Key} => {user.Value}");
+        }
+
+        static void Register(Dictionary<string, string> nameAndPlate,  string userName, string plateNumber)
+        {
+            if(!nameAndPlate.ContainsKey(userName))
+            {
+                nameAndPlate.Add(userName, plateNumber);
+                Console.WriteLine($"{userName} registered {plateNumber} successfully");
+            }
+
+            else
+                Console.WriteLine($"ERROR: already registered with plate number {plateNumber}");
+        }
+
+        static void Unregister(Dictionary<string, string> nameAndPlate, string userName)
+        {
+            if (!nameAndPlate.ContainsKey(userName))
+                Console.WriteLine($"ERROR: user {userName} not found");
+
+            else
+            {
+                nameAndPlate.Remove(userName);
+                Console.WriteLine($"{userName} unregistered successfully");
+            }
+        }
+    }
+}
