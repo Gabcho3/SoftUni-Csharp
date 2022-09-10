64650a2ba4cbe6c3ca676cc605be441723b22807
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-10 16:01:43 +0300

    Added Solution 06

---
+++ Fundamentals C#/Associative Arrays/Associative ArraysEx/T06. Student Academy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T06._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var studentsAndGrades = new Dictionary<string, List<double>>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string student = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if(studentsAndGrades.ContainsKey(student))
+                    studentsAndGrades[student].Add(grade);
+
+                else
+                {
+                    studentsAndGrades.Add(student, new List<double>());
+                    studentsAndGrades[student].Add(grade);
+                }
+            }
+
+            var goodStudentsAndGrades = studentsAndGrades.Where(g => g.Value.Average() >= 4.50);
+
+            foreach (var student in goodStudentsAndGrades)
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+        }
+    }
+}
