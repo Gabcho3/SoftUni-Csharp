e945329b688bd764dd0fffcb39960f4a93084932
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-10 16:01:20 +0300

    Added Solution 05

---
+++ Fundamentals C#/Associative Arrays/Associative ArraysEx/T05. Courses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace T05._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" : ");
+
+            var coursesAndStudents = new Dictionary<string, List<string>>();
+
+            while (input[0] != "end")
+            {
+                string courseName = input[0];
+                string studentName = input[1];
+
+                if (coursesAndStudents.ContainsKey(courseName))
+                    coursesAndStudents[courseName].Add(studentName);
+
+                else
+                { 
+                    coursesAndStudents.Add(courseName, new List<string>());
+                    coursesAndStudents[courseName].Add(studentName);
+                }
+
+                input = Console.ReadLine().Split(" : ");
+            }
+
+            foreach(var course in coursesAndStudents)
+            {
+                Console.WriteLine(course.Key + ": " + course.Value.Count);
+
+                for (int i = 0; i < course.Value.Count; i++)
+                    Console.WriteLine($"-- {course.Value[i]}");
+            }
+        }
+    }
+}
