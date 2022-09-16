56bfc7351e9747eccdbbdde96bbe12f9259f8a21
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-16 11:04:55 +0300

    Added Solution 04
    It's not ordered

---
+++ Fundamentals C#/Associative Arrays/Associative ArraysMoreEx/T04. Snowwhite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T04._Snowwhite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(" <:> ");
+
+            var dwarfsData = new Dictionary<string, Dictionary<string, int>>();
+
+            while (data[0] != "Once upon a time")
+            {
+                string name = data[0];
+                string color = data[1];
+                int physics = int.Parse(data[2]);
+
+                if(dwarfsData.ContainsKey(name) && dwarfsData[name].ContainsKey(color))
+                {
+                    if (dwarfsData[name][color] < physics)
+                        dwarfsData[name][color] = physics;
+                }
+
+                else if (dwarfsData.ContainsKey(name))
+                {
+                    dwarfsData[name].Add(color, physics);
+                }
+
+                else
+                {
+                    dwarfsData.Add(name, new Dictionary<string, int>());
+                    dwarfsData[name].Add(color, physics);
+                }
+
+                data = Console.ReadLine().Split(" <:> ");
+            }
+        }
+    }
+}
