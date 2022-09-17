c4a340a0abbf40f19da98100b990999bece143d0
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-17 17:12:51 +0300

    Added Solution 01

---
+++ Fundamentals C#/Text Processing/Text ProcessingLab/T01. Reverse Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace T01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            while(input != "end")
+            {
+                input = Console.ReadLine();
+
+                string reversed = string.Empty;
+
+                for(int i = input.Length - 1; i >= 0; i--)
+                {
+                    reversed += input[i];
+                }
+
+                Console.WriteLine($"{input} = {reversed}");
+            }
+        }
+    }
+}
