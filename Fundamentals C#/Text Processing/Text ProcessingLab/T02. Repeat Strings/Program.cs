e94a2750a530c951ab21264a50f49a84debb5f9b
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-17 17:23:08 +0300

    Added Solutuon 02

---
+++ Fundamentals C#/Text Processing/Text ProcessingLab/T02. Repeat Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace T02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string result = string.Empty;
+
+            foreach(string word in input)
+            {
+                for (int i = 0; i < word.Length; i++)
+                    result += word;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
