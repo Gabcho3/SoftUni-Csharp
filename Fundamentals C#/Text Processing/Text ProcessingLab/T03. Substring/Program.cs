a080bdd979da3e1d73f98fd3a8c50e093dbd7b43
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-17 18:15:34 +0300

    Added Solution 03

---
+++ Fundamentals C#/Text Processing/Text ProcessingLab/T03. Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = Console.ReadLine();
+            string result = Console.ReadLine();
+
+            while(result.Contains(key))
+            {
+                int index = result.IndexOf(key);
+                result = result.Remove(index, key.Length);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Fundamentals C#/Text Processing/Text ProcessingLab/T04. Text Filter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace T04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach(var banWord in bannedWords)
+            {
+                if (text.Contains(banWord))
+                {
+                    string replace = string.Empty;
+
+                    while (replace.Length != banWord.Length)
+                        replace += "*"; 
+
+                    text = text.Replace(banWord, replace);
+
+                    //text = text.Replace(banWord, new string('*', banword.Length));
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
