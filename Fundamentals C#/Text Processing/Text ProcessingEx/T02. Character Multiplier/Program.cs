a78c35fdac71ef700c6cc3103f263fe09a01cf19
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-18 15:59:17 +0300

    Added Solution 02

---
+++ Fundamentals C#/Text Processing/Text ProcessingEx/T02. Character Multiplier/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Xml;
+
+namespace T02._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split();
+
+            int sum = CharacterMultiplier(strings[0], strings[1]);
+
+            Console.WriteLine(sum);
+        }
+
+        public static int CharacterMultiplier(string str1, string str2)
+        {
+            int sum = 0;
+
+            string longest = string.Empty;
+            string shortest = string.Empty;
+
+            if (str1.Length > str2.Length || str1.Length == str2.Length)
+            {
+                longest = str1;
+                shortest = str2;
+            }
+
+            else 
+            {
+                longest = str2;
+                shortest = str1;
+            }
+
+            for(int i = 0; i < longest.Length; i++)
+            {
+                if (i > shortest.Length - 1)
+                    sum += longest[i];
+
+                else
+                    sum += longest[i] * shortest[i];
+            }
+
+            return sum;
+        }
+    }
+}
