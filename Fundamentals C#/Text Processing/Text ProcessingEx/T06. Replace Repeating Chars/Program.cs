83963dbb5f48a9ebc2c3f2a9e372867876748c27
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-18 17:59:38 +0300

    Added Solution 06

--- Fundamentals C#/Text Processing/Text ProcessingEx/T06. Replace Repeating Chars/Program.cs
+++ Fundamentals C#/Text Processing/Text ProcessingEx/T06. Replace Repeating Chars/Program.cs
@@ -6,7 +6,23 @@ namespace T06._Replace_Repeating_Chars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+
+            string result = string.Empty;
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (i == input.Length - 1)
+                {
+                    result += input[i];
+                    break;
+                }
+
+                else if (input[i] != input[i + 1])
+                    result += input[i];
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
