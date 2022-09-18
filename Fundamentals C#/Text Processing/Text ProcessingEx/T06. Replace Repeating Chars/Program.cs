e32c7a09a96d9e0236f17431716e2eb7904564ca
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-18 17:47:49 +0300

    Added Solution 05

---
+++ Fundamentals C#/Text Processing/Text ProcessingEx/T06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
