bf652985773c23584a1f6e4f90db9008be2d7bde
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-18 15:32:21 +0300

    Added Solution 01

---
+++ Fundamentals C#/Text Processing/Text ProcessingEx/T01. Valid Usernames/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            foreach(string username in usernames)
+            {
+                bool isValid = false;
+
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    foreach(char ch in username)
+                    {
+                        if (char.IsLetter(ch) || char.IsDigit(ch) || ch == '-' || ch == '_')
+                            isValid = true;
+
+                        else
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (isValid)
+                    Console.WriteLine(username);
+            }
+        }
+    }
+}
