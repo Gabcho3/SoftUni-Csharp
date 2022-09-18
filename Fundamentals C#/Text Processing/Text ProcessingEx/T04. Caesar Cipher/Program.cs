8fde1e41a9445728d2e87abb475a04c1d8e29cc8
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-18 16:34:04 +0300

    Added Solution 04

---
+++ Fundamentals C#/Text Processing/Text ProcessingEx/T04. Caesar Cipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string encrypt = string.Empty;
+
+            foreach (char ch in input)
+            {
+                int charCode = ch + 3;
+                encrypt += (char)charCode;
+            }
+
+            Console.WriteLine(encrypt);
+        }
+    }
+}
