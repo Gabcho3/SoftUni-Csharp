1719d5333e3c9f44982ec674ce80e6e0abf0f157
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-20 21:57:09 +0300

    Added Solution 01

---
+++ Fundamentals C#/Text Processing/Text ProcessingMoreEx/T01. Extract Person Information/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T01._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+
+                int startIndexOfName = input.IndexOf('@') + 1;
+                int endIndexOfName = input.IndexOf('|');
+
+                int startIndexOfAge = input.IndexOf('#') + 1;
+                int endIndexOfAge = input.IndexOf('*');
+
+                int nameLength = endIndexOfName - startIndexOfName;
+                int ageLength = endIndexOfAge - startIndexOfAge;
+
+                string name = new string(input.Substring(startIndexOfName, nameLength));
+                string age = new string(input.Substring(startIndexOfAge, ageLength));
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
---
+++ Fundamentals C#/Text Processing/Text ProcessingMoreEx/T02. Ascii Sumator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T02._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
