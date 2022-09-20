fc5e2c71600b790afa33a84d97c23e25393daecd
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-20 23:02:48 +0300

    Added Solution 03

---
+++ Fundamentals C#/Text Processing/Text ProcessingMoreEx/T03. Treasure Finder/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03._Treasure_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] key = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                string output = string.Empty;
+
+                if (input == "find")
+                    break;
+
+                int index = 0;
+
+                while (input.Length != output.Length)
+                {
+                    for (int i = 0; i < key.Length; i++)
+                    {
+                        output += (char)(input[index] - key[i]);
+
+                        index++;
+
+                        if (index > input.Length - 1)
+                            break;
+                    }
+
+                    if (index > input.Length - 1)
+                        break;
+                }
+
+                int typeIndex = output.IndexOf('&') + 1;
+                int typeLength = output.LastIndexOf('&') - typeIndex;
+
+                int corIndex = output.IndexOf('<') + 1;
+                int corLength = output.IndexOf('>') - corIndex;
+
+                string type = output.Substring(typeIndex, typeLength);
+                string coordinates = output.Substring(corIndex, corLength);
+
+                Console.WriteLine($"Found {type} at {coordinates}");
+            }
+        }
+    }
+}
