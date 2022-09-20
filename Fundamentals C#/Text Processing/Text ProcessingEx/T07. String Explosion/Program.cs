c8cd4e3b602842ae2d08c34e610dfd4707317466
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-20 21:39:41 +0300

    Added Solution 07

---
+++ Fundamentals C#/Text Processing/Text ProcessingEx/T07. String Explosion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string output = string.Empty;
+
+            int strength = 0;
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += int.Parse(input[i + 1].ToString());
+
+                    output += input[i];
+                }
+
+                else if (strength == 0)
+                    output += input[i];
+
+                else
+                    strength--;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
