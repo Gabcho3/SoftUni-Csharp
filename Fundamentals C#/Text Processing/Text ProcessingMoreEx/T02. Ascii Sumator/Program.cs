a7d53215b1872023d285810c8bac07fe6c7135a1
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-20 22:02:58 +0300

    Added Solution 02

--- Fundamentals C#/Text Processing/Text ProcessingMoreEx/T02. Ascii Sumator/Program.cs
+++ Fundamentals C#/Text Processing/Text ProcessingMoreEx/T02. Ascii Sumator/Program.cs
@@ -6,7 +6,18 @@ namespace T02._Ascii_Sumator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+
+            string str = Console.ReadLine();
+
+            int sum = 0;
+
+            foreach(char ch in str)
+                if (ch > first && ch < second)
+                    sum += ch;
+
+            Console.WriteLine(sum);
         }
     }
 }
