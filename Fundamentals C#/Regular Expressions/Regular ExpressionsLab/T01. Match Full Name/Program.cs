6765e5cd697ea6f2888ea5fc45ad56ff4a4bbb49
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-22 21:18:39 +0300

    Added Solution 01

---
+++ Fundamentals C#/Regular Expressions/Regular ExpressionsLab/T01. Match Full Name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string names = Console.ReadLine();
+
+            MatchCollection matchedNames = Regex.Matches(names, patern);
+
+            foreach (Match name in matchedNames)
+                Console.Write($"{name.Value} ");
+
+        }
+    }
+}
