4c07d34dc206495dc18962914396c5e28461ba65
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-22 21:18:50 +0300

    Added Solution 02

---
+++ Fundamentals C#/Regular Expressions/Regular ExpressionsLab/T02. Match Phone Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace T02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\+359([-|\s])2\1[0-9]{3}\1[0-9]{4}\b"; //+359 '-' or space 2 <group1> 0-9 <group1> 0-9    group1 = '-' or ' '
+
+            string phoneNumbers = Console.ReadLine();
+
+            MatchCollection validNumbers = Regex.Matches(phoneNumbers, patern);
+
+            Console.WriteLine(string.Join(", ", validNumbers));
+        }
+    }
+}
