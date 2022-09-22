c4760a545ef5ba11a9288ca6ca52703c9abc7082
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-22 23:15:24 +0300

    Added Solution 02

---
+++ Fundamentals C#/Regular Expressions/Regular ExpressionsEx/T02. Race/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace T02._Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\w";
+
+            List<string> names = Console.ReadLine().Split(", ").ToList();
+
+            string input = Console.ReadLine();
+
+            var racers = new Dictionary<string, int>();
+
+            while (input != "end of race")
+            {
+                MatchCollection match = Regex.Matches(input, patern);
+
+                string name = string.Empty;
+                int km = 0;
+
+                for(int i = 0; i < match.Count; i++)
+                {
+                    char ch = char.Parse(match[i].Value);
+
+                    if (char.IsLetter(ch))
+                        name += ch;
+
+                    else
+                        km += int.Parse(ch.ToString());
+                }
+
+                if (names.Contains(name))
+                {
+                    if (racers.ContainsKey(name))
+                        racers[name] += km;
+
+                    else
+                        racers[name] = km;
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var sortedRanking = racers.OrderByDescending(km => km.Value);
+
+            int place = 1;
+
+            foreach(var racer in sortedRanking)
+            {
+                if (place == 1)
+                    Console.WriteLine($"1st place: {racer.Key}");
+
+                else if (place == 2)
+                    Console.WriteLine($"2nd place: {racer.Key}");
+
+                else if (place == 3)
+                    Console.WriteLine($"3rd place: {racer.Key}");
+
+                else
+                    return;
+
+                place++;
+            }
+        }
+    }
+}
