85626bb41fa2df26db99d1381d6faff34b6af231
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-22 22:06:16 +0300

    Added Solution 01

---
+++ Fundamentals C#/Regular Expressions/Regular ExpressionsEx/T01. Furniture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace T01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @">>(?<name>[A-z]+)<<(?<price>\d+[.]?[0-9]+)!(?<quantity>\d+)\b";
+
+            string input = Console.ReadLine();
+
+            List<string> furnitures = new List<string>();
+
+            double sum = 0;
+
+            while(input != "Purchase")
+            {
+                Regex regex = new Regex(patern);
+                Match valid = regex.Match(input);
+
+                if (valid.Success)
+                {
+                    var name = valid.Groups["name"].Value;
+                    var price = double.Parse(valid.Groups["price"].Value) * double.Parse(valid.Groups["quantity"].Value);
+
+                    furnitures.Add(name);
+
+                    sum += price;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bought furniture:");
+
+            foreach (var furniture in furnitures)
+                Console.WriteLine(furniture);
+
+            Console.WriteLine($"Total money spend: {sum:f2}");
+        }
+    }
+}
