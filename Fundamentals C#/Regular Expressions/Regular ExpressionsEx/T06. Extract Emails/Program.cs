6d89d44ceeb9b9eba76b9fda040babc46edea998
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-23 21:41:16 +0300

    Added Solution 06

---
+++ Fundamentals C#/Regular Expressions/Regular ExpressionsEx/T06. Extract Emails/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T06._Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"(?<user>^[A-Za-z\d]+[.\-_]?[A-Za-z\d]*)@(?<host>[A-z]+[-]?[a-z]+[.]{1}[a-z]+[.]?[a-z]+)";
+
+            string[] input = Console.ReadLine().Split();
+
+            foreach(string s in input)
+            {
+                var match = Regex.Match(s, patern);
+
+                if (match.Success)
+                    Console.WriteLine(match);
+            }
+        }
+    }
+}
