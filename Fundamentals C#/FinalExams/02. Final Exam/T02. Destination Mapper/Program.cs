427bbe3ab406994718130161e61c2cfe28ac5947
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-09-29 23:40:06 +0300

    Added Solution 02

---
+++ Fundamentals C#/FinalExams/02. Final Exam/T02. Destination Mapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace T02._Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"([=]|[/])(?<place>[A-Z][A-Za-z]+)\1";
+            var matches = Regex.Matches(Console.ReadLine(), patern);
+
+            List<string> destinations = new List<string>();
+            int points = 0;
+
+            foreach(Match match in matches)
+                if (match.Groups["place"].Value.Length >= 3)
+                { 
+                    destinations.Add(match.Groups["place"].Value);
+
+                    points += match.Groups["place"].Value.Length;
+                }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {points}");
+        }
+    }
+}
