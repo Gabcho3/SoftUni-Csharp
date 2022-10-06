4820df8bd264676df452674004e4f853333ebaf8
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-07 00:11:55 +0300

    Added Solution 04

---
+++ Fundamentals C#/Archive/Intro and Basic Syntax/Intro and Basic SyntaxEx/T04. Labels/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T04._Labels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int sugar = int.Parse(Console.ReadLine());
+
+            double kcal = (energy / 100.0) * volume;
+            double gramsSugar = (sugar / 100.0) * volume;
+
+            Console.WriteLine($"{volume}ml {name}:\n{kcal:f2}kcal, {gramsSugar:f2}g sugars");
+        }
+    }
+}
