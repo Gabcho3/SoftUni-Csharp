ff885ccbdd06e185dbe62b527a7dc9bd6ce97bec
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-07 00:02:18 +0300

    Added Solution 02

---
+++ Fundamentals C#/Archive/Intro and Basic Syntax/Intro and Basic SyntaxEx/T02. Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace T02._Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a * b:f2}");
+        }
+    }
+}
