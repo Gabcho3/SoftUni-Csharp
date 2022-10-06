da13a3232f951353ac86f3d4111f68861d019406
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-06 23:42:30 +0300

    Added Solution 02

---
+++ Fundamentals C#/Archive/Intro and Basic SyntaxLab/T02. Add Two Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace T02._Add_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int sum = firstNum + secondNum;
+            Console.WriteLine("{0} + {1} = {2}", firstNum, secondNum, sum);
+        }
+    }
+}
