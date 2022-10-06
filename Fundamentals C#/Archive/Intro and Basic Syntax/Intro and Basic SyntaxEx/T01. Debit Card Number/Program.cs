bf90bf24648c9f1e1d2b5ec92ea5ccad9721a267
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-06 23:56:20 +0300

    Added Solution 01

---
+++ Fundamentals C#/Archive/Intro and Basic Syntax/Intro and Basic SyntaxEx/T01. Debit Card Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T01._Debit_Card_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1:d4} {num2:d4} {num3:d4} {num4:d4}");
+        }
+    }
+}
