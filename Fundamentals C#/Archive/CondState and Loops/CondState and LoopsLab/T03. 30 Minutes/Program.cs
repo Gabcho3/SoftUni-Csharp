7272893c5e79c0fb0c01c91a2e50d521debee300
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-08 17:25:11 +0300

    Added Solution 03

---
+++ Fundamentals C#/Archive/CondState and Loops/CondState and LoopsLab/T03. 30 Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T03._30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine()) + 30;
+
+            if(min > 60)
+            {
+                min -= 60;
+                hour++;
+
+                if (hour == 24)
+                    hour = 0;
+            }
+
+            Console.WriteLine($"{hour}:{min:d2}");
+        }
+    }
+}
