9520b51d40e98df38e383b1e9b69f096a51bf531
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-25 22:12:56 +0300

    Added Solution 03

---
+++ C# Advanced/Stacks and Queus/Stacks and Queues - Lab/T03. Simple Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T03._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            List<char> expressions = new List<char>();
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                if(i % 2 != 0)
+                    expressions.Add(char.Parse(input[i]));
+
+                else
+                    stack.Push(input[i]);
+            }
+
+            expressions.Reverse();
+            int result = int.Parse(stack.Pop());
+
+            foreach (char c in expressions)
+            {
+                if (c == '+')
+                    result += int.Parse(stack.Pop());
+
+                else
+                    result -= int.Parse(stack.Pop());
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
