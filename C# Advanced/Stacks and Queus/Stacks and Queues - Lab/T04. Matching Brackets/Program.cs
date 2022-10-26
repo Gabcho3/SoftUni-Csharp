2e423c2ef7532c2ec02045704cce43a3177e024c
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-26 22:43:13 +0300

    Added Solution 04

---
+++ C# Advanced/Stacks and Queus/Stacks and Queues - Lab/T04. Matching Brackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace T04._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+
+            int openIndex = 0;
+            int closeIndex = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                    stack.Push(i);
+
+                if (input[i] == ')')
+                {
+                    openIndex = stack.Pop();
+                    Console.WriteLine(input.Substring(openIndex, i - openIndex + 1));
+                }
+            }
+        }
+    }
+}
