85a2eea5767866679f8133494aab98a67dd449eb
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-26 23:04:14 +0300

    Added Solution 06

---
+++ C# Advanced/Stacks and Queus/Stacks and Queues - Lab/T06. Supermarket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T06._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Queue<string> queue = new Queue<string>();
+
+            while (input != "End")
+            {
+                if(input == "Paid")
+                    while(queue.Count > 0)
+                        Console.WriteLine(queue.Dequeue());
+
+                else
+                    queue.Enqueue(input);
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(queue.Count + " people remaining.");
+        }
+    }
+}
