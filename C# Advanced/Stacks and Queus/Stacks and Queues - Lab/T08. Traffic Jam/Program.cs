1e0d0ea3c9910acb6a955518ee2b974c8697db99
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-28 00:17:55 +0300

    Added Solution 08

---
+++ C# Advanced/Stacks and Queus/Stacks and Queues - Lab/T08. Traffic Jam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T08._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int canPass = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            Queue<string> queue = new Queue<string>();
+
+            int totalPassed = 0;
+
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    int currPassed = 0;
+                    while (currPassed < canPass && queue.Count > 0)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        currPassed++;
+                    }
+
+                    totalPassed += currPassed;
+                }
+                
+                else
+                    queue.Enqueue(input);
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{totalPassed} cars passed the crossroads.");
+        }
+    }
+}
