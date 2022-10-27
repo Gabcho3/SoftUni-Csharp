f6df40c81fca3bfb7883328f8001492afcd046cb
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-27 23:59:15 +0300

    Added Solution 07

---
+++ C# Advanced/Stacks and Queus/Stacks and Queues - Lab/T07. Hot Potato/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T07._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] players = Console.ReadLine().Split();
+            Queue<string> queue = new Queue<string>(players);
+
+            int n = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            while(queue.Count > 1)
+            {
+                counter++;
+
+                string currPlayer = queue.Dequeue();
+
+                if (counter == n)
+                {
+                    Console.WriteLine($"Removed {currPlayer}");
+                    counter = 0;
+                }
+
+                else
+                    queue.Enqueue(currPlayer);
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
