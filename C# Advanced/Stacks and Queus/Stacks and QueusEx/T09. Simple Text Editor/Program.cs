db29e9412ae0678b1c9eb86fe6891bbdd0b25ccf
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-29 14:57:13 +0300

    Added Solution 09

---
+++ C# Advanced/Stacks and Queus/Stacks and QueusEx/T09. Simple Text Editor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<string> stack = new Stack<string>();
+
+            string str = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                switch (command[0])
+                {
+                    case "1":
+                        str += command[1];
+                        stack.Push(str);
+                        break;
+
+                    case "2":
+                        int count = int.Parse(command[1]);
+                        str = str.Remove(str.Length - count, count);
+                        stack.Push(str);
+                        break;
+
+                    case "3":
+                        int index = int.Parse(command[1]) - 1;
+                        Console.WriteLine(str[index]);
+                        break;
+
+                    case "4":
+                        string currString = str;
+
+                        if (stack.Count == 0)
+                            str = "";
+
+                        else
+                            while (str == currString)
+                            {
+                                if (str != stack.Peek())
+                                    str = stack.Pop();
+
+                                else
+                                    stack.Pop();
+                            }
+                        break;
+                }
+            }
+        }
+    }
+}
