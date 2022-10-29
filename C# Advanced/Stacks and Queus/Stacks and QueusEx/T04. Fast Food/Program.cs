36e51eb5b20196313efa87a4460798e895d3c8a4
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-29 13:30:12 +0300

    Added Solution 04

---
+++ C# Advanced/Stacks and Queus/Stacks and QueusEx/T04. Fast Food/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(input);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (food - input[i] >= 0)
+                    food -= queue.Dequeue();
+
+                else
+                    break;
+            }
+
+            Console.WriteLine(input.Max());
+
+            string output = queue.Count == 0 ? "Orders complete" : $"Orders left: {string.Join(" ", queue)}";
+            Console.WriteLine(output);
+        }
+    }
+}
