6ab5754a83c279c3a5d87b7be889ca9ae681e212
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-29 20:17:44 +0300

    Optimized and Fixed code (Solution 03)

--- Fundamentals C#/FinalExams/02. Final Exam/T03. Plant Discovery/Program.cs
+++ Fundamentals C#/FinalExams/02. Final Exam/T03. Plant Discovery/Program.cs
@@ -10,127 +10,74 @@ namespace T03._Plant_Discovery
         {
             int n = int.Parse(Console.ReadLine());
 
-            List<Plant> plants = new List<Plant>();
+            var plants = new Dictionary<string, Plant>(); //Name, Rarity && Rating
 
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
-                string[] input = Console.ReadLine().Split("<->", StringSplitOptions.RemoveEmptyEntries);
+                string[] data = Console.ReadLine().Split("<->");
 
-                bool found = false;
+                string name = data[0];
+                int rarity = int.Parse(data[1]);
 
-                foreach(Plant plant in plants)
-                    if(plant.Name == input[0])
-                    {
-                        plant.Rarity = int.Parse(input[1]);
+                Plant plant = new Plant() { Rarity = rarity, Rating = new List<int>() };
 
-                        found = true;
-                    }
-
-                if(!found)
-                {
-                    Plant plant = new Plant { Name = input[0], Rarity = double.Parse(input[1]), Ratings = new List<double>() };
-                    plants.Add(plant);
-                }
+                plants[name] = plant;
             }
 
-            string[] command = Console.ReadLine().Split(": ", StringSplitOptions.RemoveEmptyEntries);
+            string[] command = Console.ReadLine().Split(':');
 
             while (command[0] != "Exhibition")
             {
                 string[] data = command[1].Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+                string plantName = data[0].Remove(0, 1);
 
-                switch (command[0])
+                if (!plants.ContainsKey(plantName))
+                    Console.WriteLine("error");
+
+                else
                 {
-                    case "Rate":
-                        Rate(plants, data);
-                        break;
+                    switch (command[0])
+                    {
+                        case "Rate":
+                            int rating = int.Parse(data[1]);
+                            plants[plantName].Rating.Add(rating);
+                            break;
 
-                    case "Update":
-                        Update(plants, data);
-                        break;
+                        case "Update":
+                            int rarity = int.Parse(data[1]);
+                            plants[plantName].Rarity = rarity;
+                            break;
 
-                    case "Reset":
-                        Reset(plants, data);
-                        break;
+                        case "Reset":
+                            plants[plantName].Rating = new List<int>();
+                            break;
+                    }
                 }
 
-                command = Console.ReadLine().Split(": ", StringSplitOptions.RemoveEmptyEntries);
+                command = Console.ReadLine().Split(':');
             }
 
             Console.WriteLine("Plants for the exhibition:");
 
-            foreach(Plant plant in plants)
+            foreach (var plant in plants)
             {
-                if (plant.Ratings.Count == 0)
-                    plant.Ratings.Add(0);
+                double averageRating = 0;
 
-                Console.WriteLine($"- {plant.Name}; Rarity: {plant.Rarity}; Rating: {plant.Ratings.Average():f2}");
+                if (plant.Value.Rating.Count > 0)
+                    averageRating = plant.Value.Rating.Average();
+
+                Console.WriteLine($"- {plant.Key}; " +
+                                  $"Rarity: {plant.Value.Rarity}; " +
+                                  $"Rating: {averageRating:f2}");
             }
-        }
 
-        static void Rate(List<Plant> plants, string[] data)
-        {
-            bool found = false;
-
-            for (int i = 0; i < plants.Count; i++)
-                if (plants[i].Name == data[0])
-                {
-                    plants[i].Ratings.Add(double.Parse(data[1]));
-
-                    found = true;
-
-                    break;
-                }
-
-            if (!found)
-                Console.WriteLine("error");
-        }
-
-        static void Update(List<Plant> plants, string[] data)
-        {
-            bool found = false;
-
-            for (int i = 0; i < plants.Count; i++)
-                if (plants[i].Name == data[0])
-                {
-                    plants[i].Rarity = double.Parse(data[1]);
-
-                    found = true;
-
-                    break;
-                }
-
-            if (!found)
-                Console.WriteLine("error");
-        }
-
-        static void Reset(List<Plant> plants, string[] data)
-        {
-            bool found = false;
-
-            for (int i = 0; i < plants.Count; i++)
-                if (plants[i].Name == data[0])
-                {
-                    plants[i].Ratings = new List<double>();
-
-                    plants[i].Ratings.Add(0);
-
-                    found = true;
-
-                    break;
-                }
-
-            if (!found)
-                Console.WriteLine("error");
         }
     }
 
     class Plant
     {
-        public string Name { get; set; }
+        public int Rarity { get; set; }
 
-        public double Rarity { get; set; }
-
-        public List<double> Ratings { get; set; }
+        public List<int> Rating { get; set; }
     }
 }
