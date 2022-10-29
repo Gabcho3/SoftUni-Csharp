a86fdc9062dc75c3e73d0987a16ea4b95baaef2a
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-29 13:47:01 +0300

    Added Solution 05

---
+++ C# Advanced/Stacks and Queus/Stacks and QueusEx/T05. Fashion Boutique/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>(clothes);
+
+            int sum = 0;
+            int racks = 0;
+
+            while (true)
+            {
+                if (stack.Count == 0)
+                    break;
+
+                while (sum + stack.Peek() <= capacity)
+                {
+                    sum += stack.Pop();
+
+                    if (stack.Count == 0)
+                        break;
+                }
+
+                racks++;
+                sum = 0;
+            }
+            
+            Console.WriteLine(racks);
+        }
+    }
+}
