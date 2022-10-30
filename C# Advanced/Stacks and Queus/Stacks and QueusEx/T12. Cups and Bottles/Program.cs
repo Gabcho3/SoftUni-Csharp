6272602b756b0fec46b7aec497221f4a8e01dfad
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-30 21:25:18 +0200

    Fixed typo

--- C# Advanced/Stacks and Queus/Stacks and QueusEx/T12. Cups and Bottles/Program.cs
+++ C# Advanced/Stacks and Queus/Stacks and QueusEx/T12. Cups and Bottles/Program.cs
@@ -9,23 +9,23 @@ namespace T12._Cups_and_Bottles
         static void Main(string[] args)
         {
             List<int> cups = new List<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
-            Stack<int> botlles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> bottles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
 
             int wasted = 0;
 
-            while (cups.Count > 0 && botlles.Count > 0)
+            while (cups.Count > 0 && bottles.Count > 0)
             {
-                int currBotlle = botlles.Peek();
+                int currBottle = bottles.Peek();
 
-                if (currBotlle >= cups[0])
+                if (currBottle >= cups[0])
                 {
-                    wasted += botlles.Pop() - cups[0];
+                    wasted += bottles.Pop() - cups[0];
                     cups.RemoveAt(0);
                 }
 
                 else
                 {
-                    cups[0] -= botlles.Pop();
+                    cups[0] -= bottles.Pop();
 
                     if (cups[0] <= 0)
                         cups.RemoveAt(0);
@@ -33,9 +33,9 @@ namespace T12._Cups_and_Bottles
             }
 
             if (cups.Count == 0)
-                Console.WriteLine($"Botlles: {string.Join(" ", botlles)}");
+                Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
 
-            if (botlles.Count == 0)
+            if (bottles.Count == 0)
                 Console.WriteLine($"Cups: {string.Join(" ", cups)}");
 
             Console.WriteLine($"Wasted litters of water: {wasted}");
