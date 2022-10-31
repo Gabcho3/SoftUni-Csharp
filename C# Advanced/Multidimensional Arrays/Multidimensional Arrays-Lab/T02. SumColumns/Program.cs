cdcef3b920f40e9018f359a5a2466ee0a296f8bd
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-01 00:42:28 +0200

    Added Solution 02

---
+++ C# Advanced/Multidimensional Arrays/Multidimensional Arrays-Lab/T02. SumColumns/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace T02._SumColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                    matrix[row, col] = data[col];
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                    sum += matrix[row, col];
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
