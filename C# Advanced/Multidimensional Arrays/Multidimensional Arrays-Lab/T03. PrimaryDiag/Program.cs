6d37d3cfffc18b7b7765df939e226c1d9e31d87f
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-01 00:51:44 +0200

    Added Solution 03

---
+++ C# Advanced/Multidimensional Arrays/Multidimensional Arrays-Lab/T03. PrimaryDiag/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace T03._PrimaryDiag_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+                matrix[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+            for(int row = 0; row < matrix.Length; row++)
+                for(int col = 0; col < matrix[row].Length; col++)
+                    if(row == col)
+                        sum += matrix[row][col];
+
+            Console.WriteLine(sum);
+        }
+    }
+}
