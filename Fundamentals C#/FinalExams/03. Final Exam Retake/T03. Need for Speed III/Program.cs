8ead1eff253b3be241c6db56a8e733ebb54e0c21
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-10-31 19:00:19 +0200

    Added improved code (Solution 03)

--- Fundamentals C#/FinalExams/03. Final Exam Retake/T03. Need for Speed III/Program.cs
+++ Fundamentals C#/FinalExams/03. Final Exam Retake/T03. Need for Speed III/Program.cs
@@ -10,20 +10,19 @@ namespace T03._Need_for_Speed_III
         {
             int n = int.Parse(Console.ReadLine());
 
-            List<Car> cars = new List<Car>();
+            var cars = new Dictionary<string, Car>(); //Name, Mileage && Fuel
 
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 string[] data = Console.ReadLine().Split('|');
 
-                Car car = new Car() 
-                { 
-                    Name = data[0], 
-                    Distance = int.Parse(data[1]),
-                    Fuel = int.Parse(data[2]) 
+                Car car = new Car()
+                {
+                    Mileage = int.Parse(data[1]),
+                    Fuel = int.Parse(data[2])
                 };
 
-                cars.Add(car);
+                cars[data[0]] = car;
             }
 
             string[] command = Console.ReadLine().Split(" : ");
@@ -35,107 +34,80 @@ namespace T03._Need_for_Speed_III
                 switch (command[0])
                 {
                     case "Drive":
-                        cars = Drive(cars, command, car);
+                        int distance = int.Parse(command[2]);
+                        int fuel = int.Parse(command[3]);
+                        Drive(cars, car, distance, fuel);
                         break;
 
                     case "Refuel":
-                        cars = Refuel(cars, command, car);
+                        fuel = int.Parse(command[2]);
+                        Refuel(cars, car, fuel);
                         break;
 
                     case "Revert":
-                        cars = Revert(cars, command, car);
+                        int kms = int.Parse(command[2]);
+                        Revert(cars, car, kms);
                         break;
                 }
+
 
                 command = Console.ReadLine().Split(" : ");
             }
 
             foreach (var car in cars)
-                Console.WriteLine($"{car.Name} -> Mileage: {car.Distance} kms, Fuel in the tank: {car.Fuel} lt.");
+                Console.WriteLine(
+                    $"{car.Key} -> Mileage: {car.Value.Mileage} kms, Fuel in the tank: {car.Value.Fuel} lt.");
         }
 
-        static List<Car> Drive (List<Car> cars, string[] command, string car)
+        static void Drive(Dictionary<string, Car> cars, string car, int distance, int fuel)
         {
-            int distance = int.Parse(command[2]);
-            int fuel = int.Parse(command[3]);
+            if (cars[car].Fuel - fuel < 0)
+                Console.WriteLine("Not enough fuel to make that ride");
 
-            for (int i = 0; i < cars.Count; i++)
+            else
             {
-                if (cars[i].Name == car)
+                cars[car].Mileage += distance;
+                cars[car].Fuel -= fuel;
+                Console.WriteLine($"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+
+                if (cars[car].Mileage >= 100_000)
                 {
-                    if (cars[i].Fuel < fuel)
-                        Console.WriteLine("Not enough fuel to make that ride");
-
-                    else
-                    {
-                        cars[i].Distance += distance;
-                        cars[i].Fuel -= fuel;
-
-                        Console.WriteLine($"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
-                    }
-
-                    if (cars[i].Distance > 100_000)
-                    {
-                        Console.WriteLine($"Time to sell the {car}!");
-                        cars.RemoveAt(i);
-                    }
+                    Console.WriteLine($"Time to sell the {car}!");
+                    cars.Remove(car);
                 }
-
             }
-
-            return cars;
         }
 
-        static List<Car> Refuel(List<Car> cars, string[] command, string car)
+        static void Refuel(Dictionary<string, Car> cars, string car, int fuel)
         {
-            int fuel = int.Parse(command[2]);
-            int refuel = 0;
-
-            for (int i = 0; i < cars.Count; i++)
+            int refueled = 0;
+            while (cars[car].Fuel < 75 && fuel > 0)
             {
-                if (cars[i].Name == car)
-                {
-                    while (cars[i].Fuel < 75 && refuel < fuel)
-                    {
-                        cars[i].Fuel++;
-                        refuel++;
-                    }
-
-                    Console.WriteLine($"{car} refueled with {refuel} liters");
-                }
+                cars[car].Fuel++;
+                fuel--;
+                refueled++;
             }
 
-            return cars;
+            Console.WriteLine($"{car} refueled with {refueled} liters");
         }
 
-        static List<Car> Revert(List<Car> cars, string[] command, string car)
+        static void Revert(Dictionary<string, Car> cars, string car, int kms)
         {
-            int km = int.Parse(command[2]);
+            cars[car].Mileage -= kms;
 
-            for (int i = 0; i < cars.Count; i++)
-            {
-                if (cars[i].Name == car)
-                {
-                    cars[i].Distance -= km;
+            if (cars[car].Mileage < 10_000)
+                cars[car].Mileage = 10_000;
 
-                    if (cars[i].Distance >= 10_000)
-                        Console.WriteLine($"{car} mileage decreased by {km} kilometers");
-
-                    else
-                        cars[i].Distance = 10_000;
-                }
-            }
-
-            return cars;
+            else
+                Console.WriteLine($"{car} mileage decreased by {kms} kilometers");
         }
     }
 
     class Car
     {
-        public string Name { get; set; }
-
-        public int Distance { get; set; }
+        public int Mileage { get; set; }
 
         public int Fuel { get; set; }
     }
 }
+
