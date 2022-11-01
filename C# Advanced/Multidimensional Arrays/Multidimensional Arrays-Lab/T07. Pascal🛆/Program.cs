25991b5b6954b3599b6a555cb57feba051911961
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-01 13:00:50 +0200

    Added Solution 07

---
+++ C# Advanced/Multidimensional Arrays/Multidimensional Arrays-Lab/T07. Pascal🛆/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T07._Pascal_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                triangle[row] = new long[row + 1];
+                for (int col = 0; col <= row; col++)
+                {
+                    if (col == 0)
+                        triangle[row][col] = 1;
+
+                    else if (col == row)
+                        triangle[row][col] = 1;
+
+                    else
+                    {
+                        triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col];
+                    }
+                }
+            }
+
+            foreach (var col in triangle)
+                Console.WriteLine(string.Join(" ", col));
+        }
+    }
+}
