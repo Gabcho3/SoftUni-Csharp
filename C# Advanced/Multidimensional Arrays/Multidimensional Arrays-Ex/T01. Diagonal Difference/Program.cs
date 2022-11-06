622def5b2f7b6dbbe1c9245ee960609523943467
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-06 19:26:13 +0200

    Added Solution 01

---
+++ C# Advanced/Multidimensional Arrays/Multidimensional Arrays-Ex/T01. Diagonal Difference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace T01._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[n][];
+
+            for(int i = 0; i < n; i++)
+                matrix[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int toRightSum = 0;
+            int col = 0;
+            for (int row = 0; row < n; row++)
+            {
+                toRightSum += matrix[row][col];
+                col++;
+            }
+
+            int toLeftSum = 0;
+            col = n - 1;
+            for (int row = 0; row < n; row++)
+            {
+                toLeftSum += matrix[row][col];
+                col--;
+            }
+
+            Console.WriteLine(Math.Abs(toRightSum - toLeftSum));
+        }
+    }
+}
