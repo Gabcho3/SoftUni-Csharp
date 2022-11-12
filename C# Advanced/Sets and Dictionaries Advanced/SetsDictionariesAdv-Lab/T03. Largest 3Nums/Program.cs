56ee98a58a7dd24984c114304b81b7f18a897433
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-12 23:43:13 +0200

    Added Solution 03

---
+++ C# Advanced/Sets and Dictionaries Advanced/SetsDictionariesAdv-Lab/T03. Largest 3Nums/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03._Largest_3Nums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            list = list.OrderByDescending(x => x).ToList();
+            int count = 0;
+            foreach (int i in list)
+            {
+                count++;
+                if (count <= 3)
+                    Console.Write(i + " ");
+            }
+        }
+    }
+}
