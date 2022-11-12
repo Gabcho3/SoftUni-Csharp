8b6a5384c88a0318e818933a73beab5f97c98e47
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-12 23:15:20 +0200

    Added Solution 01

---
+++ C# Advanced/Sets and Dictionaries Advanced/SetsDictionariesAdv-Lab/T01. Count Same Values/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01._Count_Same_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            var numsInfo = new Dictionary<double, int>(); //num, times
+
+            foreach (var number in nums)
+            {
+                if(numsInfo.ContainsKey(number))
+                {
+                    numsInfo[number] += 1;
+                }
+
+                else
+                {
+                    numsInfo[number] = 1;
+                }
+            }
+
+            foreach(var (num, times) in numsInfo)
+            {
+                Console.WriteLine($"{num} - {times} times");
+            }
+        }
+    }
+}
