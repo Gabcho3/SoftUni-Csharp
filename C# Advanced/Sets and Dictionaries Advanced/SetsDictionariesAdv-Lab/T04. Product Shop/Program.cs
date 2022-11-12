64cc20752ccb7f50215dabc80e4090782134da0b
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-13 00:04:42 +0200

    Added Solution 04

---
+++ C# Advanced/Sets and Dictionaries Advanced/SetsDictionariesAdv-Lab/T04. Product Shop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T04._Product_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var shops = new SortedDictionary<string, Dictionary<string, double>>();
+            string input;
+            while((input = Console.ReadLine()) != "Revision")
+            {
+                string[] data = input.Split(", ");
+                string shop = data[0];
+                string product = data[1];
+                double price = double.Parse(data[2]);
+
+                if(shops.ContainsKey(shop))
+                {
+                    shops[shop][product] = price;
+                }
+
+                else
+                {
+                    shops[shop] = new Dictionary<string, double>();
+                    shops[shop].Add(product, price);
+                }
+            }
+
+            foreach(var (shop, products) in shops)
+            {
+                Console.WriteLine(shop + "->");
+                foreach(var product in products)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
