6e0e9b6eb3622e92db0d2714eb6c2230210008a1
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-14 00:52:51 +0200

    Added Solution 04

---
+++ C# Advanced/Sets and Dictionaries Advanced/SetsDictionariesAdv-Ex/T04. Even Times/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var nums = new Dictionary<int, int>(); //num, count
+
+            for(int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if(!nums.ContainsKey(num))
+                    nums.Add(num, 0);
+
+                nums[num]++;
+            }
+
+            nums = nums.Where(x => x.Value % 2 == 0).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var num in nums)
+                Console.WriteLine(num.Key);
+        }
+    }
+}
