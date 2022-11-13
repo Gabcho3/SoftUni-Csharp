4b8185629a22fc1e06252b61dbbb40ad00b7bc17
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-13 13:45:28 +0200

    Added Solution 06

---
+++ C# Advanced/Sets and Dictionaries Advanced/SetsDictionariesAdv-Lab/T06. Record Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T06._Record_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int names = int.Parse(Console.ReadLine());
+            var uniqueNames = new HashSet<string>();
+            for(int i = 0; i < names; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+
+            foreach (var name in uniqueNames)
+                Console.WriteLine(name);
+        }
+    }
+}
