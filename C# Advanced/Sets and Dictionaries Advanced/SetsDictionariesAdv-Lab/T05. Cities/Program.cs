d24085b192f173437fffc83425692fa992df5b2d
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-13 13:40:10 +0200

    Added Solution 05

---
+++ C# Advanced/Sets and Dictionaries Advanced/SetsDictionariesAdv-Lab/T05. Cities/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T05._Cities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cities = int.Parse(Console.ReadLine());
+            var citiesInfo = new Dictionary<string, Dictionary<string, List<string>>>(); //continent, [country, city]
+            for(int i = 0; i < cities; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+                string continent = data[0];
+                string country = data[1];
+                string city = data[2];
+
+                if (citiesInfo.ContainsKey(continent))
+                {
+                    if (citiesInfo[continent].ContainsKey(country))
+                    {
+                        citiesInfo[continent][country].Add(city);
+                    }
+                    
+                    else
+                    {
+                        citiesInfo[continent][country] = new List<string> { city };
+                    }
+                }
+
+                else
+                {
+                    citiesInfo.Add(continent, new Dictionary<string, List<string>>());
+                    citiesInfo[continent].Add(country, new List<string>() { city });
+                }
+            }
+
+            foreach(var (continent, countries) in citiesInfo)
+            {
+                Console.WriteLine(continent + ":");
+                foreach(var country in countries)
+                {
+                    Console.WriteLine($"  {country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
