1ec9e3b34d3778880a486362687378e07326eec2
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-14 00:47:25 +0200

    Added Solution 03

---
+++ C# Advanced/Sets and Dictionaries Advanced/SetsDictionariesAdv-Ex/T03. Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var elements = new HashSet<string>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                for(int j = 0; j < input.Length; j++)
+                {
+                    elements.Add(input[j]);
+                }
+            }
+
+            elements = elements.OrderBy(e => e).ToHashSet();
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
