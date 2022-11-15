9be90e766f68f2dd97f83a83abfb4a08cbb9fe10
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-15 20:06:40 +0200

    Added Solution 06

---
+++ C# Advanced/Sets and Dictionaries Advanced/SetsDictionariesAdv-Ex/T06. Wardrobe/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T06._Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>(); //color, [item, count]
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(" -> ");
+                string color = data[0];
+                string[] clothes = data[1].Split(',');
+
+                if (!wardrobe.ContainsKey(color))
+                    wardrobe.Add(color, new Dictionary<string, int>());
+
+                foreach (var item in clothes)
+                {
+                    if (wardrobe[color].ContainsKey(item))
+                        wardrobe[color][item]++;
+
+                    else
+                        wardrobe[color].Add(item, 1);
+                }
+            }
+
+            string[] searchingFor = Console.ReadLine().Split();
+            foreach(var (color, items) in wardrobe)
+            {
+                Console.WriteLine($"{color} clothes:");
+                foreach (var item in items)
+                {
+                    Console.Write($"* {item.Key} - {item.Value} ");
+
+                    if (color == searchingFor[0] && item.Key == searchingFor[1])
+                        Console.Write("(found!)");
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
