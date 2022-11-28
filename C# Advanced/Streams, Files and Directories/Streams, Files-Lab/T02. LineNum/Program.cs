8db86c80de2bb99f27a271cfda2a682c3b3f738a
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-28 11:09:26 +0200

    Added Solution 02

---
+++ C# Advanced/Streams, Files and Directories/Streams, Files-Lab/T02. LineNum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            var writer = new StreamWriter(outputFilePath);
+            using (reader)
+            {
+                int num = 1;
+                using (writer)
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{num++}. {line}");
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
