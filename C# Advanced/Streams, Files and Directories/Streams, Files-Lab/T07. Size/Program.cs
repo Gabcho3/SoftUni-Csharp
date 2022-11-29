357d3be49034a88b3d0ac6773736d29aa671484a
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-11-29 20:55:25 +0200

    Added Solution 07

---
+++ C# Advanced/Streams, Files and Directories/Streams, Files-Lab/T07. Size/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            double sum = 0;
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+            FileInfo[] infos = dir.GetFiles("*", SearchOption.AllDirectories);
+
+            foreach(FileInfo info in infos )
+            {
+                sum += info.Length;
+            }
+
+            sum /= (1024) / 1024;
+            File.WriteAllText(outputFilePath, sum.ToString());
+        }
+    }
+}
+
