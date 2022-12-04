a0f7f82cb0f1d1b106a13342d4260800150313af
Gabriel Dimitrov <gabriel_work@abv.bg>
2022-12-04 14:12:52 +0200

    Added Solution 02

---
+++ Fundamentals C#/MyFinalExam-December2022/T02. Easter Eggs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T02._Easter_Eggs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"[@#]+(?<color>[a-z]{3,})[@#]+\W*\/+(?<amount>[0-9]+)\/+";
+            string input = Console.ReadLine();
+            var matches = Regex.Matches(input, patern);
+
+            foreach(Match match in matches)
+            {
+                string color = match.Groups["color"].Value;
+                int amount = int.Parse(match.Groups["amount"].Value);
+
+                Console.WriteLine($"You found {amount} {color} eggs!");
+            }
+        }
+    }
+}
