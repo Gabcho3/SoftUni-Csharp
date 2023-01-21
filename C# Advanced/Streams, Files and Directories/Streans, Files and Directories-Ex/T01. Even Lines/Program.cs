01ce49a8a8d637bf6c1e4e41c6c49eddf11fa92d
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-21 20:26:15 +0200

    Added Solution 01

---
+++ C# Advanced/Streams, Files and Directories/Streans, Files and Directories-Ex/T01. Even Lines/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            string output = string.Empty;
+
+            using(reader)
+            {
+                int line = 0;
+                while(true)
+                {
+                    string lineText = reader.ReadLine();
+                    if (lineText == null)
+                        break;
+
+                    if(line % 2 == 0)
+                    {
+                        lineText = lineText.Replace('-', '@');
+                        lineText = lineText.Replace(',', '@');
+                        lineText = lineText.Replace('.', '@');
+                        lineText = lineText.Replace('!', '@');
+                        lineText = lineText.Replace('?', '@');
+
+                        Stack<string> stack = new Stack<string>(lineText.Split().ToArray());
+
+                        while (stack.Count > 0)
+                        {
+                            output += stack.Pop() + " ";
+                        }
+                        output += "\n";
+                    }
+                    line++;
+                }
+            }
+
+            return output;
+        }
+    }
+}
