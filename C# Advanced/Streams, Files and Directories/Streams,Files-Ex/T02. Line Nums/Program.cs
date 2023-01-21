d9360d73408ac32f03832bc6dae77634c5defe3c
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-21 22:27:07 +0200

    Added Solution 02

---
+++ C# Advanced/Streams, Files and Directories/Streams,Files-Ex/T02. Line Nums/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"../../../Files/input.txt";
+            string outputFilePath = @"../../../Files/output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            var writer = new StreamWriter(outputFilePath);
+
+            using (reader)
+            {
+                using (writer)
+                {
+                    int line = 1;
+                    while (true)
+                    {
+                        string textOnLine = reader.ReadLine();
+
+                        int letters = 0;
+                        int marks = 0;
+
+                        if (textOnLine == null)
+                            break;
+
+                        foreach(var ch in textOnLine)
+                        {
+                            if (char.IsLetter(ch))
+                                letters++;
+
+                            if (char.IsPunctuation(ch))
+                                marks++;
+                        }
+
+                        writer.WriteLine("Line {0}: {1} ({2}) ({3})", line++, textOnLine, letters, marks);
+                    }
+                }
+            }
+        }
+    }
+
+}
