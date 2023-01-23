fa7aea7606399b5f5284e05668b918710eaca813
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-24 00:29:41 +0200

    Added Solution 03

---
+++ C# Advanced/Functional Programming/Functional Programming-Lab/T03. Count Uppercase/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace T03._Count_Uppercase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> isStartingWithUpper = w => char.IsUpper(w[0]);
+            Console.WriteLine(string.Join(Environment.NewLine
+                ,Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => isStartingWithUpper(w))
+                .ToArray()));
+        }
+    }
+}
