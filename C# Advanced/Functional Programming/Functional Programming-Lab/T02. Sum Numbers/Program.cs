38f6c89b8f1a5a414bcab712a9542a781ded7b80
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-24 00:10:48 +0200

    Added Solution 02

---
+++ C# Advanced/Functional Programming/Functional Programming-Lab/T02. Sum Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace T02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int[]> sum = arr => new int[2] { arr.Count(), arr.Sum() };
+            Console.WriteLine(string.Join(Environment.NewLine, 
+                sum(Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray())));
+        }
+    }
+}
