b7eefd298e12149278562d459445cef0dbeb1aed
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-24 00:49:13 +0200

    Added Solution 04

---
+++ C# Advanced/Functional Programming/Functional Programming-Lab/T04. Add VAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Transactions;
+
+namespace T04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> addVat = pr => pr *= 1.20;
+            Func<double, string> formater = pr => $"{pr:f2}";
+            Console.WriteLine(string.Join(Environment.NewLine
+                ,Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(pr => addVat(pr))
+                .Select(pr => formater(pr))
+                .ToArray()));
+        }
+    }
+}
