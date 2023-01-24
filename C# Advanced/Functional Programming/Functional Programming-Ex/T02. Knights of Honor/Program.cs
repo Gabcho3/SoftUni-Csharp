75a0ac8a94d9b37df19e175cbf6db744c9c58011
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-25 00:45:00 +0200

    Added Solution 02

---
+++ C# Advanced/Functional Programming/Functional Programming-Ex/T02. Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace T02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> addAndprint = names =>
+            {
+                foreach (var name in names)
+                {
+                    Console.WriteLine("Sir " + name);
+                }
+            };
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            addAndprint(names);
+        }
+    }
+}
