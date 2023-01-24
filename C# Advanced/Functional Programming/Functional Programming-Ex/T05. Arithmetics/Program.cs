85fd331aff809d7f0eaf3e58cc3ce86b75fccb7f
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-25 01:38:15 +0200

    Added Solution 05

---
+++ C# Advanced/Functional Programming/Functional Programming-Ex/T05. Arithmetics/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace T05._Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> add = n => ++n;
+            Func<int, int> multiply = n => n * 2;
+            Func<int, int> subtract = n => --n;
+            Action<int[]> print = n => Console.WriteLine(string.Join(" ", n));
+            Predicate<string> isEnd = s => s == "end" ? true : false;
+
+            int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string input;
+
+            while (!isEnd(input = Console.ReadLine()))
+            {
+                switch(input)
+                {
+                    case "add":
+                        ints = ints.Select(x => add(x)).ToArray();
+                        break;
+
+                    case "multiply":
+                        ints = ints.Select(x => multiply(x)).ToArray();
+                        break;
+
+                    case "subtract":
+                        ints = ints.Select(x => subtract(x)).ToArray();
+                        break;
+
+                    case "print":
+                        print(ints);
+                        break;
+                }
+            }
+        }
+    }
+}
