d3196aa75f3413cce8a8746ae4879d4c5e95f93a
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-25 00:44:51 +0200

    Added Solution 01

---
+++ C# Advanced/Functional Programming/Functional Programming-Ex/T01. Action Print/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace T01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = arr => Console.WriteLine(string.Join(Environment.NewLine, arr));
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            print(input);
+        }
+    }
+}
