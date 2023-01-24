23fb06b996ad3300df4968ed45ade571141cce8f
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-25 00:55:25 +0200

    Added Solution 03

---
+++ C# Advanced/Functional Programming/Functional Programming-Ex/T03. CustomMinFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace T03._CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> findSmallest = nums => //OR -> nums => nums.Min
+            {
+                int minValue = int.MaxValue;
+
+                foreach(int num in nums)
+                {
+                    if (num < minValue)
+                        minValue = num;
+                }
+
+                return minValue;
+            };
+            int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(findSmallest(ints));
+        }
+    }
+}
