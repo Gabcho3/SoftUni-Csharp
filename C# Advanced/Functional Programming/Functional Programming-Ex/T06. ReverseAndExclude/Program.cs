bf9bcfc4625090c6f1643274a42f1abec5b68d63
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-25 23:46:10 +0200

    Added Solution 06

---
+++ C# Advanced/Functional Programming/Functional Programming-Ex/T06. ReverseAndExclude/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace T06._ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int[]> reverse = i => i.Reverse().ToArray();
+            Func<int, int, bool> remove = (i, n) => i % n != 0;
+
+            int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int divideBy = int.Parse(Console.ReadLine());
+
+            ints = ints.Where(x => remove(x, divideBy)).ToArray();
+
+            Console.WriteLine(string.Join(" ", reverse(ints)));
+        }
+    }
+}
