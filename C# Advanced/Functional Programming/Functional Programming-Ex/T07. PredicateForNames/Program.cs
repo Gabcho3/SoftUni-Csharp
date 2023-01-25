b4a3a2decf103941f17da849bb64e7618fc5fc0b
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-25 23:55:09 +0200

    Added Solution 07

---
+++ C# Advanced/Functional Programming/Functional Programming-Ex/T07. PredicateForNames/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace T07._PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string[], int, List<string>> sortingNames = (names, l) =>
+            {
+                var result = new List<string>();
+                foreach(var name in names)
+                {
+                    if (name.Length <= l)
+                        result.Add(name);
+                }
+
+                return result;
+            };
+
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Console.WriteLine(string.Join(Environment.NewLine, sortingNames(names, length)));
+        }
+    }
+}
