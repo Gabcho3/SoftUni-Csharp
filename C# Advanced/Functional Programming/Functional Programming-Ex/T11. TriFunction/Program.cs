03d6229a478a3e6688c3ae07c312788b590a2d57
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-01-28 18:30:10 +0200

    Added Solution 11

---
+++ C# Advanced/Functional Programming/Functional Programming-Ex/T11. TriFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace T11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> func = (n, s) =>
+            {
+                int sum = 0;
+
+                foreach(var ch in n)
+                {
+                    sum += ch;
+                }
+
+                return sum >= s;
+            };
+
+            Action<string[]> printFirstName = names => { Console.WriteLine(names[0]); };
+
+            int neededSum = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split().Where(x => func(x, neededSum)).ToArray();
+            printFirstName(names);
+        }
+    }
+}
