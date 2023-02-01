26a2967185d6ff94bb771131bf7d91b21a312ed3
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-01 21:08:52 +0200

    Added Solution 02

---
+++ C# Advanced/Defining Classes/Defining Classes-Lab/T02. Car Extension/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //EXAMPLE
+            Car car = new Car()
+            {
+                Make = "VW",
+                Model = "BMW",
+                Year = 1992,
+                FuelQuantity = 200,
+                FuelConsumption = 200
+            };
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
