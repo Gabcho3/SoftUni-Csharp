f90a4963ea3413c38fbd31dc8d93aea793ea46f0
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-01 20:50:20 +0200

    Added Solution 01

---
+++ C# Advanced/Defining Classes/Defining Classes-Lab/T01. Car/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "BMW";
+            car.Model = "BMW";
+            car.Year = 2020;
+
+            Console.WriteLine("Make: {0}\nModel: {1}\nYear: {2}", car.Make, car.Model, car.Year);
+        }
+    }
+}
