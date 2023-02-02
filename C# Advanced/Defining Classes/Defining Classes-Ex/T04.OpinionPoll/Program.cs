17862d67c789b4931bd2fa02afe644cd463f5db0
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-02 13:40:29 +0200

    Added Solution 04

---
+++ C# Advanced/Defining Classes/Defining Classes-Ex/T04.OpinionPoll/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+                string name = data[0];
+                int age = int.Parse(data[1]);
+
+                people.Add(new Person(name, age));
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, 
+                people.OrderBy(x => x.Name)
+                .Where(x => x.Age > 30)
+                .ToList()));
+        }
+    }
+}
