541a58018463062d4383f89644260cb09c0126a0
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-02 14:16:50 +0200

    Added Solution 05

---
+++ C# Advanced/Defining Classes/Defining Classes-Ex/T05.DateModifier/DateModifier.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public int CalculateDifference(string[] firstDate, string[] secondDate)
+        {
+            int year1 = int.Parse(firstDate[0]);
+            int year2 = int.Parse(secondDate[0]);
+
+            int month1 = int.Parse(firstDate[1]);
+            int month2 = int.Parse(secondDate[1]);
+
+            int day1 = int.Parse(firstDate[2]);
+            int day2 = int.Parse(secondDate[2]);
+
+            DateTime first = new DateTime(year1, month1, day1);
+            DateTime second = new DateTime(year2, month2, day2);
+            TimeSpan diff = second.Subtract(first);
+
+            return Math.Abs(diff.Days);
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/Defining Classes-Ex/T05.DateModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            DateModifier dateModifier = new DateModifier();
+            string[] firstDate = Console.ReadLine().Split();
+            string[] secondDate = Console.ReadLine().Split();
+            Console.WriteLine(dateModifier.CalculateDifference(firstDate, secondDate));
+        }
+    }
+}
