b1fdd425d3d766b5bb9af5b849d6f22af5215f5b
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-02 13:02:59 +0200

    Added Solution 02

---
+++ C# Advanced/Defining Classes/Defining Classes-Ex/T02. Creating Constructors/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person peter = new Person();
+            Person george = new Person(15);
+            Person pavel = new Person("Pavel", 12);
+        }
+    }
+}
