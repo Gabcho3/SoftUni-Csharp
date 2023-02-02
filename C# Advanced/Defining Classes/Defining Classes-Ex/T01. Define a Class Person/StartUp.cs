e396fbd640fe25ba465cf5466b7a4f16b35f71a9
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-02 12:53:45 +0200

    Added Solution 01

---
+++ C# Advanced/Defining Classes/Defining Classes-Ex/T01. Define a Class Person/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person peter = new Person();
+            peter.Name = "Peter";
+            peter.Age = 20;
+            Console.WriteLine(peter.Name);
+            Console.WriteLine(peter.Age);
+        }
+    }
+}
