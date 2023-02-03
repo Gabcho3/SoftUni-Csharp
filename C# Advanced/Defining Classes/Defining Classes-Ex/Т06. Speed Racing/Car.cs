8416ffee7ff38e9107d506b4ee87838f71eccfe9
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-03 18:43:48 +0200

    Added Solution 06

---
+++ C# Advanced/Defining Classes/Defining Classes-Ex/Т06. Speed Racing/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+
+        public double DistanceTraveled { get; set; }
+
+        public void CalculateIfCarCanMove(double distanceTraveled)
+        {
+            if (FuelConsumptionPerKilometer * distanceTraveled <= FuelAmount)
+            {
+                DistanceTraveled += distanceTraveled;
+                FuelAmount -= FuelConsumptionPerKilometer * distanceTraveled;
+            }
+
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:f2} {DistanceTraveled}";
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/Defining Classes-Ex/Т06. Speed Racing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = data[0];
+                double fuelAmount = double.Parse(data[1]);
+                double fuelConsumptionFor1km = double.Parse(data[2]);
+
+                Car car = new Car()
+                {
+                    Model = model,
+                    FuelAmount = fuelAmount,
+                    FuelConsumptionPerKilometer = fuelConsumptionFor1km
+                };
+                cars.Add(car);
+            }
+
+            string[] cmd;
+
+            while(true)
+            {
+                cmd = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (cmd[0] == "End")
+                    break;
+
+                string model = cmd[1];
+                double distanceTraveled = double.Parse(cmd[2]);
+
+                Car car = cars.Where(x => x.Model == model).ToList()[0];
+                car.CalculateIfCarCanMove(distanceTraveled);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, cars));
+        }
+    }
+}
