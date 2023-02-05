0df7bdb2ba37608cee235224a0432b19ee76d722
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-05 22:57:49 +0200

    Added Solution 01

---
+++ C# Advanced/Generics/Generics-Lab/T01.BoxOfT/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            Item = new List<T>();
+        }
+
+        public List<T> Item { get; set; }
+
+        public int Count => Item.Count;
+
+        public void Add(T element)
+        {
+            Item.Add(element);
+        }
+
+        public T Remove()
+        {
+            T element = (Item.Last<T>());
+            Item.Remove(element);
+            return element;
+        }
+    }
+}
