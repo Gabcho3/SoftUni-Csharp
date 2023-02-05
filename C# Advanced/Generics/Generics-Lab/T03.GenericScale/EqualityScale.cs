56337aa73d481ff50a3fb44026f30f17ce15172a
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-05 23:29:49 +0200

    Added Solution 03

---
+++ C# Advanced/Generics/Generics-Lab/T03.GenericScale/EqualityScale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public T Left { get; set; }
+
+        public T Right { get; set; }
+
+        public bool AreEqual()
+        {
+            if (Left.Equals(Right))
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
