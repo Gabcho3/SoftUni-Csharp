8bf7658a383d16bb3748be2a5ecdab157425e226
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-06 22:27:37 +0200

    Added Solution 02

---
+++ C# Advanced/Generics/Generics-Ex/GenericBoxofInteger/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericBoxofInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>() { Value = input };
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
