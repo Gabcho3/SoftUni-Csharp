ed39a4c3da392301222267c9d3deacb1136a9f40
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-06 23:05:53 +0200

    Added Solution 04

---
+++ C# Advanced/Generics/Generics-Ex/T04. Generic Swap Method Integer/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace T04._Generic_Swap_Method_Integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> items = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int integer = int.Parse(Console.ReadLine());
+                items.Add(integer);
+            }
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            items = Swap(items, indexes[0], indexes[1]);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        public static List<T> Swap<T>(List<T> items, int index1, int index2)
+        {
+            T item1 = items[index1];
+            items[index1] = items[index2];
+            items[index2] = item1;
+
+            return items;
+        }
+    }
+}
