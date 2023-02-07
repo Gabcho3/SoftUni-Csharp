a397e05d9b7484717c1e999bb44c43c7716dfae2
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-07 21:08:48 +0200

    Added Solution 07

---
+++ C# Advanced/Generics/Generics-Ex/T07.Tuple/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace T07.Tuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Tuple<string, string> tuple1 = new Tuple<string, string>()
+            {
+                Item1 = input[0] + " " + input[1],
+                Item2 = input[2]
+            };
+
+            input = Console.ReadLine().Split();
+            Tuple<string, int> tuple2 = new Tuple<string, int>()
+            {
+                Item1 = input[0],
+                Item2 = int.Parse(input[1])
+            };
+
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Tuple<int, double> tuple3 = new Tuple<int, double>()
+            {
+                Item1 = (int)nums[0],
+                Item2 = nums[1]
+            };
+
+            Console.WriteLine(tuple1);
+            Console.WriteLine(tuple2);
+            Console.WriteLine(tuple3);
+        }
+    }
+}
---
+++ C# Advanced/Generics/Generics-Ex/T07.Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T07.Tuple
+{
+    internal class Tuple<T,V>
+    {
+        private T item1;
+        private V item2;
+
+        public T Item1 { get { return item1; } set { item1 = value; } }
+
+        public V Item2 { get { return item2; } set { item2 = value; } }
+
+        public override string ToString()
+        {
+            return $"{item1} -> {item2}";
+        }
+    }
+}
