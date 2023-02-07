875b193d30febd5b340910709ae07b41f8cb9115
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-07 21:32:21 +0200

    Added Solution 08

---
+++ C# Advanced/Generics/Generics-Ex/T08.Threeuple/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace T08.Threeuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Threeuple<string, string, string> threeuple1 = new Threeuple<string, string, string>()
+            {
+                First = string.Join(" ", input.Split().Take(2)),
+                Second = input.Split()[2],
+                Third = input.Split()[3]
+            };
+
+            input = Console.ReadLine();
+            bool isDrunk = input.Split()[2] == "drunk" ? true : false;
+            Threeuple<string, int, bool> threeuple2 = new Threeuple<string, int, bool>()
+            {
+                First = input.Split()[0],
+                Second = int.Parse(input.Split()[1]),
+                Third = isDrunk
+            };
+
+            input = Console.ReadLine();
+            Threeuple<string, double, string> threeuple3 = new Threeuple<string, double, string>()
+            {
+                First = input.Split()[0],
+                Second = double.Parse(input.Split()[1]),
+                Third = input.Split()[2]
+            };
+
+            Console.WriteLine(threeuple1);
+            Console.WriteLine(threeuple2);
+            Console.WriteLine(threeuple3);
+        }
+    }
+}
---
+++ C# Advanced/Generics/Generics-Ex/T08.Threeuple/Threeuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T08.Threeuple
+{
+    internal class Threeuple<T,V,W>
+    {
+        private T first;
+        private V second;
+        private W third;
+
+        public T First { get { return first; } set { first = value; } }
+
+        public V Second { get { return second; } set { second = value; } }
+
+        public W Third { get { return third; } set { third = value; } }
+
+        public override string ToString()
+        {
+            return $"{First} -> {Second} -> {Third}";
+        }
+    }
+}
