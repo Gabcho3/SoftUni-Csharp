f73cc09db0e87fed18e9ecd36854592682344d2f
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-07 20:45:42 +0200

    Added Solution 06

---
+++ C# Advanced/Generics/Generics-Ex/T06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T06.GenericCountMethodDouble
+{
+    internal class Box<T>
+    {
+        public List<T> Doubles { get; set; }
+
+        public int Counter<T>(List<T> doubles, T element) where T : IComparable
+        { 
+            return doubles.Where(x => x.CompareTo(element) == 1).ToList().Count; 
+        }
+    }
+}
---
+++ C# Advanced/Generics/Generics-Ex/T06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace T06.GenericCountMethodDouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>()
+            {
+                Doubles = new List<double>()
+            };
+
+            for(int i = 0; i < n; i++)
+            {
+                box.Doubles.Add(double.Parse(Console.ReadLine()));
+            }
+
+            double element = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.Counter<double>(box.Doubles, element));
+        }
+    }
+}
