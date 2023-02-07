790804e929f82361b6fc4aecfb24c49942f70c0d
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-07 20:45:28 +0200

    Added Solution 05

---
+++ C# Advanced/Generics/Generics-Ex/T05.GenericCountMethodString/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T05.GenericCountMethodString
+{
+    internal class Box<T>
+    {
+        private List<T> values;
+
+        public Box()
+        {
+            Values = new List<T>();
+        }
+
+        public List<T> Values { get { return values; } set { values = value; } }
+
+        public int Counter<T>(List<T> values, T element) where T : IComparable
+        {
+            values = values.Where(x => x.CompareTo(element) == 1).ToList();
+
+            return values.Count;
+        }
+    }
+}
---
+++ C# Advanced/Generics/Generics-Ex/T05.GenericCountMethodString/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace T05.GenericCountMethodString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string item = Console.ReadLine();
+                box.Values.Add(item);
+            }
+
+            string element = Console.ReadLine();
+            Console.WriteLine(box.Counter(box.Values, element));
+        }
+    }
+}
