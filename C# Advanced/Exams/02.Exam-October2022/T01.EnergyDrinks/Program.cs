356ca56f3706af96f166a012146c0cf4a972ac50
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-12 21:40:23 +0200

    Added Solution 01

---
+++ C# Advanced/Exams/02.Exam-October2022/T01.EnergyDrinks/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.EnergyDrinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> coffeine = new Stack<int>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+            Queue<int> drinks = new Queue<int>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int limit = 300;
+            int mgDrank = 0;
+
+            while (true)
+            {
+                int sum = coffeine.Pop() * drinks.Peek();
+
+                if (sum < limit && mgDrank < 300)
+                {
+                    mgDrank += sum;
+                    limit -= sum;
+                    drinks.Dequeue();
+                }
+
+                else
+                {
+                    int currDrink = drinks.Dequeue();
+                    drinks.Enqueue(currDrink);
+
+                    if (mgDrank >= 30)
+                    {
+                        mgDrank -= 30;
+                    }
+                    limit += 30;
+                }
+
+                if (drinks.Count == 0)
+                {
+                    Console.WriteLine("At least Stamat wasn't exceeding the maximum caffeine.");
+                    break;
+                }
+
+                if (coffeine.Count == 0)
+                {
+                    Console.WriteLine($"Drinks left: {string.Join(", ", drinks)}");
+                    break;
+                }
+            }
+            Console.WriteLine($"Stamat is going to sleep with {mgDrank} mg caffeine.");
+        }
+    }
+}
