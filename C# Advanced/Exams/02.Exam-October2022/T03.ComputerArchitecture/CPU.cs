1411089f8806011c5023e74f8deac823af925a46
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-13 20:03:10 +0200

    Added Solution 03

---
+++ C# Advanced/Exams/02.Exam-October2022/T03.ComputerArchitecture/CPU.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class CPU
+    {
+        private string brand;
+        private int cores;
+        private double frequency;
+
+        public CPU(string brand, int cores, double frequency)
+        {
+            Brand = brand;
+            Cores = cores;
+            Frequency = frequency;
+        }
+
+        public string Brand { get { return brand; } set { brand = value; } }
+
+        public int Cores { get { return cores; } set { cores = value; } }
+
+        public double Frequency { get { return frequency; } set { frequency = value; } }
+
+        public override string ToString()
+        {
+            return $"{brand} CPU:\nCores: {cores}\nFrequency: {Frequency:f1} GHz";
+        }
+    }
+}
---
+++ C# Advanced/Exams/02.Exam-October2022/T03.ComputerArchitecture/Computer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        public Computer(string model, int capacity) 
+        {
+            Model = model;
+            Capacity = capacity;
+            Multiprocessor = new List<CPU>();
+        }
+
+        public string Model { get;set; }
+
+        public int Capacity { get; set; }
+
+        public List<CPU> Multiprocessor { get; set; }
+
+        public int Count => Multiprocessor.Count;
+
+        public void Add(CPU cpu)
+        {
+            if (Multiprocessor.Count < Capacity)
+            {
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            bool exist = Multiprocessor.Any(x => x.Brand == brand);
+            Multiprocessor.RemoveAll(x => x.Brand == brand);
+            return exist;
+        }
+
+        public CPU MostPowerful() => Multiprocessor.OrderByDescending(x => x.Frequency).First();
+
+        public CPU GetCPU(string brand) => Multiprocessor.Find(x => x.Brand == brand);
+
+        public string Report()
+        {
+            StringBuilder output = new StringBuilder();
+            output.Append($"CPUs in the Computer {Model}:");
+            foreach (var cpu in Multiprocessor)
+            {
+                output.Append(Environment.NewLine + cpu.ToString());
+            }
+
+            return output.ToString();
+        }
+    }
+}
