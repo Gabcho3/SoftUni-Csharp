62e1fb908931ee858a358317bed6cbc480dea285
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-17 19:38:22 +0200

    Added Solution 03

---
+++ C# Advanced/Exams/07.RetakeExam-December2021/T03.Drones/Airfield.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Drones
+{
+    public class Airfield
+    {
+        public Airfield(string name, int capacity, double landingStrip)
+        {
+            Name = name;
+            Capacity = capacity;
+            LandingStrip = landingStrip;
+            Drones = new List<Drone>();
+        }
+
+        public List<Drone> Drones { get; set; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public double LandingStrip { get; set; }
+
+        public int Count => Drones.Count;
+
+        public string AddDrone(Drone drone)
+        {
+            string droneName = drone.Name;
+            string droneBrand = drone.Brand;
+            int droneRange = drone.Range;
+
+            if (droneName == null || droneName == string.Empty
+                || droneBrand == null || droneBrand == string.Empty
+                || droneRange < 5 || droneRange > 15)
+            {
+                return "Invalid drone.";
+            }
+
+            if (Drones.Count == Capacity)
+            {
+                return "Airfield is full.";
+            }
+            Drones.Add(drone);
+            return $"Successfully added {droneName} to the airfield.";
+        }
+
+        public bool RemoveDrone(string name)
+        {
+            var droneToRemove = Drones.Find(x => x.Name == name);
+
+            if (droneToRemove == null)
+                return false;
+
+            Drones.Remove(droneToRemove);
+            return true;
+        }
+
+        public int RemoveDroneByBrand(string brand)
+        {
+            var dronesToRemove = Drones.FindAll(x => x.Brand == brand);
+            Drones.RemoveAll(x => x.Brand == brand);
+            return dronesToRemove.Count;
+        }
+
+        public Drone FlyDrone(string name)
+        {
+            var droneToFly = Drones.Find(x => x.Name == name);
+
+            if (droneToFly != null)
+            {
+                droneToFly.Available = false;
+            }
+            return droneToFly;
+        }
+
+        public List<Drone> FlyDronesByRange(int range)
+        { 
+            var dronesToFly = Drones.Where(x => x.Range >= range).ToList();
+            for(int i = 0; i < dronesToFly.Count; i++)
+            {
+                dronesToFly[i] = FlyDrone(dronesToFly[i].Name);
+            }
+            return dronesToFly;
+        }
+
+        public string Report()
+        {
+            string output = $"Drones available at {Name}:";
+            foreach(var drone in Drones.Where(x => x.Available == true))
+            {
+                output += Environment.NewLine + drone.ToString();
+            }
+            return output;
+        }
+    }
+}
---
+++ C# Advanced/Exams/07.RetakeExam-December2021/T03.Drones/Drone.cs
@@ -0,0 +1,26 @@
+﻿namespace Drones
+{
+    public class Drone
+    {
+        public Drone(string name, string brand, int range)
+        {
+            Name = name;
+            Brand = brand;
+            Range = range;
+            Available = true;
+        }
+
+        public string Name { get; set; }    
+
+        public string Brand { get; set; }
+
+        public int Range { get; set; }
+
+        public bool Available { get; set; }
+
+        public override string ToString()
+            => $"Drone: {Name}" +
+            $"\nManufactured by: {Brand}" +
+            $"\nRange: {Range} kilometers";
+    }
+}
