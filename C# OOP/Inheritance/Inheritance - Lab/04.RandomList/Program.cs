dc73ff087b4044833999bd009789f191dbb704c3
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-21 22:33:54 +0200

    Added Solution 04

---
+++ C# OOP/Inheritance/Inheritance - Lab/04.RandomList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
---
+++ C# OOP/Inheritance/Inheritance - Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random = new Random();
+
+        public Random Random { get { return random; } }
+
+        public string RandomString()
+        {
+            return Random.Next(0, Count).ToString();
+        }
+    }
+}
