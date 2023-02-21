966d49c32220e377fb272cfab0ac52e3258ee8ae
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-21 22:46:56 +0200

    Added Solution 05

---
+++ C# OOP/Inheritance/Inheritance - Lab/05.StackofStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            stack.AddRange(new List<string>() { "1", "2", "3", "4", });
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
