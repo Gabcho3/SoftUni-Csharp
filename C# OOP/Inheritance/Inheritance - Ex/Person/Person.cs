e577e595153ae62559dcb757082ff3364210619a
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-22 20:05:54 +0200

    Added Solution 01

---
+++ C# OOP/Inheritance/Inheritance - Ex/Person/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get { return name; } set { name = value; } }
+
+        public int Age { get { return age; } set { age = value; } }
+
+        public override string ToString()
+            => $"Name: {Name}, Age: {Age}";
+    }
+}
