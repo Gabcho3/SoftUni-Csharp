eeb5389257197ab8c3606ebbdb3ea06ff034ad62
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-25 00:34:40 +0200

    Added Solution 05

---
+++ C# OOP/Inheritance/Inheritance - Ex/Restaurant/Cake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private static double grams = 250;
+        private static double calories = 1000;
+        private static decimal price = 5;
+
+        public Cake(string name)
+            :base(name, price, grams, calories) { }
+    }
+}
---
+++ C# OOP/Inheritance/Inheritance - Ex/Restaurant/Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private static double coffeeMilliliters = 50;
+        private static decimal coffeePrice = 3.50m;
+
+        public Coffee(string name, double caffeine)
+            :base(name, coffeePrice, coffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
