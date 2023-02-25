ba29a02af3325ba115520379f63c30312da19c0a
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-25 20:19:55 +0200

    Added Solution 03

---
+++ C# OOP/Encapsulation/Encapsulation-Lab/03.Validation/Person.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstname;
+        private string lastname;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return firstname; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstname = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastname; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastname = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            private set
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age <= 30)
+            {
+                Salary += Salary * percentage / 200; //People younger than 30 get half the increase
+            }
+
+            else
+            {
+                Salary += Salary * percentage / 100;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
