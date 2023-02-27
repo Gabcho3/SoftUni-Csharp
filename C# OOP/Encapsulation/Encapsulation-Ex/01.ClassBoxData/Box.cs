4bee520733d79fbee59c71ae39ed85f530fa25ea
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-02-28 00:29:39 +0200

    Added Solution 01

---
+++ C# OOP/Encapsulation/Encapsulation-Ex/01.ClassBoxData/Box.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public double Length
+        {
+            get { return length; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+            => 2 * (width * length) + 2 * (height * length) + 2 * (width * height);
+
+        public double LateralSurfaceArea()
+            => 2 * (length * height) + 2 * (width * height);
+
+        public double Volume()
+            => length * width * height;
+    }
+}
--- C# OOP/Encapsulation/Encapsulation-Ex/01.ClassBoxData/Program.cs
+++ C# OOP/Encapsulation/Encapsulation-Ex/01.ClassBoxData/Program.cs
@@ -2,11 +2,38 @@
 
 namespace _01.ClassBoxData
 {
-    internal class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Box box = new Box();
+
+            for(int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            box.Length = double.Parse(Console.ReadLine());
+                            break;
+                        case 1:
+                            box.Width = double.Parse(Console.ReadLine());
+                            break;
+                        case 2:
+                            box.Height = double.Parse(Console.ReadLine());
+                            break;
+                    }
+                }
+                catch(Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}" +
+                $"\nLateral Surface Area - {box.LateralSurfaceArea():f2}" +
+                $"\nVolume - {box.Volume():f2}");
         }
     }
 }
