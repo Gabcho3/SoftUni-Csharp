528fb92f4cd339eb7f18b6b8c94da8c6264aee2a
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-02 21:55:16 +0200

    Added Solution 02

---
+++ C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Lab/02.Cars/Seat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; }
+        public string Color { get; }
+
+        public string Start()
+            => "Engine Start";
+
+        public string Stop()
+            => "Breaaak!";
+
+        public override string ToString()
+            => $"{Color} Seat {Model}";
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Lab/02.Cars/Tesla.cs
@@ -0,0 +1,30 @@
+﻿using Cars;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get; }
+        public string Color { get; }
+        public int Battery { get; }
+
+        public string Start()
+            => "Engine Start";
+
+        public string Stop()
+            => "Breaaak!";
+
+        public override string ToString()
+            => $"{Color} Tesla {Model} with {Battery} Batteries";
+    }
+}
