bad465057348ad52769711c2283080c9f3b1b1b7
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-02 21:29:26 +0200

    Added Solution 01

---
+++ C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Lab/01.Shapes/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        private int Radius { get; }
+
+        public void Draw()
+        {
+            //Algorithm is not mine
+            double rIn = Radius - 0.4;
+            double rOut = Radius + 0.4;
+            for (double y = Radius; y >= -Radius; --y)
+            {
+                for (double x = -Radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Lab/01.Shapes/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int Width { get; }
+        private int Height { get; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        //Algorithm is not mine
+        public void Draw()
+        {
+            DrawLine(Width, '*', '*');
+            for (int i = 1; i < Height - 1; ++i)
+                DrawLine(Width, '*', ' ');
+            DrawLine(Width, '*', '*');
+        }
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; ++i)
+                Console.Write(mid);
+            Console.WriteLine(end);
+        }
+    }
+}
