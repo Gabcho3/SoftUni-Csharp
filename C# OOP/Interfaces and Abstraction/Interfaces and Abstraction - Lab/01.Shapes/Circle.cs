6795f33c96b2b4b6fca1cefb500727ac2b6fb5e8
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-02 21:54:03 +0200

    Fixed code (Solution 01)

--- C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Lab/01.Shapes/Circle.cs
+++ C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Lab/01.Shapes/Circle.cs
@@ -6,21 +6,21 @@ namespace Shapes
 {
     public class Circle : IDrawable
     {
+        private int radius;
+
         public Circle(int radius)
         {
-            Radius = radius;
+            this.radius = radius;
         }
-
-        private int Radius { get; }
 
         public void Draw()
         {
             //Algorithm is not mine
-            double rIn = Radius - 0.4;
-            double rOut = Radius + 0.4;
-            for (double y = Radius; y >= -Radius; --y)
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+            for (double y = this.radius; y >= -this.radius; --y)
             {
-                for (double x = -Radius; x < rOut; x += 0.5)
+                for (double x = -this.radius; x < rOut; x += 0.5)
                 {
                     double value = x * x + y * y;
                     if (value >= rIn * rIn && value <= rOut * rOut)
--- C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Lab/01.Shapes/Rectangle.cs
+++ C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Lab/01.Shapes/Rectangle.cs
@@ -6,22 +6,22 @@ namespace Shapes
 {
     public class Rectangle : IDrawable
     {
-        private int Width { get; }
-        private int Height { get; }
+        private int width;
+        private int height;
 
         public Rectangle(int width, int height)
         {
-            Width = width;
-            Height = height;
+            this.width = width;
+            this.height = height;
         }
 
         //Algorithm is not mine
         public void Draw()
         {
-            DrawLine(Width, '*', '*');
-            for (int i = 1; i < Height - 1; ++i)
-                DrawLine(Width, '*', ' ');
-            DrawLine(Width, '*', '*');
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; ++i)
+                DrawLine(this.width, '*', ' ');
+            DrawLine(this.width, '*', '*');
         }
         private void DrawLine(int width, char end, char mid)
         {
