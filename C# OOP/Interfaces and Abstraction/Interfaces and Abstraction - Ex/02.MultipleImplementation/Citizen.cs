92433af593c6f18ea0ccb3785c34c970720ffe14
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-03 11:56:57 +0200

    Added Solution 02

---
+++ C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Ex/02.MultipleImplementation/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthDate)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthDate;
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+        public string Id { get; }
+        public string Birthdate { get; }
+    }
+}
