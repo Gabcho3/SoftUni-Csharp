41f5ea3e93817366e2559a50594d362644232ef0
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-06 22:21:50 +0200

    Added Solution 03

---
+++ C# OOP/Polymorphism/Polymorphism - Lab/03.Shapes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(10, 20);
+            Shape circle = new Circle(20);
+
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.Draw());
+
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
---
+++ C# OOP/Polymorphism/Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public override double CalculatePerimeter()
+            => height * 2 + width * 2;
+
+        public override double CalculateArea()
+            => height * width;
+    }
+}
