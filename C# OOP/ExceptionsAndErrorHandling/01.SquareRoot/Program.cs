842bb500c89a96faa4f3c5185fd23ace9b27c91c
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-10 21:35:05 +0200

    Added Solution 01

---
+++ C# OOP/ExceptionsAndErrorHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (num < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(num));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+;                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
