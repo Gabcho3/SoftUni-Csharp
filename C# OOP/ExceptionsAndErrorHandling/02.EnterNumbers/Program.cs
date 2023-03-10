9a4daeb78081a493c37c72d91205edd5c2b9a130
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-10 22:12:41 +0200

    Added Solution 02

---
+++ C# OOP/ExceptionsAndErrorHandling/02.EnterNumbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.EnterNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>();
+
+            while(ints.Count < 10)
+            {
+                try
+                {
+                    if (ints.Count > 0)
+                    {
+                        ints.Add(ReadNumber(ints.Max(), 100));
+                    }
+                    else
+                    {
+                        ints.Add(ReadNumber(1, 100));
+                    }
+                }
+                catch(FormatException formEx)
+                {
+                    Console.WriteLine(formEx.Message);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.WriteLine(string.Join(", ", ints));
+        }
+
+        static int ReadNumber(int start, int  end)
+        {
+            int num;
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+
+                if (num <= start || num >= end)
+                {
+                    throw new ArgumentException($"Your number is not in range {start} - {end}!");
+                }
+            }
+            catch(FormatException)
+            {
+                throw new FormatException("Invalid Number!");
+            }
+
+            return num;
+        }
+    }
+}
