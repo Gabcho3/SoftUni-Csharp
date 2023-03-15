7df66cab13362f2a5fafa835080443270c1315b4
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-16 00:35:33 +0200

    Added Solution 02

---
+++ C# OOP/Reflection and Attributes/Reflection and Attributes-Lab/02.Mistakes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Hacker
+    {
+        public string username = "securityGod82";
+        private string password = "mySuperSecretPassw0rd";
+
+        public string Password
+        {
+            get => this.password;
+            set => this.password = value;
+        }
+
+        private int Id { get; set; }
+
+        public double BankAccountBalance { get; private set; }
+
+        public void DownloadAllBankAccountsInTheWorld()
+        {
+        }
+    }
+}
---
+++ C# OOP/Reflection and Attributes/Reflection and Attributes-Lab/02.Mistakes/Spy.cs
@@ -0,0 +1,57 @@
+﻿using Stealer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, string[] fieldNames)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type type = Type.GetType(className);
+            FieldInfo[] fields = type.GetFields((BindingFlags)60);
+            Object classInstance = Activator.CreateInstance(type);
+
+            sb.Append($"Class under investigation: {className}");
+            foreach (FieldInfo field in fields.Where(f => fieldNames.Contains(f.Name)))
+            {
+                sb.Append(Environment.NewLine + $"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type type = Type.GetType(className);
+            FieldInfo[] fields = type.GetFields();
+            MethodInfo[] publicMethods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo[] nonPublicMethods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            foreach (var field in fields.Where(f => f.IsPublic))
+            {
+                sb.Append($"{field.Name} must be private!" + Environment.NewLine);
+            }
+
+            foreach (var method in nonPublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.Append($"{method.Name} have to be public!" + Environment.NewLine);
+            }
+
+            foreach (var method in publicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.Append($"{method.Name} have to be private!" + Environment.NewLine);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
