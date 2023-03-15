c987b7252caa770f6b624866c8d1aac7356fa01f
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-16 00:43:31 +0200

    Added Solution 03

---
+++ C# OOP/Reflection and Attributes/Reflection and Attributes-Lab/03.Mission/Spy.cs
@@ -0,0 +1,31 @@
+﻿using Stealer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string RevealPrivateMethods(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type type = Type.GetType(className);
+            MethodInfo[] privateMethods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+            
+            sb.Append($"All Private Methods of Class: {type.FullName}");
+            sb.Append(Environment.NewLine + $"Base Class: {type.BaseType.Name}");
+
+            foreach (MethodInfo method in privateMethods )
+            {
+                sb.Append(Environment.NewLine + method.Name);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
