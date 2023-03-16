549768a5a9222c23ce73af60ef6307261f1926df
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-16 16:02:20 +0200

    Added Solution 04

---
+++ C# OOP/Reflection and Attributes/Reflection and Attributes-Lab/04.Collector/Spy.cs
@@ -0,0 +1,34 @@
+﻿using Stealer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string GollectGettersAndSetters(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type type = Type.GetType(className);
+            MethodInfo[] methods = type.GetMethods((BindingFlags)60);
+            
+            foreach (MethodInfo method in methods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (MethodInfo method in methods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
