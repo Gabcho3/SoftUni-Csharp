7e4b16e8504fa6addf20df890ff12f1af0b59a70
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-16 16:45:27 +0200

    Added Solution 05

---
+++ C# OOP/Reflection and Attributes/Reflection and Attributes-Lab/05.Attribute/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
