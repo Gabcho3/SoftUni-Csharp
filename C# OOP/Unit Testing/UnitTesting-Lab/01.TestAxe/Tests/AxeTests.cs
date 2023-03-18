c75f4ab8a694a173e33a5f35f3691ccdb903c68f
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-18 23:37:55 +0200

    Added Solution 01

---
+++ C# OOP/Unit Testing/UnitTesting-Lab/01.TestAxe/Tests/AxeTests.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using System;
+using System.ComponentModel.Design.Serialization;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private Axe axe;
+        private Dummy dummy;
+        private int axeAttack;
+        private int axeDurability;
+        private int dummyHealth;
+        private int dummyexpirience;
+
+        [SetUp]
+        public void SetUp()
+        {
+            axeAttack = 3;
+            axeDurability = 5;
+            dummyHealth = 6;
+            dummyexpirience = 20;
+
+            axe = new Axe(axeAttack, axeDurability);
+            dummy = new Dummy(dummyHealth, dummyexpirience);
+        }
+
+        [Test]
+        public void Test_AxesLosesDurabilityAfterAtack()
+        {
+            axe.Attack(dummy);
+            Assert.AreEqual(4, axe.DurabilityPoints);
+        }
+
+        [Test]
+        public void Test_AxeAttacksWithBrokenWeapon()
+        {
+            axeDurability -= 6;
+            axe = new Axe(axeAttack, axeDurability);
+
+            Assert.Catch<InvalidOperationException>(() =>
+            {
+                axe.Attack(dummy);
+            });
+        }
+    }
+}
