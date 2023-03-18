83b22c19b57c657d94238744dc34cae48984dce9
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-03-19 01:39:58 +0200

    Added Solution 02

--- C# OOP/Unit Testing/Unit Testing-Lab/Skeleton.Tests/AxeTests.cs
+++ C# OOP/Unit Testing/Unit Testing-Lab/Skeleton.Tests/AxeTests.cs
@@ -1,6 +1,5 @@
 using NUnit.Framework;
 using System;
-using System.ComponentModel.Design.Serialization;
 
 namespace Skeleton.Tests
 {
---
+++ C# OOP/Unit Testing/Unit Testing-Lab/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        private Axe axe;
+        private Dummy dummy;
+        private int axeAttack;
+        private int axeDurability;
+        private int dummyHealth;
+        private int dummyexpirience;
+
+        [SetUp]
+        public void SetUp()
+        {
+            axeAttack = 3;
+            axeDurability = 5;
+            dummyHealth = 6;
+            dummyexpirience = 20;
+
+            axe = new Axe(axeAttack, axeDurability);
+            dummy = new Dummy(dummyHealth, dummyexpirience);
+        }
+
+        [Test]
+        public void Test_DummyLossesHealthWhenIsAttacked()
+        {
+            axe.Attack(dummy);
+
+            Assert.AreEqual(3, dummy.Health);
+        }
+
+        [Test]
+        public void Test_DummyIsDeadThrows()
+        {
+            axe = new Axe(6, axeDurability);
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                axe.Attack(dummy);
+                axe.Attack(dummy);
+            });
+        }
+
+        [Test]
+        public void Test_DeadDummyCanGiveXpThrows()
+        {
+            dummy = new Dummy(1, 1);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dummy.GiveExperience();
+            });
+        }
+
+        [Test]
+        public void Test_AliveDummyCannotGiveXp()
+        {
+            dummy = new Dummy(0, 1);
+
+            Assert.IsTrue(dummy.GiveExperience() > 0);
+        }
+    }
+}
