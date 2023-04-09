8482173ab254bc1af5a6d9a36bfacb2772fb31ee
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-04-09 20:52:03 +0300

    Added new Solution 01

--- C# Advanced/C# Advanced/Exams/01.RetakeExam-December2022/T01.ClimbThePeaks/Program.cs
+++ C# Advanced/C# Advanced/Exams/01.RetakeExam-December2022/T01.ClimbThePeaks/Program.cs
@@ -4,57 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Queue<string[]> peaks = new Queue<string[]>();
-            peaks.Enqueue(new string[2] { "Vihren", "80" });
-            peaks.Enqueue(new string[2] { "Kutelo", "90" });
-            peaks.Enqueue(new string[2] { "Banski Suhodol", "100" });
-            peaks.Enqueue(new string[2] { "Polezhan", "60" });
-            peaks.Enqueue(new string[2] { "Kamenitza", "70" });
-
-            Stack<int> foodPortions = new Stack<int>(Console.ReadLine()
-                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToList());
-            Queue<int> stamina = new Queue<int>(Console.ReadLine()
-                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToList());
-
-            List<string> peaksClimbed = new List<string>();
-            int dayCount = 1;
-            while (peaks.Count > 0)
+            Stack<int> portions = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            Queue<int> stamina = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            Dictionary<string, int> peaks = new Dictionary<string, int>()
             {
-                if ((foodPortions.Count == 0 && stamina.Count == 0) || dayCount == 7)
+                ["Vihren"] = 80,
+                ["Kutelo"] = 90,
+                ["Banski Suhodol"] = 100,
+                ["Polezhan"] = 60,
+                ["Kamenitza"] = 70
+            };
+            List<string> conqueredPeaks = new List<string>();
+
+            while (true)
+            {
+                int sum = portions.Pop() + stamina.Dequeue();
+
+                if (sum >= peaks.First().Value)
+                {
+                    conqueredPeaks.Add(peaks.First().Key);
+                    peaks.Remove(peaks.First().Key);
+                }
+
+                if (peaks.Count == 0)
+                {
+                    Console.WriteLine("Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK");
+                    break;
+                }
+
+                if (stamina.Count == 0)
                 {
                     Console.WriteLine("Alex failed! He has to organize his journey better next time -> @PIRINWINS");
-                    PrintPeaks(peaksClimbed);
-                    return;
+                    break;
                 }
-
-                int sum = foodPortions.Pop() + stamina.Dequeue();
-                int difficultyLevel = int.Parse(peaks.Peek()[1]);
-
-                if (sum >= difficultyLevel)
-                {
-                    peaksClimbed.Add(peaks.Dequeue()[0]);
-                }
-
-                else
-                    dayCount++;
             }
-            Console.WriteLine("Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK");
-            PrintPeaks(peaksClimbed);
-        }
 
-        private static void PrintPeaks(List<string> peaksClimbed)
-        {
-            if (peaksClimbed.Count == 0)
-                return;
-
-            Console.WriteLine("Conquered peaks:");
-            foreach (var peak in peaksClimbed)
+            if (conqueredPeaks.Count > 0)
             {
-                Console.WriteLine(peak);
+                Console.WriteLine("Conquered peaks:");
+                Console.WriteLine(string.Join(Environment.NewLine, conqueredPeaks));
             }
         }
     }
