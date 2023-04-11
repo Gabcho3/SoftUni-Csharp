ccd8d83cb1121162e05014131642e0f4ec0723ec
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-04-11 17:05:20 +0300

    Added new Solution 02

--- C# Advanced/C# Advanced/Exams/02.Exam-October2022/T02.RallyRacing/Program.cs
+++ C# Advanced/C# Advanced/Exams/02.Exam-October2022/T02.RallyRacing/Program.cs
@@ -8,23 +8,30 @@ namespace T02.RallyRacing
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int size = int.Parse(Console.ReadLine());
             string racingNumber = Console.ReadLine();
 
-            char[,] route = new char[n, n];
-            for (int row = 0; row < n; row++)
+            char[,] raceRoute = new char[size, size];
+
+            Dictionary<int, int> tunnelsPositions = new Dictionary<int, int>();
+            for (int row = 0; row < size; row++)
             {
-                char[] values = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
-                for (int col = 0; col < n; col++)
+                char[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < size; col++)
                 {
-                    route[row, col] = values[col];
+                    char currElement = rowData[col];
+                    if (currElement == 'T')
+                    {
+                        tunnelsPositions.Add(row, col);
+                    }
+
+                    raceRoute[row, col] = currElement;
                 }
             }
 
             int carRow = 0;
             int carCol = 0;
-            int km = 0;
-            List<int[]> tunnelPositions = FindTunnels(route);
+            int kilometersPassed = 0;
 
             while (true)
             {
@@ -33,83 +40,61 @@ namespace T02.RallyRacing
                 if (cmd == "End")
                 {
                     Console.WriteLine($"Racing car {racingNumber} DNF.");
-                    Console.WriteLine($"Distance covered {km} km.");
-                    route[carRow, carCol] = 'C';
-                    PrintingRoute(route);
-                    return;
+                    break;
                 }
 
-                if (cmd == "left")
+                switch (cmd)
                 {
-                    carCol--;
+                    case "up": carRow--; break;
+                    case "right": carCol++; break;
+                    case "down": carRow++; break;
+                    case "left": carCol--; break;
                 }
 
-                else if (cmd == "up")
+                if (raceRoute[carRow, carCol] == '.')
+                    kilometersPassed += 10;
+
+                else if (raceRoute[carRow, carCol] == 'F')
                 {
-                    carRow--;
-                }
-
-                else if (cmd == "right")
-                {
-                    carCol++;
-                }
-
-                else if (cmd == "down")
-                {
-                    carRow++;
-                }
-
-                if (tunnelPositions.Any(x => x[0] == carRow && x[1] == carCol))
-                {
-                    int[] endOfTunnel = tunnelPositions.Find(x => x[0] != carRow || x[1] != carCol);
-                    carRow = endOfTunnel[0];
-                    carCol = endOfTunnel[1];
-
-                    route[tunnelPositions[0][0], tunnelPositions[0][1]] = '.';
-                    route[tunnelPositions[1][0], tunnelPositions[1][1]] = '.';
-
-                    km += 20; //we always add 10kms
-                }
-                km += 10;
-
-                if (route[carRow, carCol] == 'F')
-                {
+                    kilometersPassed += 10;
                     Console.WriteLine($"Racing car {racingNumber} finished the stage!");
-                    Console.WriteLine($"Distance covered {km} km.");
-                    route[carRow, carCol] = 'C';
-                    PrintingRoute(route);
-                    return;
+                    break;
+                }
+
+                else if (tunnelsPositions.ContainsKey(carRow))
+                {
+                    if (tunnelsPositions[carRow] == carCol)
+                    {
+                        kilometersPassed += 30;
+                        carRow = tunnelsPositions.FirstOrDefault(k => k.Key != carRow).Key;
+                        carCol = tunnelsPositions[carRow];
+                        RemovingTunnels(tunnelsPositions, raceRoute);
+                    }
                 }
             }
+            Console.WriteLine($"Distance covered {kilometersPassed} km.");
+            raceRoute[carRow, carCol] = 'C';
+            PrintRaceRoute(raceRoute);
         }
 
-        private static void PrintingRoute(char[,] route)
+        private static void PrintRaceRoute(char[,] raceRoute)
         {
-            for (int i = 0; i < route.GetLength(0); i++)
+            for (int row = 0; row < raceRoute.GetLength(0); row++)
             {
-                for (int j = 0; j < route.GetLength(1); j++)
+                for (int col = 0; col < raceRoute.GetLength(0); col++)
                 {
-                    Console.Write(route[i, j]);
+                    Console.Write(raceRoute[row, col]);
                 }
                 Console.WriteLine();
             }
         }
 
-        private static List<int[]> FindTunnels(char[,] route)
+        private static void RemovingTunnels(Dictionary<int, int> tunnelsPositions, char[,] raceRoute)
         {
-            List<int[]> coordinates = new List<int[]>();
-            for (int i = 0; i < route.GetLength(0); i++)
+            foreach (var (row, col) in tunnelsPositions)
             {
-                for (int j = 0; j < route.GetLength(1); j++)
-                {
-                    if (route[i, j] == 'T')
-                    {
-                        coordinates.Add(new int[2] { i, j });
-                    }
-                }
+                raceRoute[row, col] = '.';
             }
-
-            return coordinates;
         }
     }
 }
