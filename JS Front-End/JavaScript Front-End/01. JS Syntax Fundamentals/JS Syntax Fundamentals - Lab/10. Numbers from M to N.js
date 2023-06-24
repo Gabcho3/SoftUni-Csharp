1e29d5640fde72a0fb8d532a563a776860629a41
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-06-24 16:27:54 +0300

    Added Solution 10

---
+++ JS Front-End/JavaScript Front-End/01. JS Syntax Fundamentals/JS Syntax Fundamentals - Lab/10. Numbers from M to N.js
@@ -0,0 +1,7 @@
+function printNumbersFromMToN(m, n){
+    for(let num = m; num >= n; num--){
+        console.log(num);
+    }
+}
+printNumbersFromMToN(6, 2); //6 5 4 3 2
+printNumbersFromMToN(4, 1); //4 3 2 1
