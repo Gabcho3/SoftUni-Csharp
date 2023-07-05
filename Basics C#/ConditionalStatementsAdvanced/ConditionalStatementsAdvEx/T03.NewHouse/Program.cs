5b18e1db8f8215dc09710d834a0cf3e175d27254
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-05 14:58:23 +0300

    Added improved code (Solution 03)

--- Basics C#/ConditionalStatementsAdvanced/ConditionalStatementsAdvEx/T03.NewHouse/Program.cs
+++ Basics C#/ConditionalStatementsAdvanced/ConditionalStatementsAdvEx/T03.NewHouse/Program.cs
@@ -6,122 +6,65 @@ namespace T03.NewHouse
     {
         static void Main(string[] args)
         {
-            string flo = Console.ReadLine();
-            int tot = int.Parse(Console.ReadLine());
-            int buj = int.Parse(Console.ReadLine());
-            double price = 0.00;
+            string flowersType = Console.ReadLine(); //Roses || Dahlias || Tulips || Narcissus || Gladiolus
+            int flowersCount = int.Parse(Console.ReadLine()); //брой цветя, цяло число
+            int budget = int.Parse(Console.ReadLine()); //бюджата, цяло число
+            double totalPrice = 0; //общата цена за цветята
 
-            if (flo == "Roses")
+            switch (flowersType)
             {
+                case "Roses":
+                    totalPrice = flowersCount * 5; //пресмятаме общата цена според табличката
+                    if(flowersCount > 80)
+                    {
+                        totalPrice *= 0.90; //ако розите са повече от 80, отстъпка 10% (100% - 10% = 90% = 0.90)
+                    }
+                    break;
 
-                if (tot <= 80)
-                {
-                    price = tot * 5.00;
-                }
-                else if (tot > 80)
-                {
-                    price = tot * 5.00 - (tot * 5.00 * 0.10);
-                }
-                if (buj >= price)
-                {
-                    Console.WriteLine($"Hey, you have a great garden with {tot} {flo} and {buj - price:F2} leva left.");
-                }
-                if (buj < price)
-                {
-                    Console.WriteLine($"Not enough money, you need {price - buj:F2} leva more.");
-                }
+                case "Dahlias":
+                    totalPrice = flowersCount * 3.80; //пресмятаме общата цена според табличката
+                    if (flowersCount > 90)
+                    {
+                        totalPrice *= 0.85; //ако далиите са повече от 90, отстъпка 15% (100% - 15% = 85% = 0.85)
+                    }
+                    break;
+
+                case "Tulips":
+                    totalPrice = flowersCount * 2.80; //пресмятаме общата цена според табличката
+                    if (flowersCount > 80)
+                    {
+                        totalPrice *= 0.85; //ако лалетата са повече от 80, отстъпка 15% (100% - 15% = 85% = 0.85)
+                    }
+                    break;
+
+                case "Narcissus":
+                    totalPrice = flowersCount * 3; //пресмятаме общата цена според табличката
+                    if (flowersCount < 120)
+                    {
+                        totalPrice *= 1.15; //ако нарцисите са по-малко от 120, оскъпяване 15% (100% + 15% = 115% = 1.15)
+                    }
+                    break;
+
+                case "Gladiolus":
+                    totalPrice = flowersCount * 2.50; //пресмятаме общата цена според табличката
+                    if (flowersCount < 80)
+                    {
+                        totalPrice *= 1.20; //ако гладиолите са по-малко от 80, оскъпяване 20% (100% + 20% = 120% = 1.20)
+                    }
+                    break;
             }
 
-            if (flo == "Dahlias")
+            //ПРАВИМ ПРОВЕРКИ ДАЛИ БЮДЖЕТА НИ СТИГА
+            if(budget >= totalPrice)
             {
-                price = tot * 3.80;
-                if (tot <= 90)
-                {
-                    price = tot * 3.80;
-                }
-
-                else if (tot > 90)
-                {
-                    price = tot * 3.80 - (tot * 3.80 * 0.15);
-                }
-                if (buj >= price)
-                {
-                    Console.WriteLine($"Hey, you have a great garden with {tot} {flo} and {buj - price:F2} leva left.");
-                }
-                if (buj < price)
-                {
-                    Console.WriteLine($"Not enough money, you need {price - buj:F2} leva more.");
-                }
-
+                double leftSum = budget - totalPrice; //останалата сума
+                Console.WriteLine($"Hey, you have great garden with {flowersCount} {flowersType} and {leftSum:f2} leva left.");
+                //:f2 -> закръгляне до второто число след десетичната запетайка
             }
-
-            if (flo == "Tulips")
+            else //ако бюджета не стига
             {
-                price = tot * 2.80;
-                if (tot <= 80)
-                {
-                    price = tot * 2.80;
-                }
-
-
-                else if (tot > 80)
-                {
-                    price = tot * 2.80 - (tot * 2.80 * 0.15);
-                }
-                if (buj >= price)
-                {
-                    Console.WriteLine($"Hey, you have a great garden with {tot} {flo} and {buj - price:F2} leva left.");
-                }
-                if (buj < price)
-                {
-                    Console.WriteLine($"Not enough money, you need {price - buj:F2} leva more.");
-                }
-            }
-
-
-            if (flo == "Narcissus")
-            {
-                price = tot * 3.00;
-                if (tot >= 120)
-                {
-                    price = tot * 3.00;
-                }
-
-
-                else if (tot < 120)
-                {
-                    price = tot * 3.00 + (tot * 3.00 * 0.15);
-                }
-                if (buj >= price)
-                {
-                    Console.WriteLine($"Hey, you have a great garden with {tot} {flo} and {buj - price:F2} leva left.");
-                }
-                if (buj < price)
-                {
-                    Console.WriteLine($"Not enough money, you need {price - buj:F2} leva more.");
-                }
-            }
-
-            if (flo == "Gladiolus")
-            {
-
-                if (tot >= 80)
-                {
-                    price = tot * 2.50;
-                }
-
-                else if (tot < 80)
-                {
-                    price = tot * 2.50 + (tot * 2.50 * 0.20);
-                }
-                if (buj >= price)
-                {
-                    Console.WriteLine($"Hey, you have a great garden with {tot} {flo} and {buj - price:F2} leva left.");
-                }
-                if (buj < price)
-                {
-                    Console.WriteLine($"Not enough money, you need {price - buj:F2} leva more.");
-                }
+                double needSum = totalPrice - budget; //нужната сума
+                Console.WriteLine($"Not enough money, you need {needSum:f2} leva more.");
             }
         }
     }
