5f32418347354768928c45d960ff20f55bed75ba
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-11 21:51:00 +0300

    Added Solution 08

---
+++ JS Front-End/JavaScript Front-End/03. Objects and Classes/Object and Classes - Ex/08. Piccolo.js
@@ -0,0 +1,32 @@
+function registerCarOnParkingLot(input){
+    let carsOnParking = new Set();
+    for (let i = 0; i < input.length; i++) {
+        const element = input[i];
+        const [direction, carNumber] = element.split(', ');
+        switch(direction){
+            case 'IN': carsOnParking.add(carNumber); break;
+            case 'OUT': 
+                carsOnParking.delete(carNumber);
+                break;
+        }
+    }
+    carsOnParking = Array.from(carsOnParking).sort();
+    const output = carsOnParking.length > 0 ? carsOnParking.join('\n') : 'Parking Lot is Empty';
+    console.log(output);
+}
+registerCarOnParkingLot(['IN, CA2844AA',
+'IN, CA1234TA',
+'OUT, CA2844AA',
+'IN, CA9999TT',
+'IN, CA2866HI',
+'OUT, CA1234TA',
+'IN, CA2844AA',
+'OUT, CA2866HI',
+'IN, CA9876HH',
+'IN, CA2822UU']);
+
+registerCarOnParkingLot(['IN, CA2844AA',
+'IN, CA1234TA',
+'OUT, CA2844AA',
+'OUT, CA1234TA']
+);
