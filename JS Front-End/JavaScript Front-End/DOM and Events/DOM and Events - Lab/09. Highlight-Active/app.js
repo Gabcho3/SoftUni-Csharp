32bbbbeac3bb9351a62ac557bbda2e1bc61bb2c6
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-16 19:54:08 +0300

    Added Solution 09

--- JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/09. Highlight-Active/app.js
+++ JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/09. Highlight-Active/app.js
@@ -1,3 +1,13 @@
 function focused() {
-    console.log('TODO:...');
+  const inputs = Array.from(document.querySelectorAll("input"));
+
+  inputs.forEach((input) => {
+    input.addEventListener("focus", (e) => {
+      e.target.parentElement.classList.add("focused");
+    });
+
+    input.addEventListener("blur", (e) => {
+      e.target.parentElement.classList.remove("focused");
+    });
+  });
 }
