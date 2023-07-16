3b8f2ce11f170f535813f9de32910315ea043baf
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-16 18:36:18 +0300

    Added Solution 03

--- JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/03. Collect-List-Items/CollectListItems.js
+++ JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,7 @@
 function extractText() {
-    // TODO
+  let textToAdd = "";
+  Array.from(document.querySelectorAll("ul li")).forEach(
+    (li) => (textToAdd += li.textContent + "\n")
+  );
+  document.querySelector("#result").textContent = textToAdd;
 }
