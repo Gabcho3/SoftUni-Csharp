b9426c1859b39e084104a52f03a85a4713c8bbf6
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-16 19:34:25 +0300

    Added Solution 06

--- JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/06. Add-Delete/app.js
+++ JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/06. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+  const list = document.getElementById("items");
+  const itemToAdd = document.querySelector("#newItemText").value;
+
+  let listItem = document.createElement("li");
+  listItem.textContent = itemToAdd;
+
+  let remove = document.createElement("a");
+  remove.href = "#";
+  remove.textContent = "[Delete]";
+  listItem.appendChild(remove);
+
+  remove.addEventListener("click", deleteItem);
+
+  list.appendChild(listItem);
+  document.querySelector("#newItemText").value = "";
+
+  function deleteItem() {
+    listItem.remove();
+  }
 }
