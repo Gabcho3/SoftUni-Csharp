2be6a78b3e64d3533e6da7148ad92c9689ed2712
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-16 19:45:01 +0300

    Added Solution 08

--- JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/08. Sum-Table/SumTable.js
+++ JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+  const prices = Array.from(
+    document.querySelectorAll("td:nth-child(even):not(#sum)")
+  );
+  const sum = prices.reduce((acc, curr) => {
+    return acc + Number(curr.innerText);
+  }, 0);
 
+  document.getElementById("sum").innerText = sum;
 }
