dd75ed3debda356f1b22d5093d8bc5dfe67b50b9
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-16 18:47:29 +0300

    Added Solution 04

--- JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/04. List-Of-Items/app.js
+++ JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/04. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+  const itemToAdd = document.querySelector("#newItemText").value;
+
+  const newLi = document.createElement("li");
+  newLi.textContent = itemToAdd;
+
+  const list = document.getElementById("items");
+  list.appendChild(newLi);
+
+  document.querySelector("#newItemText").value = "";
 }
