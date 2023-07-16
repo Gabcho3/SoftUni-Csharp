8fd044265f4de90383e6713eb25268b17a635df4
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-16 19:15:08 +0300

    Added Solution 05

--- JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/05. Delete-From-Table/app.js
+++ JS Front-End/JavaScript Front-End/DOM and Events/DOM and Events - Lab/05. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  let emails = Array.from(
+    document.querySelectorAll("tbody tr td:nth-child(even)")
+  );
+  const emailToMatch = document.querySelector("input[type=text]").value;
+
+  const elementToRemove = emails.find(
+    (email) => email.innerText === emailToMatch
+  );
+
+  if (elementToRemove) {
+    elementToRemove.parentElement.remove();
+    document.getElementById("result").textContent = "Deleted";
+  } else {
+    document.getElementById("result").textContent = "Not found.";
+  }
 }
