b94b15ec395eb4c0cc739ce7ee9f03198c986d34
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-17 19:18:27 +0300

    Added Solution 04

--- JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/04. Locked-Profile/app.js
+++ JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/04. Locked-Profile/app.js
@@ -1,3 +1,17 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const profiles = Array.from(document.querySelectorAll("div.profile"));
+
+  profiles.forEach((profile) => {
+    const unlockRadio = profile.querySelector("input[value=unlock]");
+    const button = profile.querySelector("button");
+    const divToShow = profile.querySelector("div");
+    button.addEventListener("click", (e) => {
+      if (unlockRadio.checked) {
+        divToShow.style.display =
+          divToShow.style.display === "block" ? "none" : "block";
+        button.innerText =
+          button.innerText === "Show more" ? "Hide it" : "Show more";
+      }
+    });
+  });
 }
