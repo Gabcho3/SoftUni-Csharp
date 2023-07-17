773bc9393e8a0c9883663e579d8606eed2d0aaaa
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-17 22:48:33 +0300

    Added Solution 05

--- JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/05. Fill-Dropdown/dropdown.js
+++ JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+  const textToAdd = document.querySelector("#newItemText").value;
+  const valueToAdd = document.querySelector("#newItemValue").value;
+
+  const option = document.createElement("option");
+  option.textContent = textToAdd;
+  option.value = valueToAdd;
+
+  document.getElementById("menu").appendChild(option);
+
+  document.querySelector("#newItemText").value = "";
+  document.querySelector("#newItemValue").value = "";
 }
