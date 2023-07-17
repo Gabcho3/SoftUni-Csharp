17de4c0f8159d91f2215128e713ebeec6f699bf4
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-17 18:07:35 +0300

    Added Solution 03

--- JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/03. Accordion/accordion.js
+++ JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/03. Accordion/accordion.js
@@ -1,3 +1,8 @@
 function toggle() {
-    console.log('TODO:...');
+  const divToShow = document.querySelector("#extra");
+  const moreButton = document.querySelector(".head .button");
+
+  divToShow.style.display =
+    divToShow.style.display === "block" ? "none" : "block";
+  moreButton.innerText = moreButton.innerText === "More" ? "Less" : "More";
 }
