91c5c1e190f8f7fa9eec86c42cbfc29558b671b7
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-17 17:43:41 +0300

    Added Solution 02

--- JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/02. Sections/script.js
+++ JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/02. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+  const paragraphsToAdd = words.map((word) => {
+    const paragraph = document.createElement("p");
+    paragraph.style.display = "none";
+    paragraph.innerText = word;
+    return paragraph;
+  });
+
+  paragraphsToAdd.forEach((paragraph) => {
+    const div = document.createElement("div");
+
+    div.appendChild(paragraph);
+
+    div.addEventListener("click", (e) => {
+      paragraph.style.display = "block";
+    });
+
+    document.getElementById("content").appendChild(div);
+  });
 }
