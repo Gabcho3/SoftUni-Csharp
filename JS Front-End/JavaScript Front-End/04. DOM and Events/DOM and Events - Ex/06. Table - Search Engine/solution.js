eddf4c558022c77f0af5e7f0072797942e695fdd
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-17 23:03:30 +0300

    Added Solution 06

--- JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/06. Table - Search Engine/solution.js
+++ JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/06. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    const cells = Array.from(document.querySelectorAll("tbody td"));
+    const stringToMatch = document.getElementById("searchField").value;
 
-   }
+    const selectedRows = [];
+    cells.forEach((cell) => {
+      if (cell.textContent.toLowerCase().includes(stringToMatch)) {
+        selectedRows.push(cell);
+      }
+      cell.parentElement.classList.remove("select");
+    });
+
+    selectedRows.forEach((cell) => {
+      cell.parentElement.classList.add("select");
+    });
+
+    document.getElementById("searchField").value = "";
+  }
 }
