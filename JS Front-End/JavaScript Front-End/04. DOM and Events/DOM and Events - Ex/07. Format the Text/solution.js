41dd87d1a3bc3c75c6f62574cf28cfb8100a644d
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-18 15:37:19 +0300

    Added Solution 07

--- JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/07. Format the Text/solution.js
+++ JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - Ex/07. Format the Text/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO
+  let texts = document.getElementById("input").value.split(".");
+  texts = texts.filter((text) => text.length > 0);
+
+  let textToAdd = [];
+  for (let i = 1; i <= texts.length; i++) {
+    textToAdd.push(texts[i - 1]);
+
+    if (i % 3 === 0) {
+      appendParagraph(textToAdd);
+      textToAdd = [];
+    }
+  }
+
+  if (textToAdd.length > 0 && textToAdd.length < 3) {
+    appendParagraph(textToAdd);
+  }
+
+  function appendParagraph(textToAdd) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = textToAdd.join(".") + ".";
+    document.getElementById("output").appendChild(paragraph);
+  }
 }
