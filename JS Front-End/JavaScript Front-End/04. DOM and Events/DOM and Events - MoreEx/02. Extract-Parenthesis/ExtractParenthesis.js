6c5237d0396ee84e58c02eb47904d22c9cec0fb1
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-19 16:45:37 +0300

    Added Solution 02

--- JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - MoreEx/02. Extract-Parenthesis/ExtractParenthesis.js
+++ JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - MoreEx/02. Extract-Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,21 @@
 function extract(content) {
+  const text = document.getElementById(content).innerText;
 
+  let parenthesizedText = [];
+  let startIndex = 0;
+  let lastIndex = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    const element = text[i];
+
+    if (element === "(") {
+      startIndex = i;
+    }
+    if (element === ")") {
+      lastIndex = i;
+      parenthesizedText.push(text.substring(startIndex, lastIndex));
+    }
+  }
+
+  return parenthesizedText.join("; ");
 }
