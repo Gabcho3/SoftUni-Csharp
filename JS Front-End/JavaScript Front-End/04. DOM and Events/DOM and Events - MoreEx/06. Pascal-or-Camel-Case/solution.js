786ada32b4da42c1e294755062f9c4f0d4bd799a
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-19 19:05:27 +0300

    Added Solution 06

--- JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - MoreEx/06. Pascal-or-Camel-Case/solution.js
+++ JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - MoreEx/06. Pascal-or-Camel-Case/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+  const words = document.getElementById("text").value.split(" ");
+  const convention = document.getElementById("naming-convention").value;
+  const result = document.getElementById("result");
+  let output = "";
+
+  if (convention != "Pascal Case" && convention != "Camel Case") {
+    result.textContent = "Error!";
+  } else {
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i].toLowerCase();
+      if (i == 0 && convention === "Camel Case") {
+        output += word;
+      }
+
+      if (i != 0 || convention === "Pascal Case") {
+        output += word[0].toUpperCase() + word.substring(1);
+      }
+    }
+    result.textContent = output;
+  }
 }
