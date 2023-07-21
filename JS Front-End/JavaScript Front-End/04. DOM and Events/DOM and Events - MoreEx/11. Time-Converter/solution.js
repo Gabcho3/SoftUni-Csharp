43b156367a0e369bb38fc0b0992368d21a546ba7
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-21 16:04:37 +0300

    Added Solution 11

--- JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - MoreEx/11. Time-Converter/solution.js
+++ JS Front-End/JavaScript Front-End/04. DOM and Events/DOM and Events - MoreEx/11. Time-Converter/solution.js
@@ -1,4 +1,33 @@
 function attachEventsListeners() {
+  const daysButton = document.getElementById("daysBtn");
+  const hoursButton = document.getElementById("hoursBtn");
+  const minutesButton = document.getElementById("minutesBtn");
+  const secondsButton = document.getElementById("secondsBtn");
 
-    console.log('TODO:...');
+  daysButton.addEventListener("click", convertDays);
+  hoursButton.addEventListener("click", (e) => {
+    const hours = Number(document.getElementById("hours").value);
+    document.getElementById("days").value = hours / 24;
+    convertDays();
+  });
+
+  minutesButton.addEventListener("click", (e) => {
+    const minutes = Number(document.getElementById("minutes").value);
+    document.getElementById("days").value = minutes / 1440;
+    convertDays();
+  });
+
+  secondsButton.addEventListener("click", (e) => {
+    const seconds = Number(document.getElementById("seconds").value);
+    document.getElementById("days").value = seconds / 86400;
+    convertDays();
+  });
+
+  function convertDays() {
+    const days = Number(document.getElementById("days").value);
+
+    document.getElementById("hours").value = days * 24;
+    document.getElementById("minutes").value = days * 1440;
+    document.getElementById("seconds").value = days * 86400;
+  }
 }
