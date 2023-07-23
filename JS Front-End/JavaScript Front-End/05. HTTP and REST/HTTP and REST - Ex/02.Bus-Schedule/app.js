718749879ded7ff9a7eb1d54a4e1113a60d589c0
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-23 23:51:21 +0300

    Added Solution 02

--- JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - Ex/02.Bus-Schedule/app.js
+++ JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - Ex/02.Bus-Schedule/app.js
@@ -1,16 +1,35 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
+  const departButton = document.querySelector("#depart");
+  const arriveButton = document.querySelector("#arrive");
+  const infoBox = document.querySelector("#info span");
 
-    async function arrive() {
-        // TODO:
-    }
+  let busInfo = {
+    name: "",
+    next: "depot",
+  };
 
-    return {
-        depart,
-        arrive
-    };
+  function depart() {
+    fetch(`http://localhost:3030/jsonstore/bus/schedule/${busInfo.next}`)
+      .then((res) => res.json())
+      .then((busStop) => {
+        busInfo = busStop;
+        infoBox.textContent = `Next stop ${busInfo.name}`;
+      });
+
+    departButton.disabled = true;
+    arriveButton.disabled = false;
+  }
+
+  async function arrive() {
+    infoBox.textContent = `Arriving at  ${busInfo.name}`;
+    departButton.disabled = false;
+    arriveButton.disabled = true;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
