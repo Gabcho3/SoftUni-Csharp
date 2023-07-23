ee8a8a4b482bdfcc724bd1fe446651ebd772ab51
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-23 20:06:07 +0300

    Added Solution 01

--- JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - Ex/01.Bus-Stop/app.js
+++ JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - Ex/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
 function getInfo() {
-    
+  const busStopId = document.querySelector("#stopId").value;
+  const stopName = document.querySelector("#stopName");
+  const list = document.querySelector("#buses");
+
+  list.innerHTML = "";
+
+  fetch(`http://localhost:3030/jsonstore/bus/businfo/${busStopId}`)
+    .then((res) => res.json())
+    .then((busStop) => {
+      stopName.textContent = busStop.name;
+
+      const data = Object.entries(busStop.buses);
+      data.forEach(([busId, time]) => {
+        const item = document.createElement("li");
+        item.textContent = `Bus ${busId} arrives in ${time} minutes`;
+
+        list.appendChild(item);
+      });
+    })
+    .catch(() => {
+      stopName.textContent = "Error";
+    });
 }
