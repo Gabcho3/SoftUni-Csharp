6ee14a25e8228893bb79e30df4e52d1d4be7479f
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-25 02:14:57 +0300

    Added Solution 07

--- JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - Ex/07.Students/app.js
+++ JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - Ex/07.Students/app.js
@@ -1,5 +1,49 @@
-function attachEvents() {
-  // TODO:
-}
+async function attachEvents() {
+  const submitButton = document.getElementById("submit");
 
+  submitButton.addEventListener("click", (e) => {
+    const inputs = Array.from(document.querySelectorAll("input[type=text"));
+    const student = {};
+    inputs.forEach((input) => {
+      if (input.value) {
+        student[input.name] = input.value;
+      }
+    });
+
+    fetch("http://localhost:3030/jsonstore/collections/students", {
+      method: "post",
+      body: JSON.stringify(student),
+    });
+
+    showResults();
+  });
+}
+function showResults() {
+  document.querySelector("#results tbody").innerHTML = "";
+
+  fetch("http://localhost:3030/jsonstore/collections/students")
+    .then((res) => res.json())
+    .then((students) => {
+      for (const student of Object.values(students)) {
+        const tr = document.createElement("tr");
+        for (const [key, value] of Object.entries(student)) {
+          if (key === "_id") {
+            continue;
+          }
+          const th = document.createElement("th");
+
+          if (key === "grade") {
+            th.textContent = Number(value).toFixed(2);
+          } else {
+            th.textContent = value;
+          }
+
+          tr.appendChild(th);
+        }
+        if (tr.innerHTML) {
+          document.querySelector("#results tbody").appendChild(tr);
+        }
+      }
+    });
+}
 attachEvents();
