45a31dfd277e6c2e27ac94a0d4a21d77e999257d
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-25 00:12:12 +0300

    Added Solution 05

--- JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - Ex/05.Messenger/app.js
+++ JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - Ex/05.Messenger/app.js
@@ -1,5 +1,39 @@
 function attachEvents() {
-    // TODO:
-}
+  const sendButton = document.getElementById("submit");
+  const refreshButton = document.getElementById("refresh");
 
+  sendButton.addEventListener("click", postMessages);
+  refreshButton.addEventListener("click", refreshMessages);
+}
+function postMessages() {
+  const authorName = document.querySelector("input[name=author").value;
+  const messageToSend = document.querySelector("input[name=content]").value;
+
+  if (!authorName || !messageToSend) {
+    document.getElementById("messages").value = "Error!";
+    return;
+  }
+
+  const obj = {
+    author: authorName,
+    content: messageToSend,
+  };
+
+  fetch("http://localhost:3030/jsonstore/messenger", {
+    method: "post",
+    headers: { "Content-type": "aplication/json" },
+    body: JSON.stringify(obj),
+  });
+}
+function refreshMessages() {
+  fetch("http://localhost:3030/jsonstore/messenger")
+    .then((res) => res.json())
+    .then((messages) => {
+      const ouput = [];
+      for (const obj of Object.values(messages)) {
+        ouput.push(`${obj.author}: ${obj.content}`);
+      }
+      document.getElementById("messages").value = ouput.join("\n");
+    });
+}
 attachEvents();
