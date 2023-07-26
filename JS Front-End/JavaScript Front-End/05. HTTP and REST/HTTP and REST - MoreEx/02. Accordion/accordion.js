3030785bd1919c3c28e6a11c31fa80177447c205
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-07-26 18:53:55 +0300

    Added Solution 02

--- JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - MoreEx/02. Accordion/accordion.js
+++ JS Front-End/JavaScript Front-End/05. HTTP and REST/HTTP and REST - MoreEx/02. Accordion/accordion.js
@@ -1,3 +1,44 @@
 function solution() {
-    //TODO .....
+  fetch("http://localhost:3030/jsonstore/advanced/articles/list")
+    .then((res) => res.json())
+    .then((articles) => {
+      for (let i = 0; i < articles.length; i++) {
+        const articleObj = articles[i];
+
+        const div = document.createElement("div");
+        div.classList.add("accordion");
+        div.innerHTML = `
+            <div class="head">
+                <span>${articleObj.title}</span>
+                <button class="button" id="${articleObj["_id"]}">More</button>
+            </div>
+            <div class="extra">
+                <p></p>
+            </div>
+        </div>`;
+
+        document.getElementById("main").appendChild(div);
+      }
+
+      const moreButtons = Array.from(document.getElementsByClassName("button"));
+
+      moreButtons.forEach((button) => {
+        const divToShow =
+          button.parentElement.parentElement.querySelector(".extra");
+
+        button.addEventListener("click", (e) => {
+          fetch(
+            `http://localhost:3030/jsonstore/advanced/articles/details/${button.id} `
+          )
+            .then((res) => res.json())
+            .then((article) => {
+              divToShow.querySelector("p").textContent = article.content;
+              button.textContent =
+                button.textContent === "More" ? "Less" : "More";
+              divToShow.style.display =
+                divToShow.style.display === "block" ? "none" : "block";
+            });
+        });
+      });
+    });
 }
