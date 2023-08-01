fc9ef617615da3e9b73b5556576a3870c653b67e
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-08-01 21:36:16 +0300

    Added Solution 03

--- JS Front-End/JavaScript Front-End/06. Exams/03. RegularExam - April 2023/03. Sprint Board/solve.js
+++ JS Front-End/JavaScript Front-End/06. Exams/03. RegularExam - April 2023/03. Sprint Board/solve.js
@@ -1,6 +1,131 @@
-// TODO:
-function attachEvents() {
+const API_URL = "http://localhost:3030/jsonstore/tasks/";
 
+const tasksSelector = {
+  ToDo: document.querySelector("#todo-section ul"),
+  "In Progress": document.querySelector("#in-progress-section ul"),
+  "Code Review": document.querySelector("#code-review-section ul"),
+  Done: document.querySelector("#done-section ul"),
+};
+
+const buttonsContent = {
+  ToDo: "Move to In Progress",
+  "In Progress": "Move to Code Review",
+  "Code Review": "Move to Done",
+  Done: "Close",
+};
+
+const inputFields = {
+  title: document.getElementById("title"),
+  description: document.getElementById("description"),
+};
+
+const moveStatuses = {
+  ToDo: "In Progress",
+  "In Progress": "Code Review",
+  "Code Review": "Done",
+};
+
+let allTasks = [];
+
+function attachEvents() {
+  document
+    .getElementById("load-board-btn")
+    .addEventListener("click", loadTasks);
+
+  document.getElementById("create-task-btn").addEventListener("click", addTask);
 }
 
+async function loadTasks() {
+  Object.values(tasksSelector).forEach((selector) => {
+    selector.innerHTML = "";
+  });
+
+  const response = await fetch(API_URL);
+  const tasks = await response.json();
+
+  allTasks = [];
+  Object.values(tasks).forEach((task) => {
+    allTasks.push(task);
+    const listItem = createElement(
+      "li",
+      null,
+      ["task"],
+      task._id,
+      tasksSelector[task.status]
+    );
+    createElement("h3", task.title, null, null, listItem);
+    createElement("p", task.description, null, null, listItem);
+    const moveButton = createElement(
+      "button",
+      buttonsContent[task.status],
+      null,
+      null,
+      listItem
+    );
+
+    moveButton.addEventListener("click", moveTask);
+  });
+}
+
+function createElement(type, textContent, classes, id, parentElement) {
+  const element = document.createElement(type);
+
+  if (textContent) {
+    element.textContent = textContent;
+  }
+
+  if (classes && classes.length > 0) {
+    element.classList.add(...classes);
+  }
+
+  if (id) {
+    element.id = id;
+  }
+
+  if (parentElement) {
+    parentElement.appendChild(element);
+  }
+
+  return element;
+}
+
+function addTask() {
+  const task = {
+    title: inputFields["title"].value,
+    description: inputFields["description"].value,
+    status: "ToDo",
+  };
+
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(task),
+  });
+
+  Object.values(inputFields).forEach((input) => {
+    input.value = "";
+  });
+
+  loadTasks();
+}
+
+function moveTask(e) {
+  const id = e.target.parentElement.id;
+  const task = allTasks.find((task) => task._id === id);
+  task.status = moveStatuses[task.status];
+
+  let method = "PATCH";
+  let body = JSON.stringify(task);
+
+  if (e.target.textContent === "Close") {
+    method = "DELETE";
+    body = undefined;
+  }
+
+  fetch(API_URL + id, {
+    method,
+    body,
+  });
+
+  loadTasks();
+}
 attachEvents();
--- JS Front-End/JavaScript Front-End/06. Exams/03. RegularExam - April 2023/03. Sprint Board/tests/e2e.test.js
+++ JS Front-End/JavaScript Front-End/06. Exams/03. RegularExam - April 2023/03. Sprint Board/tests/e2e.test.js
@@ -1,7 +1,7 @@
-const { chromium } = require('playwright-chromium');
-const { expect } = require('chai');
+const { chromium } = require("playwright-chromium");
+const { expect } = require("chai");
 
-const host = 'http://localhost:3000'; // Application host (NOT service host - that can be anything)
+const host = "http://localhost:3000"; // Application host (NOT service host - that can be anything)
 
 const DEBUG = false;
 const slowMo = 500;
@@ -9,34 +9,34 @@ const slowMo = 500;
 const mockData = {
   list: [
     {
-      title: 'Bug 1',
-      description: 'My first bug',
-      status: 'ToDo',
-      _id: '1001',
+      title: "Bug 1",
+      description: "My first bug",
+      status: "ToDo",
+      _id: "1001",
     },
     {
-      title: 'Bug 2',
-      description: 'My second bug',
-      status: 'In Progress',
-      _id: '1002',
+      title: "Bug 2",
+      description: "My second bug",
+      status: "In Progress",
+      _id: "1002",
     },
     {
-      title: 'Bug 3',
-      description: 'My third bug',
-      status: 'Code Review',
-      _id: '1003',
+      title: "Bug 3",
+      description: "My third bug",
+      status: "Code Review",
+      _id: "1003",
     },
     {
-      title: 'Bug 4',
-      description: 'My fourth bug',
-      status: 'Done',
-      _id: '1004',
+      title: "Bug 4",
+      description: "My fourth bug",
+      status: "Done",
+      _id: "1004",
     },
   ],
 };
 
 const endpoints = {
-  catalog: '/jsonstore/tasks',
+  catalog: "/jsonstore/tasks",
   byId: (id) => `/jsonstore/tasks/${id}`,
 };
 
@@ -44,7 +44,7 @@ let browser;
 let context;
 let page;
 
-describe('E2E tests', function () {
+describe("E2E tests", function () {
   // Setup
   this.timeout(DEBUG ? 120000 : 7000);
   before(
@@ -64,140 +64,145 @@ describe('E2E tests', function () {
     await context.close();
   });
 
-  describe('Sprint Board Tests', () => {
-    it('Load Board (loads all in correct categories)', async () => {
+  describe("Sprint Board Tests", () => {
+    it("Load Board (loads all in correct categories)", async () => {
       const data = mockData.list;
-      const [ firstTask, secondTask, thirdTask, fourthTask ] = data;
+      const [firstTask, secondTask, thirdTask, fourthTask] = data;
       const { get } = await handle(endpoints.catalog);
       get(data);
 
       await page.goto(host);
-      await page.waitForSelector('#load-board-btn');
+      await page.waitForSelector("#load-board-btn");
 
-      await page.click('#load-board-btn');
+      await page.click("#load-board-btn");
 
-      const [ todoTask ] = await page.$$eval(`#todo-section li`, (t) =>
+      const [todoTask] = await page.$$eval(`#todo-section li`, (t) =>
         t.map((s) => s.textContent)
       );
-      const [ inProgressTask ] = await page.$$eval(`#in-progress-section li`, (t) =>
-        t.map((s) => s.textContent)
+      const [inProgressTask] = await page.$$eval(
+        `#in-progress-section li`,
+        (t) => t.map((s) => s.textContent)
       );
-      const [ codeReviewTask ] = await page.$$eval(`#code-review-section li`, (t) =>
-        t.map((s) => s.textContent)
+      const [codeReviewTask] = await page.$$eval(
+        `#code-review-section li`,
+        (t) => t.map((s) => s.textContent)
       );
-      const [ doneTask ] = await page.$$eval(`#done-section li`, (t) =>
+      const [doneTask] = await page.$$eval(`#done-section li`, (t) =>
         t.map((s) => s.textContent)
       );
 
-      expect(todoTask).to.equal(`${firstTask.title}${firstTask.description}Move to In Progress`);
-      expect(inProgressTask).to.equal(`${secondTask.title}${secondTask.description}Move to Code Review`);
-      expect(codeReviewTask).to.equal(`${thirdTask.title}${thirdTask.description}Move to Done`);
-      expect(doneTask).to.equal(`${fourthTask.title}${fourthTask.description}Close`);
+      expect(todoTask).to.equal(
+        `${firstTask.title}${firstTask.description}Move to In Progress`
+      );
+      expect(inProgressTask).to.equal(
+        `${secondTask.title}${secondTask.description}Move to Code Review`
+      );
+      expect(codeReviewTask).to.equal(
+        `${thirdTask.title}${thirdTask.description}Move to Done`
+      );
+      expect(doneTask).to.equal(
+        `${fourthTask.title}${fourthTask.description}Close`
+      );
     });
 
-    it('Create Task (successful create & clear inputs)', async () => {
+    it("Create Task (successful create & clear inputs)", async () => {
       const data = mockData.list[0];
       await page.goto(host);
 
       const { post } = await handle(endpoints.catalog);
       const { onRequest } = post();
 
-      await page.waitForSelector('#title');
-      await page.waitForSelector('#description');
+      await page.waitForSelector("#title");
+      await page.waitForSelector("#description");
 
-      await page.fill('#title', data.title + '1');
-      await page.fill('#description', data.description + '1');
+      await page.fill("#title", data.title + "1");
+      await page.fill("#description", data.description + "1");
 
       const [request] = await Promise.all([
         onRequest(),
-        page.click('#create-task-btn'),
+        page.click("#create-task-btn"),
       ]);
 
       const postData = JSON.parse(request.postData());
 
-      const [ titleValue ] = await page.$$eval(`#title`, (t) =>
+      const [titleValue] = await page.$$eval(`#title`, (t) =>
         t.map((s) => s.value)
       );
-      const [ descriptionValue ] = await page.$$eval(`#description`, (t) =>
+      const [descriptionValue] = await page.$$eval(`#description`, (t) =>
         t.map((s) => s.value)
       );
 
-      expect(postData.title).to.equal(data.title + '1');
-      expect(postData.description).to.equal(data.description + '1');
-      expect(titleValue).to.equal('');
-      expect(descriptionValue).to.equal('');
+      expect(postData.title).to.equal(data.title + "1");
+      expect(postData.description).to.equal(data.description + "1");
+      expect(titleValue).to.equal("");
+      expect(descriptionValue).to.equal("");
     });
-    
-    it('Move Task (from ToDo to In Progress)', async () => {
+
+    it("Move Task (from ToDo to In Progress)", async () => {
       const data = mockData.list[0];
       await page.goto(host);
       const { patch } = await handle(endpoints.byId(data._id));
       const { onRequest } = patch({ id: data._id });
 
-      await page.click('#load-board-btn');
-      await page.waitForSelector('#todo-section li button');
+      await page.click("#load-board-btn");
+      await page.waitForSelector("#todo-section li button");
       const [request] = await Promise.all([
         onRequest(),
-        page.click('#todo-section li button'),
+        page.click("#todo-section li button"),
       ]);
 
       const postData = JSON.parse(request.postData());
 
-      expect(postData.status).to.equal('In Progress');
+      expect(postData.status).to.equal("In Progress");
     });
 
-    it('Move Task (from In Progress to Code Review)', async () => {
+    it("Move Task (from In Progress to Code Review)", async () => {
       const data = mockData.list[1];
       await page.goto(host);
       const { patch } = await handle(endpoints.byId(data._id));
       const { onRequest } = patch({ id: data._id });
 
-      await page.click('#load-board-btn');
-      await page.waitForSelector('#in-progress-section li button');
+      await page.click("#load-board-btn");
+      await page.waitForSelector("#in-progress-section li button");
       const [request] = await Promise.all([
         onRequest(),
-        page.click('#in-progress-section li button'),
+        page.click("#in-progress-section li button"),
       ]);
 
       const postData = JSON.parse(request.postData());
 
-      expect(postData.status).to.equal('Code Review');
+      expect(postData.status).to.equal("Code Review");
     });
 
-    it('Move Task (from Code Review to Done)', async () => {
+    it("Move Task (from Code Review to Done)", async () => {
       const data = mockData.list[2];
       await page.goto(host);
       const { patch } = await handle(endpoints.byId(data._id));
       const { onRequest } = patch({ id: data._id });
 
-      await page.click('#load-board-btn');
-      await page.waitForSelector('#code-review-section li button');
+      await page.click("#load-board-btn");
+      await page.waitForSelector("#code-review-section li button");
       const [request] = await Promise.all([
         onRequest(),
-        page.click('#code-review-section li button'),
+        page.click("#code-review-section li button"),
       ]);
 
       const postData = JSON.parse(request.postData());
 
-      expect(postData.status).to.equal('Done');
+      expect(postData.status).to.equal("Done");
     });
 
-    it('Close Task', async () => {
+    it("Close Task", async () => {
       const data = mockData.list[3];
       await page.goto(host);
       const { del } = await handle(endpoints.byId(data._id));
       const { onResponse, isHandled } = del({ id: data._id });
 
-      await page.click('#load-board-btn');
+      await page.click("#load-board-btn");
 
-      await page.waitForSelector('#done-section li button');
+      await page.waitForSelector("#done-section li button");
 
-      await Promise.all([
-        onResponse(),
-        page.click(
-          '#done-section li button'
-        ),
-      ]);
+      await Promise.all([onResponse(), page.click("#done-section li button")]);
 
       expect(isHandled()).to.be.true;
     });
@@ -209,7 +214,7 @@ async function setupContext(context) {
   await handleContext(context, endpoints.catalog, { get: mockData.list });
   await handleContext(context, endpoints.catalog, { post: mockData.list[0] });
 
-  await handleContext(context, endpoints.byId('1001'), {
+  await handleContext(context, endpoints.byId("1001"), {
     get: mockData.list[0],
   });
 
@@ -218,7 +223,7 @@ async function setupContext(context) {
     (url) => url.href.slice(0, host.length) != host,
     (route) => {
       if (DEBUG) {
-        console.log('Preventing external call to ' + route.request().url());
+        console.log("Preventing external call to " + route.request().url());
       }
       route.abort();
     }
@@ -236,20 +241,20 @@ function handleContext(context, match, handlers) {
 async function handleRaw(match, handlers) {
   const methodHandlers = {};
   const result = {
-    get: (returns, options) => request('GET', returns, options),
-    get2: (returns, options) => request('GET', returns, options),
-    post: (returns, options) => request('POST', returns, options),
-    put: (returns, options) => request('PUT', returns, options),
-    patch: (returns, options) => request('PATCH', returns, options),
-    del: (returns, options) => request('DELETE', returns, options),
-    delete: (returns, options) => request('DELETE', returns, options),
+    get: (returns, options) => request("GET", returns, options),
+    get2: (returns, options) => request("GET", returns, options),
+    post: (returns, options) => request("POST", returns, options),
+    put: (returns, options) => request("PUT", returns, options),
+    patch: (returns, options) => request("PATCH", returns, options),
+    del: (returns, options) => request("DELETE", returns, options),
+    delete: (returns, options) => request("DELETE", returns, options),
   };
 
   const context = this;
 
   await context.route(urlPredicate, (route, request) => {
     if (DEBUG) {
-      console.log('>>>', request.method(), request.url());
+      console.log(">>>", request.method(), request.url());
     }
 
     const handler = methodHandlers[request.method().toLowerCase()];
@@ -258,11 +263,12 @@ async function handleRaw(match, handlers) {
     } else {
       handler(route, request);
     }
-  });``
+  });
+  ``;
 
   if (handlers) {
     for (let method in handlers) {
-      if (typeof handlers[method] == 'function') {
+      if (typeof handlers[method] == "function") {
         handlers[method](result[method]);
       } else {
         result[method](handlers[method]);
@@ -306,10 +312,10 @@ function respond(data, options = {}) {
   );
 
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   };
   if (options.json) {
-    headers['Content-Type'] = 'application/json';
+    headers["Content-Type"] = "application/json";
     data = JSON.stringify(data);
   }
 
