d4075818a9e1a7c631da3a2d273a47bd9552bc5b
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-08-30 17:40:39 +0300

    Added Solution 01

---
+++ C# Advanced/C# Advanced/Exams/09. Retake Exam – August 2023/01. Monster Extermination/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _01._Monster_Extermination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> armors = new Queue<int>(Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Stack<int> strikes = new Stack<int>(Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+
+            int totalKilled = 0;
+            while (armors.Count > 0 && strikes.Count > 0)
+            {
+                int currArmor = armors.Dequeue();
+                int currStrike = strikes.Pop();
+
+                if (currStrike >= currArmor)
+                {
+                    totalKilled++;
+                    currStrike -= currArmor;
+
+                    if (currStrike > 0)
+                    {
+                        if (strikes.Count > 0)
+                        {
+                            currStrike += strikes.Pop();
+                        }
+                        strikes.Push(currStrike);
+                    }
+                }
+
+                else
+                {
+                    currArmor -= currStrike;
+                    armors.Enqueue(currArmor);
+                }
+            }
+
+            if (armors.Count == 0)
+                Console.WriteLine("All monsters have been killed!");
+
+            if(strikes.Count == 0)
+                Console.WriteLine("The soldier has been defeated.");
+
+            Console.WriteLine($"Total monsters killed: {totalKilled}");
+        }
+    }
+}
