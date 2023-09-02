9d212b76942648a19acb59961dafc9311f735497
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-09-02 16:11:14 +0300

    Added Solution 03

---
+++ C# Advanced/C# Advanced/Exams/09. Retake Exam – August 2023/03. Vending System/StartUp.cs
@@ -0,0 +1,65 @@
+﻿ using System;
+
+namespace VendingSystem
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //Initialize the repository (VendingMachine)
+            VendingMachine vendingMachine = new VendingMachine(6);
+
+            //Initialize entity (Drink)
+            Drink tea = new Drink("Tea", 1.5m, 300);
+            Drink coffee = new Drink("Coffee", 2.0m, 120);
+            Drink hotChocolate = new Drink("Hot Chocolate", 2.5m, 200);
+            Drink latte = new Drink("Latte", 3.5m, 220);
+            Drink cappuccino = new Drink("Cappuccino", 2.8m, 180);
+            Drink mocha = new Drink("Mocha", 2.1m, 150);
+            Drink herbalTea = new Drink("Herbal Tea", 1.75m, 300);
+
+            //Get Count
+            Console.WriteLine(vendingMachine.GetCount);
+            //0
+
+            //Add Drinks
+            vendingMachine.AddDrink(tea);
+            vendingMachine.AddDrink(coffee);
+            vendingMachine.AddDrink(hotChocolate);
+            vendingMachine.AddDrink(latte);
+            vendingMachine.AddDrink(cappuccino);
+            vendingMachine.AddDrink(mocha);
+
+            //Try to add drinks when the capacity is full
+            vendingMachine.AddDrink(herbalTea);
+
+            //Get Count
+            Console.WriteLine(vendingMachine.GetCount);
+            //6
+
+            //Remove Drink
+            Console.WriteLine(vendingMachine.RemoveDrink("Herbal Tea"));//False
+            Console.WriteLine(vendingMachine.RemoveDrink("Tea"));//True
+
+            //Get Longest Drink
+            Console.WriteLine(vendingMachine.GetLongest());
+            //Name: Latte, Price: $3.5, Volume: 220 ml
+
+            //Get Cheapest Drink
+            Console.WriteLine(vendingMachine.GetCheapest());
+            //Name: Coffee, Price: $2.0, Volume: 120 ml
+
+            //Buy a specific Drink
+            Console.WriteLine(vendingMachine.BuyDrink("Cappuccino"));
+            //Name: Cappuccino, Price: $2.8, Volume: 180 ml
+
+            //Drinks Report
+            Console.WriteLine(vendingMachine.Report());
+            //Drinks available:
+            //Name: Coffee, Price: $2.0, Volume: 120 ml
+            //Name: Hot Chocolate, Price: $2.5, Volume: 200 ml
+            //Name: Latte, Price: $3.5, Volume: 220 ml
+            //Name: Cappuccino, Price: $2.8, Volume: 180 ml
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced/Exams/09. Retake Exam – August 2023/03. Vending System/VendingMachine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+        public VendingMachine(int buttonCapacity)
+        {
+            ButtonCapacity = buttonCapacity;
+            Drinks = new List<Drink>();
+        }
+
+        public int ButtonCapacity { get; set; }
+
+        public List<Drink> Drinks { get; set; }
+
+        public int GetCount => Drinks.Count;
+
+        public void AddDrink(Drink drink)
+        {
+            if (GetCount < ButtonCapacity)
+            {
+                Drinks.Add(drink);
+            }
+        }
+
+        public bool RemoveDrink(string name)
+        {
+            Drink drink = Drinks.FirstOrDefault(d => d.Name == name);
+            return Drinks.Remove(drink);
+        }
+
+        public Drink GetLongest() 
+            => Drinks.OrderByDescending(d => d.Volume).FirstOrDefault();
+
+        public Drink GetCheapest() 
+            => Drinks.OrderBy(d => d.Price).FirstOrDefault();
+
+        public string BuyDrink(string name)
+            => Drinks.Find(d => d.Name == name).ToString();
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Drinks available:");
+
+            foreach (var drink in Drinks)
+                sb.AppendLine(drink.ToString());
+
+            return sb.ToString();
+        }
+    }
+}
