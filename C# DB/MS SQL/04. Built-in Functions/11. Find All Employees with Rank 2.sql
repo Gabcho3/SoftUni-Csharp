b51e02bf76703314ad84685dfd2b7fb25d1650a1
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-09-12 19:44:16 +0300

    Create 11. Find All Employees with Rank 2.sql

---
+++ C# DB/MS SQL/04. Built-in Functions/11. Find All Employees with Rank 2.sql
@@ -0,0 +1,13 @@
+SELECT * FROM
+(
+	SELECT EmployeeID, FirstName, LastName, Salary, 
+	DENSE_RANK() OVER 
+	(PARTITION BY Salary ORDER BY EmployeeID) 
+	AS [Rank]
+	FROM Employees
+) AS Ranked
+WHERE 
+	Salary BETWEEN 10000 AND 50000
+	AND [Rank] = 2
+ORDER BY Salary DESC;
+
