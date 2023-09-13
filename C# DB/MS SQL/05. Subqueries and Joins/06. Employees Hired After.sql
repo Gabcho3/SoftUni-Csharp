d672c06271f8a986aaf686e7a24701544db28f24
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-09-13 20:20:56 +0300

    Basic Joins

---
+++ C# DB/MS SQL/05. Subqueries and Joins/06. Employees Hired After.sql
@@ -0,0 +1,11 @@
+SELECT
+	e.FirstName,
+	e.LastName,
+	e.HireDate,
+	d.[Name] AS DeptName
+FROM Employees AS e
+	JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE 
+	YEAR(e.HireDate) > 1999
+	AND d.[Name] IN ('Sales', 'Finance')
+ORDER BY e.HireDate;
