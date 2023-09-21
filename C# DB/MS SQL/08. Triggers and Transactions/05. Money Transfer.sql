e299e76fbb97bffabbb624b047e3bdb1a60236fe
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-09-21 11:18:11 +0300

    Added files

--- C# DB/MS SQL/08. Triggers and Transactions/05. Money Transfer.sql
+++ C# DB/MS SQL/08. Triggers and Transactions/05. Money Transfer.sql
@@ -6,8 +6,8 @@ AS
 BEGIN TRANSACTION
 	IF(@Amount < 0) THROW 50001, 'Invalid Amount', 1
 	BEGIN TRY
-		EXEC usp_DepositMoney @ReceiverId @Amount;
-		EXEC usp_WithdrawMoney @SenderId @Amount;
+		EXEC usp_DepositMoney @ReceiverId, @Amount;
+		EXEC usp_WithdrawMoney @SenderId, @Amount;
 	END TRY
 	BEGIN CATCH
 		ROLLBACK;
