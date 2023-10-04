c0f0e95a21149c6b0a6821496a91b4621c575c34
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-05 01:05:05 +0300

    Added Solution 01

---
+++ C# DB/Entity Framework Core/01. ADO.NET/01. Initial Setup/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._Initial_Setup
+{
+    internal static class Config
+    {
+        public const string SqlConnection = @"Server=.;Database=MinionsDB;Integrated Security=True;Trust Server Certificate=True";
+
+        public const string GetVillainsNamesAndTheirMinionsQuery = @"
+            SELECT
+	            v.[Name],
+	            COUNT(mv.MinionId) AS MinionsCount
+            FROM Villains AS v
+	            JOIN MinionsVillains as mv ON v.Id = mv.VillainId
+            GROUP BY v.[Name]
+            HAVING COUNT(mv.MinionId) > 3
+            ORDER BY MinionsCount DESC;";
+    }
+}
---
+++ C# DB/Entity Framework Core/01. ADO.NET/01. Initial Setup/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using _01._Initial_Setup;
+using Microsoft.Data.SqlClient;
+
+await using SqlConnection sqlConnection = 
+    new SqlConnection(Config.SqlConnection);
+
+await sqlConnection.OpenAsync();
+SqlCommand sqlCommand = new SqlCommand(Config.GetVillainsNamesAndTheirMinionsQuery, sqlConnection);
+
+SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+StringBuilder sb = new StringBuilder();
+while (reader.Read())
+{
+    string villainName = (string)reader["Name"];
+    int minionsCount = (int)reader["MinionsCount"];
+    sb.Append(villainName + " - " + minionsCount);
+}
+
+Console.WriteLine(sb.ToString().TrimEnd());
