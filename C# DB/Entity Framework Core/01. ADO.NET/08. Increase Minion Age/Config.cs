64111c5be7fdd5123bda9ad1d6782534f7c24406
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-07 23:30:36 +0300

    Added Solution 08

---
+++ C# DB/Entity Framework Core/01. ADO.NET/08. Increase Minion Age/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace _07._Increase_Minion_Age
+{
+    internal static class Config
+    {
+        public const string SqlConnection = @"Server=.;Database=MinionsDB;Integrated Security=True;Trust Server Certificate=True;MultipleActiveResultSets=True";
+    }
+}
---
+++ C# DB/Entity Framework Core/01. ADO.NET/08. Increase Minion Age/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using _07._Increase_Minion_Age;
+using Microsoft.Data.SqlClient;
+
+int[] ids = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+await using SqlConnection sqlConnection = new SqlConnection(Config.SqlConnection);
+sqlConnection.Open();
+
+IncreaseMinionsAgeByOne(sqlConnection, ids);
+PrintAllMinions(sqlConnection);
+static void IncreaseMinionsAgeByOne(SqlConnection sqlConnection, int[] ids)
+{
+    SqlCommand increaseAgeByOne = new SqlCommand(@"
+    UPDATE Minions
+        SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+    WHERE Id = @Id", sqlConnection);
+
+    foreach (int id in ids)
+    {
+        increaseAgeByOne.Parameters.AddWithValue("@Id", id);
+        increaseAgeByOne.ExecuteNonQuery();
+        increaseAgeByOne.Parameters.Clear();
+    }
+}
+
+static void PrintAllMinions(SqlConnection sqlConnection)
+{
+    SqlCommand getAllMinions = new SqlCommand(@"SELECT Name, Age FROM Minions", sqlConnection);
+    SqlDataReader minionsReader = getAllMinions.ExecuteReader();
+
+    while (minionsReader.Read())
+    {
+        string name = (string)minionsReader["Name"];
+        int age = (int)minionsReader["Age"];
+
+        Console.WriteLine(name + ' ' + age);
+    }
+}
