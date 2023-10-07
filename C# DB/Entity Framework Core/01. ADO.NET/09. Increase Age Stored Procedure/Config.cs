e987ad469d6ee192a9c1a2e6202e7179fca2a82e
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-07 23:50:24 +0300

    Added Solution 09

---
+++ C# DB/Entity Framework Core/01. ADO.NET/09. Increase Age Stored Procedure/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace _09._Increase_Age_Stored_Procedure
+{
+    internal static class Config
+    {
+        public const string SqlConnection = @"Server=.;Database=MinionsDB;Integrated Security=True;Trust Server Certificate=True;MultipleActiveResultSets=True";
+    }
+}
---
+++ C# DB/Entity Framework Core/01. ADO.NET/09. Increase Age Stored Procedure/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using _09._Increase_Age_Stored_Procedure;
+using Microsoft.Data.SqlClient;
+
+int minionId = int.Parse(Console.ReadLine());
+
+await using SqlConnection sqlConnection = new SqlConnection(Config.SqlConnection);
+sqlConnection.Open();
+
+SqlCommand increaseAgeByOneByProc = new SqlCommand(@"EXEC usp_GetOlder @Id", sqlConnection);
+increaseAgeByOneByProc.Parameters.AddWithValue("@Id", minionId);
+increaseAgeByOneByProc.ExecuteNonQuery();
+
+SqlCommand selectMinion = new SqlCommand(@"SELECT Name, Age FROM Minions WHERE Id = @Id", sqlConnection);
+selectMinion.Parameters.AddWithValue("@Id", minionId);
+SqlDataReader minionReader = selectMinion.ExecuteReader();
+
+while (minionReader.Read())
+{
+    string name = minionReader["Name"].ToString();
+    int age = (int)minionReader["Age"];
+    Console.WriteLine($"{name} - {age} years old");
+}
