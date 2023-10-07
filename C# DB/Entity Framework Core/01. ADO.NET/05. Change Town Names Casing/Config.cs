c879c40c7eaee6935a39f02a087ecc41ef028d43
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-07 19:46:02 +0300

    Added Solution 05

---
+++ C# DB/Entity Framework Core/01. ADO.NET/05. Change Town Names Casing/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace _05._Change_Town_Names_Casing
+{
+    internal static class Config
+    {
+        public const string SqlConnection = @"Server=.;Database=MinionsDB;Integrated Security=True;Trust Server Certificate=True;MultipleActiveResultSets=True";
+    }
+}
---
+++ C# DB/Entity Framework Core/01. ADO.NET/05. Change Town Names Casing/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using _05._Change_Town_Names_Casing;
+using Microsoft.Data.SqlClient;
+
+string countryName = Console.ReadLine();
+
+await using SqlConnection sqlConnection = new SqlConnection(Config.SqlConnection);
+sqlConnection.Open();
+
+int affectedRows = await ChangeTownNames(countryName, sqlConnection);
+
+if (affectedRows > 1)
+{
+    Console.WriteLine($"{affectedRows} town names were affected.");
+}
+else
+{
+    Console.WriteLine("No town names were affected.");
+    return;
+}
+
+List<string> towns = await GetTownsNames(countryName, sqlConnection);
+Console.WriteLine($"[{string.Join(", ", towns)}]");
+
+static async Task<int> ChangeTownNames(string countryName, SqlConnection sqlConnection)
+{
+    SqlCommand changeTownsNames = new SqlCommand(
+        @"UPDATE Towns 
+        SET Name = UPPER(Name) 
+    WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)", sqlConnection);
+
+    changeTownsNames.Parameters.AddWithValue("@countryName", countryName);
+    return await changeTownsNames.ExecuteNonQueryAsync();
+}
+
+static async Task<List<string>> GetTownsNames(string countryName, SqlConnection sqlConnection)
+{
+    List<string> townsList = new List<string>();
+
+    SqlCommand selectTowns = new SqlCommand(
+        @"SELECT t.Name 
+    FROM Towns as t
+        JOIN Countries AS c ON c.Id = t.CountryCode
+    WHERE c.Name = @countryName", sqlConnection);
+
+    selectTowns.Parameters.AddWithValue("@countryName", countryName);
+
+    SqlDataReader townsReader = await selectTowns.ExecuteReaderAsync();
+
+    while(townsReader.Read())
+    {
+        townsList.Add(townsReader["Name"].ToString());
+    }
+
+    return townsList;
+}
+
