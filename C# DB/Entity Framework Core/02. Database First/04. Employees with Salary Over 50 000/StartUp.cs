a3898e5cf19151fe4543e0ff07f802d96116a622
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-11 00:32:00 +0300

    Added Solution 04

---
+++ C# DB/Entity Framework Core/02. Database First/04. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            string output = GetEmployeesWithSalaryOver50000(context);
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var richEmployees = context.Employees
+                .AsNoTracking()
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    Salary = e.Salary
+                })
+                .OrderBy(e => e.FirstName);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var e in richEmployees)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
