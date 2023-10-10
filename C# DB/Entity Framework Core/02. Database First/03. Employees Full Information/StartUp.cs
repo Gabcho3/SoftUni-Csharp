60267763f50d64f51a769e491549094a744d581f
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-11 00:19:55 +0300

    Added Solution 03

---
+++ C# DB/Entity Framework Core/02. Database First/03. Employees Full Information/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            string output = await GetEmployeesFullInformation(context);
+            Console.WriteLine(output);
+        }
+
+        public static async Task<string> GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = await context.Employees
+                .AsNoTracking()
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    MiddleName = e.MiddleName,
+                    JobTitle = e.JobTitle,
+                    Salary = e.Salary
+                })
+                .ToListAsync();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
