5fd19f7b7a38dab03a73b477ad6b0cf6b7e18cfa
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-10 23:57:38 +0300

    Added Solution 02

---
+++ C# DB/Entity Framework Core/02. Database First/02. Entity Framework Introduction/StratUp.cs
@@ -0,0 +1,12 @@
+﻿﻿using System;
+
+namespace SoftUni
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
