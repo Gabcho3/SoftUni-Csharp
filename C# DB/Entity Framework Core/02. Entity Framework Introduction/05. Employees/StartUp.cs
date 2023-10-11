ee36feb1973e98d876ba625e7fd3761ddf4af128
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-11 15:56:55 +0300

    Added Solution 05

---
+++ C# DB/Entity Framework Core/02. Entity Framework Introduction/05. Employees/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            string output = GetEmployeesFromResearchAndDevelopment(context);
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employeesInDepartment = context.Employees
+                .AsNoTracking()
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    DepartmentName = e.Department.Name,
+                    Salary = e.Salary,
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var e in employeesInDepartment)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
