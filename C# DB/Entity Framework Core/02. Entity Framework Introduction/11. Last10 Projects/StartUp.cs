6a6084947fa5d3f5a8f2bf3ab6a6eba98bbe38ff
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-13 00:33:55 +0300

    Added Solution 11

---
+++ C# DB/Entity Framework Core/02. Entity Framework Introduction/11. Last10 Projects/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            string output = GetLatestProjects(context);
+;            Console.WriteLine(output);
+        }
+
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            var projects = context.Projects
+                .AsNoTracking()
+                .OrderByDescending(p => p.StartDate)
+                .Take(10)
+                .OrderBy(p => p.Name)
+                .Select(p => new
+                {
+                    Name = p.Name!,
+                    Description = p.Description!,
+                    StartDate = p.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)
+                })
+                
+                .ToList();
+
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var p in projects)
+            {
+                sb.AppendLine(p.Name);
+                sb.AppendLine(p.Description);
+                sb.AppendLine(p.StartDate);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
