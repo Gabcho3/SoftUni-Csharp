a60beb94d02264285f7d131ee5a584f68f4e2eb9
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-13 00:13:47 +0300

    Added Solution 10

---
+++ C# DB/Entity Framework Core/02. Entity Framework Introduction/10. Departments/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            string output = GetDepartmentsWithMoreThan5Employees(context);
+;            Console.WriteLine(output);
+        }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            var departments = context.Departments
+                .AsNoTracking()
+                .Where(d => d.Employees.Count > 5)
+                .OrderBy(d => d.Employees.Count)
+                .ThenBy(d => d.Name)
+                .Select(d => new
+                {
+                    DepartmentName = d.Name,
+                    ManagerName = d.Manager.FirstName + " " + d.Manager.LastName,
+                    EmployeeInfo = d.Employees.Select(e => new
+                    {
+                        FirstName = e.FirstName,
+                        LastName = e.LastName,
+                        JobTitle = e.JobTitle
+                    })
+                    .OrderBy(e => e.FirstName)
+                    .ThenBy(e => e.LastName)
+                    .ToList()
+
+                })
+                .ToList();
+
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var d in departments)
+            {
+                sb.AppendLine($"{d.DepartmentName} - {d.ManagerName}");
+                foreach (var e in d.EmployeeInfo)
+                {
+                    sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
