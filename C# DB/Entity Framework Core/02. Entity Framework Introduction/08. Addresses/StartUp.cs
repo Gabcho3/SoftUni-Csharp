6a31ee0eb2c898a226f05e9076a26e42c762964d
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-12 23:49:32 +0300

    Added Solution 08

---
+++ C# DB/Entity Framework Core/02. Entity Framework Introduction/08. Addresses/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            string output = GetAddressesByTown(context);
+;           Console.WriteLine(output);
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+           var addresses = context.Addresses
+               .AsNoTracking()
+               .OrderByDescending(a => a.Employees.Count)
+               .ThenBy(a => a.Town!.Name)
+               .ThenBy(a => a.AddressText)
+               .Select(a => new
+               {
+                   AddressText = a.AddressText!,
+                   TownName = a.Town!.Name,
+                   EmployeesCount = a.Employees.Count
+               })
+               .Take(10)
+               .ToList();
+
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var a in addresses)
+            {
+                sb.AppendLine($"{a.AddressText}, {a.TownName} - {a.EmployeesCount} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
