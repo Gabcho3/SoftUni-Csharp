f08019a877f766523fe5fc50b85b81d5e930524e
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-13 00:01:51 +0300

    Added Solution 09

---
+++ C# DB/Entity Framework Core/02. Entity Framework Introduction/09. Employee 147/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            string output = GetEmployee147(context);
+;            Console.WriteLine(output);
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .AsNoTracking()
+                .Where(e => e.EmployeeId == 147)
+                .Select(e => new
+                {
+                    FirstName = e.FirstName!,
+                    LastName = e.LastName!,
+                    JobTitle = e.JobTitle!,
+                    ProjectsNames = e.Projects
+                        .OrderBy(p => p.Name)
+                        .Select(p => p.Name)
+                });
+
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                foreach (var projectName in e.ProjectsNames)
+                {
+                    sb.AppendLine(projectName);
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
