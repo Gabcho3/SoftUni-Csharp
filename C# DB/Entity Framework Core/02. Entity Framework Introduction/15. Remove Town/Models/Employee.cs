4c9572c6bc4e362c0c637f4310215af525f1ef09
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-13 23:38:49 +0300

    Added Solution 15

---
+++ C# DB/Entity Framework Core/02. Entity Framework Introduction/15. Remove Town/Models/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoftUni.Models
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            Departments = new HashSet<Department>();
+            InverseManager = new HashSet<Employee>();
+            EmployeesProjects = new HashSet<EmployeeProject>();
+        }
+
+        [Key]
+        [Column("EmployeeID")]
+        public int EmployeeId { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string FirstName { get; set; } = null!;
+        [StringLength(50)]
+        [Unicode(false)]
+        public string LastName { get; set; } = null!;
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? MiddleName { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string JobTitle { get; set; } = null!;
+        [Column("DepartmentID")]
+        public int DepartmentId { get; set; }
+        [Column("ManagerID")]
+        public int? ManagerId { get; set; }
+        [Column(TypeName = "smalldatetime")]
+        public DateTime HireDate { get; set; }
+        [Column(TypeName = "decimal(15, 4)")]
+        public decimal Salary { get; set; }
+        [Column("AddressID")]
+        public int? AddressId { get; set; }
+
+        [ForeignKey(nameof(AddressId))]
+        [InverseProperty("Employees")]
+        public virtual Address? Address { get; set; }
+        [ForeignKey(nameof(DepartmentId))]
+        [InverseProperty("Employees")]
+        public virtual Department Department { get; set; } = null!;
+        [ForeignKey(nameof(ManagerId))]
+        [InverseProperty(nameof(InverseManager))]
+        public virtual Employee? Manager { get; set; }
+        [InverseProperty("Manager")]
+        public virtual ICollection<Department> Departments { get; set; }
+        [InverseProperty(nameof(Manager))]
+        public virtual ICollection<Employee> InverseManager { get; set; }
+        public virtual ICollection<EmployeeProject> EmployeesProjects { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/02. Entity Framework Introduction/15. Remove Town/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            string output = RemoveTown(context);
+;           Console.WriteLine(output);
+        }
+
+        public static string RemoveTown(SoftUniContext context)
+        {
+            string townName = "Seattle";
+
+            var employeesWhoLivesInTown = context.Employees
+                .Where(e => e.Address!.Town!.Name == townName)
+                .ToList();
+
+            foreach (var e in employeesWhoLivesInTown)
+            {
+                e.AddressId = null;
+            }
+            context.SaveChanges();
+
+            var addressesWithTown = context.Addresses
+                .Where(a => a.Town!.Name == townName)
+                .ToList();
+            context.Addresses.RemoveRange(addressesWithTown);
+
+            Town townToDelete = context.Towns.FirstOrDefault(t => t.Name == townName)!;
+            context.Towns.Remove(townToDelete);
+            context.SaveChanges();
+
+            return $"{addressesWithTown.Count} addresses in Seattle were deleted";
+        }
+    }
+}
