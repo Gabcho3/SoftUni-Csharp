bc530e3537360b368c5deb795c0d361dc302be2c
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-20 23:29:05 +0300

    Added Solution 01

---
+++ C# DB/Entity Framework Core/03. Entity Relations/01. Student System/Data/Models/Resource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+
+        [Unicode][MaxLength(50)] public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/03. Entity Relations/01. Student System/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data;
+
+namespace P01_StudentSystem
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
