47c7f863c6f253f183f194af6fe09485b75f77eb
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-23 22:27:45 +0300

    Created MusicHub Database

---
+++ C# DB/Entity Framework Core/04. LINQ/01. MusicHub Database/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace MusicHub
+{
+    using System;
+
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            MusicHubDbContext context =
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            //context.Database.EnsureDeleted();
+            // context.Database.EnsureCreated();
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
