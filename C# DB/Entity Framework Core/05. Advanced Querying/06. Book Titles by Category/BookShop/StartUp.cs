52e0ceb088f1e3ae2f02cc9a24328ab99bd41307
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-27 22:36:05 +0300

    Added Solution 06

---
+++ C# DB/Entity Framework Core/05. Advanced Querying/06. Book Titles by Category/BookShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            string result = GetBooksByCategory(db, input);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] categories = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(c => c.ToLower())
+                .ToArray();
+
+            var booksByCategory = context.BooksCategories
+                .AsNoTracking()
+                .Where(bc => categories.Contains(bc.Category.Name.ToLower()))
+                .Select(bc => bc.Book.Title)
+                .OrderBy(t => t);
+
+            return string.Join(Environment.NewLine, booksByCategory);
+        }
+    }
+}
+
+
