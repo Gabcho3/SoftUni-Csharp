a113cf541c3edca9a5260b31a08cc0dda6550b5d
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-27 22:00:10 +0300

    Added Solution 03

---
+++ C# DB/Entity Framework Core/05. Advanced Querying/03. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string result = GetGoldenBooks(db);
+            Console.WriteLine(result);
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var enumValue = Enum.Parse<EditionType>("Gold");
+
+            var goldenBooks = context.Books
+                .AsNoTracking()
+                .Where(b => b.EditionType == enumValue && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title);
+
+            return string.Join(Environment.NewLine, goldenBooks);
+        }
+    }
+}
+
+
