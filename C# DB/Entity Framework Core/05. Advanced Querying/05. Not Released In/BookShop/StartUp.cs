35ef099b2e1aebfcaea5fdbb420356f4a974c227
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-27 22:35:52 +0300

    Added Solution 05

---
+++ C# DB/Entity Framework Core/05. Advanced Querying/05. Not Released In/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            int year = int.Parse(Console.ReadLine());
+            string result = GetBooksNotReleasedIn(db, year);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var booksNotReleasedIn = context.Books
+                .Where(b => b.ReleaseDate.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title);
+
+            return string.Join(Environment.NewLine, booksNotReleasedIn);
+        }
+    }
+}
+
+
