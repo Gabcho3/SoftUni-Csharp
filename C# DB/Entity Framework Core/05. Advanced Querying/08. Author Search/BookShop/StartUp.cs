bde03a32e4d5e024d99db7a44ccd06188da8ce5a
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-28 16:30:07 +0300

    Added Solution 08

---
+++ C# DB/Entity Framework Core/05. Advanced Querying/08. Author Search/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine()!;
+            string result = GetAuthorNamesEndingIn(db, input);
+            Console.WriteLine(result);
+        }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string endsIn)
+        {
+            var authorsFullNamesEndIn = context.Authors
+                .Where(a => a.FirstName.EndsWith(endsIn))
+                .Select(a => a.FirstName + ' ' + a.LastName)
+                .OrderBy(n => n);
+
+            return string.Join(Environment.NewLine, authorsFullNamesEndIn);
+        }
+    }
+}
+
+
