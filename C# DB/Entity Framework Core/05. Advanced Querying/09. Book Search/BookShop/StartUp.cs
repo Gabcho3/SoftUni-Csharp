1899c7ca4e930f0c678c3ea89317138e599b7801
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-28 16:39:30 +0300

    Added Solution 09

---
+++ C# DB/Entity Framework Core/05. Advanced Querying/09. Book Search/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine()!;
+            string result = GetBookTitlesContaining(db, input);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            var booksContaining = context.Books
+                .Where(b => b.Title.ToLower().Contains(input.ToLower()))
+                .Select(b => b.Title)
+                .OrderBy(t => t);
+
+            return string.Join(Environment.NewLine, booksContaining);
+        }
+    }
+}
+
+
