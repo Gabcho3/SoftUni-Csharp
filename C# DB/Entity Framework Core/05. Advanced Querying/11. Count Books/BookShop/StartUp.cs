c35220a1953b5f46b44ccff5a9252f22dc8cfd37
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-28 16:51:58 +0300

    Added Solution 11

---
+++ C# DB/Entity Framework Core/05. Advanced Querying/11. Count Books/BookShop/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            int input = int.Parse(Console.ReadLine());
+            int result = CountBooks(db, input);
+            Console.WriteLine(result);
+        }
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            int booksWithLongerTitle = context.Books
+                .Count(b => b.Title.Length > lengthCheck);
+
+            return booksWithLongerTitle;
+        }
+    }
+}
+
+
