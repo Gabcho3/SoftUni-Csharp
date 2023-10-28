9fc4576e5c07e0e527e6d4ad2cefac6a133cc9f8
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-28 16:59:14 +0300

    Added Solution 12

---
+++ C# DB/Entity Framework Core/05. Advanced Querying/12. Total Book Copies/BookShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.ValueGeneration;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string result = CountCopiesByAuthor(db);
+            Console.WriteLine(result);
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            var totalCopiesBuAuthor = context.Authors
+                .Select(a => new
+                {
+                    AuthorFullName = a.FirstName + ' ' + a.LastName,
+                    TotalBookCopies = a.Books.Sum(b => b.Copies)
+                })
+                .OrderByDescending(a => a.TotalBookCopies);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var author in totalCopiesBuAuthor)
+            {
+                sb.AppendLine($"{author.AuthorFullName} - {author.TotalBookCopies}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
