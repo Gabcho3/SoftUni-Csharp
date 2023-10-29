b1122f9d5c4c3a9745b7faa00aecf3682e0c03c9
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-29 13:35:16 +0200

    Added Solution 15

---
+++ C# DB/Entity Framework Core/05. Advanced Querying/15. Increase Prices/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.ValueGeneration;
+    using System.Diagnostics;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            IncreasePrices(db);
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var booksBeforeYear = context.Books
+                .Where(b => b.ReleaseDate!.Value.Year < 2010);
+
+            foreach (var book in booksBeforeYear)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
+
+
