bbf010c60656aaf91ebb45d413ef7f3cd212c30c
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-10-29 13:43:41 +0200

    Added Solution 16

---
+++ C# DB/Entity Framework Core/05. Advanced Querying/16. Remove Books/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.ValueGeneration;
+    using System.Diagnostics;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            int deletedBooks = RemoveBooks(db);
+            Console.WriteLine(deletedBooks);
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var booksToDelete = context.Books
+                .Where(b => b.Copies < 4200)
+                .ToArray();
+
+            context.RemoveRange(booksToDelete);
+            context.SaveChanges();
+
+            return booksToDelete.Count();
+        }
+    }
+}
+
+
