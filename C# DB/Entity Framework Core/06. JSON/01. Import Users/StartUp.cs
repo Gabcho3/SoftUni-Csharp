f1693b4dc8a3dd27dac0ddf81242fb0b1e3aa8c0
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-02 22:40:47 +0200

    Added Solution 01

---
+++ C# DB/Entity Framework Core/06. JSON/01. Import Users/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText(@"../../../Datasets/users.json");
+            string result;
+            result = ImportUsers(context, inputJson);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            List<User> usersToImport = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            context.Users.AddRange(usersToImport);
+            context.SaveChanges();
+
+            return $"Successfully imported {usersToImport.Count}";
+        }
+    }
+}
