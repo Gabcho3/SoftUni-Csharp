890a10562fc9173e8915eadcbe55b78ded2fce81
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-04 23:14:55 +0200

    Added Solution 10

---
+++ C# DB/Entity Framework Core/06. JSON/10. Import Parts/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var suppliersInputJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
+            ImportSuppliers(context, suppliersInputJson);
+
+            var partsInputJson = File.ReadAllText(@"../../../Datasets/parts.json");
+            Console.WriteLine(ImportParts(context, partsInputJson));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            List<Supplier> suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson)!;
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var allSuppliersIds = context.Suppliers.Select(s => s.Id);
+
+            List<Part> parts = JsonConvert.DeserializeObject<List<Part>>(inputJson)!
+                .Where(p => allSuppliersIds.Contains(p.SupplierId))
+                .ToList();
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count}.";
+        }
+    }
+}
