26341bdee93f5443817d0dfa06401e911f92420d
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-04 22:59:12 +0200

    Added Solution 09

---
+++ C# DB/Entity Framework Core/06. JSON/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var suppliersInputJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
+            Console.WriteLine(ImportSuppliers(context, suppliersInputJson));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+    }
+}
