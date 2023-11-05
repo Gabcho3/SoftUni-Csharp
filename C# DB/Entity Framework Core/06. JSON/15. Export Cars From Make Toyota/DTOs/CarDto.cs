ef6682fe8e3d0e2b6c61b25fffd71fc2af362967
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-05 14:56:23 +0200

    Added Solution 15

---
+++ C# DB/Entity Framework Core/06. JSON/15. Export Cars From Make Toyota/DTOs/CarDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._Import_Cars
+{
+    public class CarDto
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int TraveledDistance { get; set; }
+
+        public List<int> PartsId { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/06. JSON/15. Export Cars From Make Toyota/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace CarDealer.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; } = null!;
+
+        public string Model { get; set; } = null!;
+
+        public long TraveledDistance { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Sale> Sales { get; set; } = new List<Sale>();
+
+        [JsonIgnore]
+        public ICollection<PartCar> PartsCars { get; set; } = new List<PartCar>();
+    }
+}
