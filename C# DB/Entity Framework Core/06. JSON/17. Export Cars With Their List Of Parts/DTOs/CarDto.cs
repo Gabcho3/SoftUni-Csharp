6b5beb4a6f253cc1de2907a5fd5526398cd77071
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-07 19:44:05 +0200

    Added Solution 17

---
+++ C# DB/Entity Framework Core/06. JSON/17. Export Cars With Their List Of Parts/DTOs/CarDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace _11._Import_Cars
+{
+    public class CarDto
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public long TraveledDistance { get; set; }
+
+        [JsonIgnore]
+        public List<int> PartsId { get; set; }
+    }
+}
