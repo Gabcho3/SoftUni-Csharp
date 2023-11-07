4b1032b88d4eba1d421babc9d2a237fbe170229e
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-07 20:56:23 +0200

    Added Solution 19

---
+++ C# DB/Entity Framework Core/06. JSON/19. Export Sales With Applied Discount/DTOs/SalesWithDiscount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace _19._Export_Sales_With_Applied_Discount.DTOs
+{
+    public class SalesWithDiscount
+    {
+        private decimal discount;
+        private decimal price;
+        private decimal priceWithDiscount;
+
+        public SalesWithDiscount(Car car, string customerName, decimal discount)
+        {
+            this.Car = car;
+            this.CustomerName = customerName;
+
+            this.discount = discount;
+            this.price = car.PartsCars.Sum(pc => pc.Part.Price);
+            this.priceWithDiscount = this.price * (1 - this.discount / 100);
+        }
+
+        [JsonProperty("car")]
+        public Car Car { get; set; }
+
+        [JsonProperty("customerName")]
+        public string CustomerName { get; set; }
+
+        [JsonProperty("discount")] 
+        public string Discount => $"{this.discount:f2}";
+
+        [JsonProperty("price")] 
+        public string Price => $"{this.price:f2}";
+
+        [JsonProperty("priceWithDiscount")] public string PriceWithDiscount => $"{this.priceWithDiscount:f2}";
+
+    }
+}
