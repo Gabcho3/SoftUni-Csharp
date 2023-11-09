8b5877ce1f296d332f3767a378b1dbcdcf2e24de
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-09 23:27:46 +0200

    Added Solution 01

---
+++ C# DB/Entity Framework Core/07. XML/01. Import Users/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml.Linq;
+using System.Xml.Serialization;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var inputUsersXml = @"../../../Datasets/users.xml";
+            Console.WriteLine(ImportUsers(context, inputUsersXml));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            XDocument xDocument = XDocument.Load(inputXml);
+
+            var usersElements = xDocument.Root!.Elements("User");
+
+            var usersToImport = new List<User>();
+            foreach (var userElement in usersElements)
+            {
+                var userDto = new User()
+                {
+                    FirstName = userElement.Element("firstName")!.Value,
+                    LastName = userElement.Element("lastName")!.Value,
+                    Age = int.Parse(userElement.Element("age")!.Value)
+                };
+                usersToImport.Add(userDto);
+            }
+
+            context.Users.AddRange(usersToImport);
+            context.SaveChanges();
+
+            return $"Successfully imported {usersToImport.Count}";
+        }
+    }
+}
