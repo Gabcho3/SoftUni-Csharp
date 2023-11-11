ff28debfde3e66283c3302a4372619e022507025
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-11 21:52:04 +0200

    Added Solution 07

---
+++ C# DB/Entity Framework Core/07. XML/07. Export Categories By Products Count/DTOs/ProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _06._Export_Sold_Products.DTOs
+{
+    [XmlType("Product")]
+    public class ProductDTO
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/07. XML/07. Export Categories By Products Count/ProductShopProfile.cs
@@ -0,0 +1,36 @@
+﻿using _02._Import_Products.DTOs.Import;
+using _03._Import_Categories.DTOs.Import;
+using _04._Import_Categories_and_Products.DTOs.Import;
+using _05._Export_Products_In_Range.DTOs.Export;
+using _06._Export_Sold_Products.DTOs;
+using _06._Export_Sold_Products.DTOs.Export;
+using AutoMapper;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            CreateMap<ImportUserDTO, User>();
+
+            //Product
+            CreateMap<ImportProductDTO, Product>();
+            CreateMap<Product, ExportProductDTO>()
+                .ForMember(dest => dest.BuyerFullName,
+                    opt =>
+                        opt.MapFrom(src => src.Buyer.FirstName + ' ' + src.Buyer.LastName));
+            CreateMap<Product, ProductDTO>();
+
+            //Categories 
+            CreateMap<ImportCategoryDTO, Category>();
+
+            //CategoriesProducts
+            CreateMap<ImportCategoryProductsDTO, CategoryProduct>();
+
+
+        }
+    }
+}
