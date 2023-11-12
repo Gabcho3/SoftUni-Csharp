bb2d9a10bcc1659607b7371b72ec334ebd3b5a46
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-12 20:47:55 +0200

    Added Solution 13

---
+++ C# DB/Entity Framework Core/07. XML/13. Import Sales/CarDealerProfile.cs
@@ -0,0 +1,31 @@
+﻿using _09._Import_Suppliers.DTOs.Import;
+using _10._Import_Parts.DTOs.Import;
+using _11._Import_Cars.DTOs.Import;
+using _12._Import_Customers.DTOs.Import;
+using _13._Import_Sales.DTOs.Import;
+using AutoMapper;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Suppliers
+            CreateMap<ImportSuppliersDTO, Supplier>();
+
+            //Parts
+            CreateMap<ImportPartsDTO, Part>();
+
+            //Cars
+            CreateMap<ImportCarsDTO, Car>();
+
+            //Customers
+            CreateMap<ImportCustomersDTO, Customer>();
+
+            //Cars
+            CreateMap<ImportSalesDTO, Sale>();
+        }
+    }
+}
