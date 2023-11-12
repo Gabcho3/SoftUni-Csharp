ca82285afeb22ab17f01c94213d004b78875aa42
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-12 20:47:44 +0200

    Added Solution 12

---
+++ C# DB/Entity Framework Core/07. XML/12. Import Customers/CarDealerProfile.cs
@@ -0,0 +1,27 @@
+﻿using _09._Import_Suppliers.DTOs.Import;
+using _10._Import_Parts.DTOs.Import;
+using _11._Import_Cars.DTOs.Import;
+using _12._Import_Customers.DTOs.Import;
+using AutoMapper;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Suppliers
+            CreateMap<ImportSuppliersDTO, Supplier>();
+
+            //Parts
+            CreateMap<ImportPartsDTO, Part>();
+
+            //Cars
+            CreateMap<ImportCarsDTO, Car>();
+
+            //Customers
+            CreateMap<ImportCustomersDTO, Customer>();
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/07. XML/12. Import Customers/DTOs/Import/ImportCarsDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _11._Import_Cars.DTOs.Import
+{
+    [XmlType("Car")]
+    public class ImportCarsDTO
+    {
+        [XmlElement("make")] public string Make { get; set; } = null!;
+
+        [XmlElement("model")] public string Model { get; set; } = null!;
+
+        [XmlElement("traveledDistance")] public long TraveledDistance { get; set; }
+
+        [XmlArray("parts")]
+        public HashSet<PartIdsDTO> PartIds { get; set; }
+    }
+
+    [XmlType("partId")]
+    public class PartIdsDTO
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+    }
+}
