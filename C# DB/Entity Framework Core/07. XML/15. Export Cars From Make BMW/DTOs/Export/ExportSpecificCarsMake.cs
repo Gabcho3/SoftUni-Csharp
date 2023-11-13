521f7684e9f6c5c5dd5b13dbc9a3565cfcc70f7f
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-13 19:46:10 +0200

    Added Solution 15

---
+++ C# DB/Entity Framework Core/07. XML/15. Export Cars From Make BMW/DTOs/Export/ExportSpecificCarsMake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _15._Export_Cars_From_Make_BMW.DTOs.Export
+{
+    [XmlType("car")]
+    public class ExportSpecificCarsMake
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("model")]
+        public string Model { get; set; } = null!;
+
+        [XmlAttribute("traveled-distance")]
+        public long TraveledDistance { get; set; }
+    }
+}
