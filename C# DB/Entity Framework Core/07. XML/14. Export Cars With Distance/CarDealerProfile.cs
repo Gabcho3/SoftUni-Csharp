4db0ce7d50b390b63ee13a56c3d36148de899bc1
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-13 19:46:00 +0200

    Added Solution 14

---
+++ C# DB/Entity Framework Core/07. XML/14. Export Cars With Distance/CarDealerProfile.cs
@@ -0,0 +1,33 @@
+﻿using _09._Import_Suppliers.DTOs.Import;
+using _10._Import_Parts.DTOs.Import;
+using _11._Import_Cars.DTOs.Import;
+using _12._Import_Customers.DTOs.Import;
+using _13._Import_Sales.DTOs.Import;
+using _14._Export_Cars_With_Distance.DTOs.Export;
+using AutoMapper;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Suppliers
+            CreateMap<ImportSuppliersDTO, Supplier>();
+
+            //Parts
+            CreateMap<ImportPartsDTO, Part>();
+
+            //Cars
+            CreateMap<ImportCarsDTO, Car>();
+
+            //Customers
+            CreateMap<ImportCustomersDTO, Customer>();
+
+            //Cars
+            CreateMap<ImportSalesDTO, Sale>();
+            CreateMap<Car, ExportCarsDTO>();
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/07. XML/14. Export Cars With Distance/DTOs/Import/ImportCustomersDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _12._Import_Customers.DTOs.Import
+{
+    [XmlType("Customer")]
+    public class ImportCustomersDTO
+    {
+        [XmlElement("name")]
+        public string? Name { get; set; }
+
+        [XmlElement("birthDate")]
+        public DateTime BirthDate { get; set; }
+
+        [XmlElement("isYoungDriver")]
+        public bool IsYoungDriver { get; set; }
+    }
+}
