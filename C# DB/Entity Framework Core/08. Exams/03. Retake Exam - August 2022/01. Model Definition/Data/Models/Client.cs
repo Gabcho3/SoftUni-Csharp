567a3590b2b7c5d23ac90fc810a06b2309c754b4
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-20 21:26:38 +0200

    Added Models

---
+++ C# DB/Entity Framework Core/08. Exams/03. Retake Exam - August 2022/01. Model Definition/Data/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trucks.Data.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(maximumLength: 40, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(maximumLength: 40, MinimumLength = 2)]
+        public string Nationality { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+
+        public ICollection<ClientTruck> ClientsTrucks { get; set; } = new HashSet<ClientTruck>();
+    }
+}
---
+++ C# DB/Entity Framework Core/08. Exams/03. Retake Exam - August 2022/01. Model Definition/Data/Models/ClientTruck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trucks.Data.Models
+{
+    public class ClientTruck
+    {
+        public int ClientId { get; set; }
+        public Client Client { get; set; } = null!;
+
+        public int TruckId { get; set; }
+        public Truck Truck { get; set; } = null!;
+    }
+}
