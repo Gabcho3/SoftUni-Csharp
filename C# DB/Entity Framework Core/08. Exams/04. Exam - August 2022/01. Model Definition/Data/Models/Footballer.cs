9c94dba4b60dcf15b55d7637a3f03791fc9d9d8c
Gabriel Dimitrov <gabriel_work@abv.bg>
2023-11-23 20:05:11 +0200

    Created Models

---
+++ C# DB/Entity Framework Core/08. Exams/04. Exam - August 2022/01. Model Definition/Data/Models/Footballer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Footballers.Data.Models.Enums;
+
+namespace Footballers.Data.Models
+{
+    public class Footballer
+    {
+        public Footballer()
+        {
+            TeamsFootballers = new HashSet<TeamFootballer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(maximumLength: 40, MinimumLength = 2)]
+        public string Name { get; set; } = null!;
+
+        public DateTime ContractStartDate { get; set; }
+
+        public DateTime ContractEndDate { get; set; }
+
+        [Range(0, 3)]
+        public PositionType Position { get; set; }
+
+        [Range(0, 4)]
+        public BestSkillType BestSkill { get; set; }
+
+        [ForeignKey(nameof(Coach))]
+        public int CoachId { get; set; }
+        public Coach Coach { get; set; } = null!;
+
+        public ICollection<TeamFootballer> TeamsFootballers { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/08. Exams/04. Exam - August 2022/01. Model Definition/Data/Models/Team.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.Data.Models
+{
+    public  class Team
+    {
+        public Team()
+        {
+            TeamsFootballers = new HashSet<TeamFootballer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [RegularExpression(@"^[A-Za-z\d\s.-]*$")]
+        [StringLength(maximumLength: 40, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(maximumLength: 40, MinimumLength = 2)]
+        public string Nationality { get; set; } = null!;
+
+        public int Trophies { get; set; }
+
+        public ICollection<TeamFootballer> TeamsFootballers { get; set; }
+    }
+}
