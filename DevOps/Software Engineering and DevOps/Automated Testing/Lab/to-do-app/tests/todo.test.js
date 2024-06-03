df591342ddc7ae06151ffc6732eb0bf5f8791b25
Gabriel Dimitrov <gabriel_work@abv.bg>
2024-06-03 21:57:22 +0300

    Test for deleting task added

--- DevOps/Software Engineering and DevOps/Automated Testing/Lab/to-do-app/tests/todo.test.js
+++ DevOps/Software Engineering and DevOps/Automated Testing/Lab/to-do-app/tests/todo.test.js
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 
+//Verify if user can add a task
 test("user can add a task", async ({ page }) => {
   await page.goto("http://localhost:5500/");
   await page.fill("#task-input", "Test Task");
@@ -7,4 +8,21 @@ test("user can add a task", async ({ page }) => {
 
   const taskText = await page.textContent(".task");
   expect(taskText).toContain("Test Task");
+});
+
+//Verify if user can delete a task
+test("user can delete a task", async ({ page }) => {
+  //Add a task
+  await page.goto("http://localhost:5500/");
+  await page.fill("#task-input", "Test Task");
+  await page.click("#add-task");
+
+  //Delete the task
+  await page.click(".delete-task");
+
+  const tasks = await page.$$eval(".task", (tasks) =>
+    tasks.map((task) => task.textContent)
+  );
+
+  expect(tasks).not.toContain("Test Task");
 });
