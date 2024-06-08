6f6325b4e84ec6b716e35b683748cf9229357414
Gabriel Dimitrov <gabriel_work@abv.bg>
2024-06-08 22:42:15 +0300

    Add test for verifying that user can login

--- DevOps/Software Engineering and DevOps/Automated Testing/Exercise/Library-Catalog/tests/ui.test.js
+++ DevOps/Software Engineering and DevOps/Automated Testing/Exercise/Library-Catalog/tests/ui.test.js
@@ -35,3 +35,21 @@ test('Verify "Register" link is visible', async ({ page }) => {
   const isVisible = await registerLink.isVisible();
   expect(isVisible).toBe(true);
 });
+
+test("Verify user can login", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.waitForSelector("nav.navbar");
+
+  //Click on login
+  await page.click('a[href="/login"]');
+
+  //Submit loging form
+  await page.fill("#email", "peter@abv.bg");
+  await page.fill("#password", "123456");
+  await page.click('input[type="submit"]');
+
+  //Verify there is logout button
+  const logoutBtn = await page.$("#logoutBtn");
+  const isVisible = await logoutBtn.isVisible();
+  expect(isVisible).toBe(true);
+});
