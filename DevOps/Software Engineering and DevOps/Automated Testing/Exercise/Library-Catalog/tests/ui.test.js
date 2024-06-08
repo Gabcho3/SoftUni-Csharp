136928bb977485b27fe73694da6268480ef950da
Gabriel Dimitrov <gabriel_work@abv.bg>
2024-06-08 22:21:07 +0300

    Add tests for visibility of login and register links

--- DevOps/Software Engineering and DevOps/Automated Testing/Exercise/Library-Catalog/tests/ui.test.js
+++ DevOps/Software Engineering and DevOps/Automated Testing/Exercise/Library-Catalog/tests/ui.test.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test('Verify "All books" links is visible', async ({ page }) => {
+test('Verify "All books" link is visible', async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await page.waitForSelector("nav.navbar");
 
@@ -9,5 +9,29 @@ test('Verify "All books" links is visible', async ({ page }) => {
 
   //Verify link is visible
   const isVisible = await allBooksLink.isVisible();
+  expect(isVisible).toBe(true);
+});
+
+test('Verify "Login" link is visible', async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.waitForSelector("nav.navbar");
+
+  //Getting link
+  const loginLink = await page.$('a[href="/login"]');
+
+  //Verify link is visible
+  const isVisible = await loginLink.isVisible();
+  expect(isVisible).toBe(true);
+});
+
+test('Verify "Register" link is visible', async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.waitForSelector("nav.navbar");
+
+  //Getting link
+  const registerLink = await page.$('a[href="/register"]');
+
+  //Verify link is visible
+  const isVisible = await registerLink.isVisible();
   expect(isVisible).toBe(true);
 });
