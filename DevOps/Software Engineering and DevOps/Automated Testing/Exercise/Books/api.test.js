eb6c622ce0552212e3f31d5928b508461579bac5
Gabriel Dimitrov <gabriel_work@abv.bg>
2024-06-10 11:40:27 +0300

    Add test to verify that you can add a Book

---
+++ DevOps/Software Engineering and DevOps/Automated Testing/Exercise/Books/api.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('./server');
+const { exec } = require('child_process');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Books API', () => {
+  const bookId = "1";
+
+  it("shoud be able to add a Book", (done) => {
+    const book = { id: bookId, title: "Test Book", author: "Test Author" };
+
+    chai.request(server)
+      .post("/books")
+      .send(book)
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.a("object");
+        expect(res.body.id).to.be.equal(book.id);
+        expect(res.body.title).to.be.equal(book.title);
+        expect(res.body.author).to.be.equal(book.author);
+        done();
+      });
+  });
+});
